0212f2a0462ddc3aacf0e0aadedef6612699e781
platypusguy <920630+platypusguy@users.noreply.github.com>
2021-07-22 00:14:09 -0700

    Added unit tests. Coverage should be complete for all principal actions now.

--- src/logger_test.go
+++ src/logger_test.go
@@ -6,6 +6,9 @@
 package main
 
 import (
+	"io/ioutil"
+	"os"
+	"strings"
 	"testing"
 )
 
@@ -22,5 +25,62 @@ func TestLogLevelTooHigh(t *testing.T) {
 	err := SetLogLevel(99)
 	if err == nil {
 		t.Error("setting logging level to 99 did not generate an error")
+	}
+}
+
+func TestEmptyLogMessage(t *testing.T) {
+	Global = initGlobals("test")
+	SetLogLevel(WARNING)
+	err := Log("", SEVERE)
+	if err == nil {
+		t.Error("trying to log an empty message did not generate an error")
+	}
+}
+
+func TestValidLogMessageFineLevel(t *testing.T) {
+	Global = initGlobals("test")
+	SetLogLevel(FINE)
+
+	// to test the error message, capture the writing done to stderr
+	normalStderr := os.Stderr
+	r, w, _ := os.Pipe()
+	os.Stderr = w
+
+	Log("Test message (FINE)", FINE)
+
+	// reset stderr to what it was before
+	w.Close()
+	out, _ := ioutil.ReadAll(r)
+	os.Stdout = normalStderr
+
+	msg := string(out[:])
+
+	if !strings.Contains(msg, "Test message (FINE)") ||
+		!strings.HasPrefix(msg, "[") { // a FINE message should start with elapsed time between [ ]'s
+		t.Error("valid FINE logging message was not logged properly")
+	}
+}
+
+func TestValidLogMessageWarningLevel(t *testing.T) {
+	Global = initGlobals("test")
+	SetLogLevel(WARNING)
+
+	// to test the error message, capture the writing done to stderr
+	normalStderr := os.Stderr
+	r, w, _ := os.Pipe()
+	os.Stderr = w
+
+	Log("Test message (WARNING)", WARNING)
+
+	// reset stderr to what it was before
+	w.Close()
+	out, _ := ioutil.ReadAll(r)
+	os.Stdout = normalStderr
+
+	msg := string(out[:])
+
+	if !strings.Contains(msg, "Test message (WARNING)") ||
+		strings.HasPrefix(msg, "[") { // if the global log level is warning, no elapsed time should be logged
+		t.Error("valid WARNING logging message was not logged properly")
 	}
 }
