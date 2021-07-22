b8125d8544afe803e6a1231e9ca41c7dfadcffd6
platypusguy <920630+platypusguy@users.noreply.github.com>
2021-07-21 19:31:15 -0700

    Added copyright

--- src/main_test.go
+++ src/main_test.go
@@ -1,3 +1,8 @@
+/* Jacobin VM -- A Java virtual machine
+ * © Copyright 2021 by Andrew Binstock. All rights reserved
+ * Licensed under Mozilla Public License 2.0 (MPL-2.0)
+ */
+
 package main
 
 import (
