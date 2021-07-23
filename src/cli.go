5fc34ea60c649764c0b97557186e13433c622ce3
platypusguy <920630+platypusguy@users.noreply.github.com>
2021-07-22 23:28:38 -0700

    Initial skeleton of CLI processing

---
+++ src/cli.go
@@ -0,0 +1,10 @@
+/* Jacobin VM -- A Java virtual machine
+ * © Copyright 2021 by Andrew Binstock. All rights reserved
+ * Licensed under Mozilla Public License 2.0 (MPL-2.0)
+ */
+
+package main
+
+func HandleCli() (err error) {
+	return
+}
