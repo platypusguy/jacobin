0ba0fb9a2630d58afd3746752e8cf2ce2e7f3e69
platypusguy <920630+platypusguy@users.noreply.github.com>
2021-07-23 15:10:28 -0700

    added retrieval of options specified in environment variables

--- src/cli.go
+++ src/cli.go
@@ -5,12 +5,44 @@
 
 package main
 
+import (
+	"os"
+	"strings"
+)
+
 func HandleCli() (err error) {
+	var javaEnvOptions = getEnvArgs()
+	Log("Java environment variables: "+javaEnvOptions, FINEST)
 	return
 }
 
-/*
-JAVA_TOOL_OPTIONS
-_JAVA_OPTIONS ☑
-JDK_JAVA_OPTIONS
-*/
+// you can can set JVM options using the three environment variables that are
+// inspected in this function. Note: order is important because later options
+// can override earlier ones. These are checked before any of the command-line
+// options are processed.
+func getEnvArgs() string {
+	jto := os.Getenv("JAVA_TOOL_OPTIONS")
+	jo := os.Getenv("_JAVA_OPTIONS")
+	jjo := os.Getenv("JDK_JAVA_OPTIONS")
+
+	envArgs := ""
+	if len(jto) > 0 {
+		envArgs += jto
+		if !strings.HasSuffix(envArgs, " ") {
+			envArgs += " "
+		}
+	}
+
+	if len(jo) > 0 {
+		envArgs += jo
+		if !strings.HasSuffix(envArgs, " ") {
+			envArgs += " "
+		}
+	}
+
+	if len(jjo) > 0 {
+		envArgs += jjo
+	}
+
+	return strings.TrimSpace(envArgs)
+}
