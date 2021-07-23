cef78a0749a6aa15a859d8666e73b3e56fc8b9e5
platypusguy <920630+platypusguy@users.noreply.github.com>
2021-07-22 23:28:01 -0700

    Added test to make sure that calling main() did generate a copyright notice to stdout.

--- src/main_test.go
+++ src/main_test.go
@@ -12,6 +12,28 @@ import (
 	"testing"
 )
 
+// test the main() function. As in some of the other tests in this file,
+// stdout is rerouted so that the test can capture and test the output.
+// It is then restored to its usual settings
+func TestMainFunc(t *testing.T) {
+	normalStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	main()
+
+	w.Close()
+	out, _ := ioutil.ReadAll(r)
+	os.Stdout = normalStdout
+
+	msg := string(out[:])
+
+	if !strings.Contains(msg, "All rights reserved.") ||
+		!strings.Contains(msg, "2021") {
+		t.Error("Copyright notice in main() does not appear or appears incorrectly")
+	}
+}
+
 func TestShowCopyright(t *testing.T) {
 	normalStdout := os.Stdout
 	r, w, _ := os.Pipe()
