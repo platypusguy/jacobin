5ca951b4de3e29a81d2ed5a2ea807b02ab655f7c
platypusguy <920630+platypusguy@users.noreply.github.com>
2021-08-23 00:17:14 -0700

    Began work on parsing of classfile

--- src/classloader/classloader.go
+++ src/classloader/classloader.go
@@ -7,6 +7,7 @@
 package classloader
 
 import (
+	"errors"
 	"fmt"
 	"jacobin/log"
 	"os"
@@ -19,12 +20,24 @@ import (
 type Classloader struct {
 	Name    string
 	Parent  string
-	Classes map[string]Klass
+	Classes map[string]parsedClass
 }
 
 var AppCL Classloader
 var BootstrapCL Classloader
 var ExtensionCL Classloader
+
+// the parsed class
+type parsedClass struct {
+}
+
+// cfe = class format error, which is the error thrown by the parser for most
+// of the errors arising from malformed bytecode
+func cfe(msg string) error {
+	errMsg := "Class Format Error: " + msg
+	log.Log(errMsg+". Exiting.", log.SEVERE)
+	return errors.New(errMsg)
+}
 
 // first canonicalizes the filename, checks whether the class is already loaded,
 // and if not, then parses the class and loads it.
@@ -34,35 +47,63 @@ var ExtensionCL Classloader
 // 3 TODO: determine which classloader should load the class, then
 // 4 TODO: have *it* parse and load the class.
 */
-func (cl Classloader) LoadClassFromFile(filename string) (Klass, error) {
+func (cl Classloader) LoadClassFromFile(filename string) error {
 	rawBytes, err := os.ReadFile(filename)
 	if err != nil {
 		log.Log("Could not read file: "+filename+". Exiting.", log.SEVERE)
-		return Klass{}, fmt.Errorf("file I/O error")
+		return fmt.Errorf("file I/O error")
 	} else {
 		log.Log(filename+" read", log.FINE)
 	}
 
-	parsedClass, err := Parse(rawBytes)
+	fullyParsedClass, err := parse(rawBytes)
 	if err != nil {
 		log.Log("error parsing "+filename+". Exiting.", log.SEVERE)
-		return Klass{}, fmt.Errorf("parsing error")
+		return fmt.Errorf("parsing error")
 	} else {
-		return parsedClass, nil
+		return insert(fullyParsedClass)
 	}
 }
 
 func Init() error {
 	BootstrapCL.Name = "bootstrap"
 	BootstrapCL.Parent = ""
-	BootstrapCL.Classes = make(map[string]Klass)
+	BootstrapCL.Classes = make(map[string]parsedClass)
 
 	ExtensionCL.Name = "extension"
 	ExtensionCL.Parent = "bootstrap"
-	ExtensionCL.Classes = make(map[string]Klass)
+	ExtensionCL.Classes = make(map[string]parsedClass)
 
 	AppCL.Name = "app"
 	AppCL.Parent = "system"
-	AppCL.Classes = make(map[string]Klass)
+	AppCL.Classes = make(map[string]parsedClass)
 	return nil
+}
+
+// insert the fully parsed class into the classloader
+func insert(class parsedClass) error {
+	return nil //TODO: fill out after finishing parser
+}
+
+// the main parsing operation on the classfile. Drives the numberous functions that follow.
+func parse(rawBytes []byte) (parsedClass, error) {
+	var klass = parsedClass{}
+	err := parseMagicNumber(rawBytes)
+	if err != nil {
+		return klass, err
+	}
+
+	return klass, nil
+}
+
+// all bytecode files start with 0xCAFEBABE ( it was the 90s!)
+// this checks for that.
+func parseMagicNumber(bytes []byte) error {
+	if len(bytes) < 4 {
+		return cfe("invalid magic number")
+	} else if (bytes[0] != 0xCA) || (bytes[1] != 0xFE) || (bytes[2] != 0xBA) || (bytes[3] != 0xBE) {
+		return cfe("invalid magic number")
+	} else {
+		return nil
+	}
 }
