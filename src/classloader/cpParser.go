1fb1d995cb0f06fe19bb4d6a176de1d07c3f5168
platypusguy <920630+platypusguy@users.noreply.github.com>
2021-08-27 01:06:18 -0700

    JACOBIN-6 Working on figuring out how to create the CP table which has varying structs. Exploring using interfaces as the principal types. Lots more work to be done to figure this out.

--- src/classloader/cpParser.go
+++ src/classloader/cpParser.go
@@ -31,8 +31,54 @@ const (
 	Package       = 20
 )
 
+// every CP entry must update the position in the classfile
+type posUdater interface {
+	update(int) int
+}
+
+// each entry in the CP table consists of an identifying integer (see enums above)
+// and a structure that has varying fields and fulfills the posUdater interface.
+// all entries are defined at the end of this file
+type cpEntry struct {
+	cpType int
+	posUdater
+}
+
+var cp []posUdater
+
 func parseConstantPool(rawBytes []byte, klass *parsedClass) (int, error) {
 	pos := 10 // position of the first byte of the constant pool
 
+	// cpE := cpEntry{-1, dumbbell.updatePos(0) }
+	// cpDummyEntry := dummyEntry{-1,interface{cpEntry.update}}
+	cp[0] = cpEntry{cpType: -1}
+	cp[1] = dumbbell{i: -1}
+	println(cp)
+
 	return pos, nil
 }
+
+type dumbbell struct {
+	i int
+}
+
+func (dumbbell) update(i int) int {
+	return 0
+}
+
+// d := dumbbell {i: -1, d.update: udpatePos, }
+
+// func (dumbbell) update( i int) int { return 0 }
+
+//
+// var dumdum posUdater
+func update(i int) int {
+	return 0
+}
+
+// type dummyEntry posUdater {
+//
+// }
+// func (dumbbell) update( i int ) int {
+// 	return 0
+// }
