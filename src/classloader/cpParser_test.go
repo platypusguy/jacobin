f8b4b42fb9838b0680f311c9238194ef2042d157
platypusguy <920630+platypusguy@users.noreply.github.com>
2021-08-31 00:55:43 -0700

    JACOBIN-11 Added tests for parsing of class reference entries in CP; cleaned up some of the other tests

--- src/classloader/cpParser_test.go
+++ src/classloader/cpParser_test.go
@@ -13,7 +13,7 @@ import (
 	"testing"
 )
 
-func TestCPvalidFieldRef(t *testing.T) {
+func TestCPvalidClassRef(t *testing.T) {
 
 	globals.InitGlobals("test")
 	log.Init()
@@ -22,7 +22,7 @@ func TestCPvalidFieldRef(t *testing.T) {
 	bytesToTest := []byte{
 		0xCA, 0xFE, 0xBA, 0xBA, 0x00,
 		0x00, 0xFF, 0xF0, 0x00, 0x00,
-		0x09, 0x00, 0x14, 0x01, 0x01,
+		0x07, 0x02, 0x05,
 	}
 
 	pc := parsedClass{}
@@ -30,66 +30,20 @@ func TestCPvalidFieldRef(t *testing.T) {
 	loc, err := parseConstantPool(bytesToTest, &pc)
 
 	if err != nil {
-		t.Error("Parsing valid CP FieldRef generated an unexpected error")
+		t.Error("Parsing valid CP class reference (7) generated an unexpected error")
 	}
 
-	if loc != 14 {
-		t.Error("Was expecting a new position of 14, but got: " + strconv.Itoa(loc))
+	if loc != 12 {
+		t.Error("Was expecting a new position of 12, but got: " + strconv.Itoa(loc))
 	}
 
-	if len(fieldRefs) != 1 {
-		t.Error("Was expecting the field ref array to have 1 entry, but it has: " + strconv.Itoa(len(fieldRefs)))
+	if len(classRefs) != 1 {
+		t.Error("Was expecting the class ref array to have 1 entry, but it has: " + strconv.Itoa(len(classRefs)))
 	}
 
-	fre := fieldRefs[0]
-	if fre.classIndex != 20 {
-		t.Error("Was expecting a field ref classIndex of 20, but got: " + strconv.Itoa(fre.classIndex))
-	}
-
-	if fre.nameAndTypeIndex != 257 {
-		t.Error("Was expecting a field ref classIndex of 257, but got: " + strconv.Itoa(fre.nameAndTypeIndex))
-	}
-
-	if len(cpool) != 2 {
-		t.Error("Was expecting cpool to have 2 entries, but instead got: " + strconv.Itoa(len(cpool)))
-	}
-}
-
-func TestCPvalidMethodRef(t *testing.T) {
-
-	globals.InitGlobals("test")
-	log.Init()
-	log.SetLogLevel(log.WARNING)
-
-	bytesToTest := []byte{
-		0xCA, 0xFE, 0xBA, 0xBA, 0x00,
-		0x00, 0xFF, 0xF0, 0x00, 0x00,
-		0x0A, 0x00, 0x15, 0x01, 0x06,
-	}
-
-	pc := parsedClass{}
-	pc.cpCount = 2
-	loc, err := parseConstantPool(bytesToTest, &pc)
-
-	if err != nil {
-		t.Error("Parsing valid CP FieldRef generated an unexpected error")
-	}
-
-	if loc != 14 {
-		t.Error("Was expecting a new position of 14, but got: " + strconv.Itoa(loc))
-	}
-
-	if len(methodRefs) != 1 {
-		t.Error("Was expecting the method ref array to have 1 entry, but it has: " + strconv.Itoa(len(methodRefs)))
-	}
-
-	mre := methodRefs[0]
-	if mre.classIndex != 21 {
-		t.Error("Was expecting a method ref classIndex of 21, but got: " + strconv.Itoa(mre.classIndex))
-	}
-
-	if mre.nameAndTypeIndex != 262 {
-		t.Error("Was expecting a method ref classIndex of 262, but got: " + strconv.Itoa(mre.nameAndTypeIndex))
+	cre := classRefs[0]
+	if cre.index != 517 {
+		t.Error("Was expecting a class ref index of 517, but got: " + strconv.Itoa(cre.index))
 	}
 
 	if len(cpool) != 2 {
@@ -114,7 +68,7 @@ func TestCPvalidStringConstRef(t *testing.T) {
 	loc, err := parseConstantPool(bytesToTest, &pc)
 
 	if err != nil {
-		t.Error("Parsing valid CP StringConstRef generated an unexpected error")
+		t.Error("Parsing valid CP StringConstRef (8) generated an unexpected error")
 	}
 
 	if loc != 12 {
@@ -122,12 +76,96 @@ func TestCPvalidStringConstRef(t *testing.T) {
 	}
 
 	if len(stringRefs) != 1 {
-		t.Error("Was expecting the string const ref array to have 1 entry, but it has: " + strconv.Itoa(len(methodRefs)))
+		t.Error("Was expecting the string const ref array to have 1 entry, but it has: " + strconv.Itoa(len(stringRefs)))
 	}
 
 	sre := stringRefs[0]
 	if sre.index != 32 {
-		t.Error("Was expecting a method ref classIndex of 32, but got: " + strconv.Itoa(sre.index))
+		t.Error("Was expecting a string ref index of 32, but got: " + strconv.Itoa(sre.index))
+	}
+
+	if len(cpool) != 2 {
+		t.Error("Was expecting cpool to have 2 entries, but instead got: " + strconv.Itoa(len(cpool)))
+	}
+}
+
+func TestCPvalidFieldRef(t *testing.T) {
+
+	globals.InitGlobals("test")
+	log.Init()
+	log.SetLogLevel(log.WARNING)
+
+	bytesToTest := []byte{
+		0xCA, 0xFE, 0xBA, 0xBA, 0x00,
+		0x00, 0xFF, 0xF0, 0x00, 0x00,
+		0x09, 0x00, 0x14, 0x01, 0x01,
+	}
+
+	pc := parsedClass{}
+	pc.cpCount = 2
+	loc, err := parseConstantPool(bytesToTest, &pc)
+
+	if err != nil {
+		t.Error("Parsing valid CP FieldRef (09) generated an unexpected error")
+	}
+
+	if loc != 14 {
+		t.Error("Was expecting a new position of 14, but got: " + strconv.Itoa(loc))
+	}
+
+	if len(fieldRefs) != 1 {
+		t.Error("Was expecting the field ref array to have 1 entry, but it has: " + strconv.Itoa(len(fieldRefs)))
+	}
+
+	fre := fieldRefs[0]
+	if fre.classIndex != 20 {
+		t.Error("Was expecting a field ref classIndex of 20, but got: " + strconv.Itoa(fre.classIndex))
+	}
+
+	if fre.nameAndTypeIndex != 257 {
+		t.Error("Was expecting a field ref nameAndTypeIndex of 257, but got: " + strconv.Itoa(fre.nameAndTypeIndex))
+	}
+
+	if len(cpool) != 2 {
+		t.Error("Was expecting cpool to have 2 entries, but instead got: " + strconv.Itoa(len(cpool)))
+	}
+}
+
+func TestCPvalidMethodRef(t *testing.T) {
+
+	globals.InitGlobals("test")
+	log.Init()
+	log.SetLogLevel(log.WARNING)
+
+	bytesToTest := []byte{
+		0xCA, 0xFE, 0xBA, 0xBA, 0x00,
+		0x00, 0xFF, 0xF0, 0x00, 0x00,
+		0x0A, 0x00, 0x15, 0x01, 0x06,
+	}
+
+	pc := parsedClass{}
+	pc.cpCount = 2
+	loc, err := parseConstantPool(bytesToTest, &pc)
+
+	if err != nil {
+		t.Error("Parsing valid CP MethodRef (10) generated an unexpected error")
+	}
+
+	if loc != 14 {
+		t.Error("Was expecting a new position of 14, but got: " + strconv.Itoa(loc))
+	}
+
+	if len(methodRefs) != 1 {
+		t.Error("Was expecting the method ref array to have 1 entry, but it has: " + strconv.Itoa(len(methodRefs)))
+	}
+
+	mre := methodRefs[0]
+	if mre.classIndex != 21 {
+		t.Error("Was expecting a method ref classIndex of 21, but got: " + strconv.Itoa(mre.classIndex))
+	}
+
+	if mre.nameAndTypeIndex != 262 {
+		t.Error("Was expecting a method ref nameAndType of 262, but got: " + strconv.Itoa(mre.nameAndTypeIndex))
 	}
 
 	if len(cpool) != 2 {
