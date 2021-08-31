0f8d2cfb328cf57ad67c78404add25a4f15e17d7
platypusguy <920630+platypusguy@users.noreply.github.com>
2021-08-30 23:46:16 -0700

    JACOBIN-10 Added test to parsing of string constant reference entries (0x08)

--- src/classloader/cpParser_test.go
+++ src/classloader/cpParser_test.go
@@ -33,7 +33,7 @@ func TestCPvalidFieldRef(t *testing.T) {
 	}
 
 	if loc != 14 {
-		t.Error("Was expecting a new position of 13, but got: " + strconv.Itoa(loc))
+		t.Error("Was expecting a new position of 14, but got: " + strconv.Itoa(loc))
 	}
 
 	if len(fieldRefs) != 1 {
@@ -74,7 +74,7 @@ func TestCPvalidMethodRef(t *testing.T) {
 	}
 
 	if loc != 14 {
-		t.Error("Was expecting a new position of 13, but got: " + strconv.Itoa(loc))
+		t.Error("Was expecting a new position of 14, but got: " + strconv.Itoa(loc))
 	}
 
 	if len(methodRefs) != 1 {
@@ -88,6 +88,44 @@ func TestCPvalidMethodRef(t *testing.T) {
 
 	if mre.nameAndTypeIndex != 262 {
 		t.Error("Was expecting a method ref classIndex of 262, but got: " + strconv.Itoa(mre.nameAndTypeIndex))
+	}
+
+	if len(cpool) != 2 {
+		t.Error("Was expecting cpool to have 2 entries, but instead got: " + strconv.Itoa(len(cpool)))
+	}
+}
+
+func TestCPvalidStringConstRef(t *testing.T) {
+
+	globals.InitGlobals("test")
+	log.Init()
+	log.SetLogLevel(log.WARNING)
+
+	bytesToTest := []byte{
+		0xCA, 0xFE, 0xBA, 0xBA, 0x00,
+		0x00, 0xFF, 0xF0, 0x00, 0x00,
+		0x08, 0x00, 0x20,
+	}
+
+	pc := parsedClass{}
+	pc.cpCount = 2
+	loc, err := parseConstantPool(bytesToTest, &pc)
+
+	if err != nil {
+		t.Error("Parsing valid CP StringConstRef generated an unexpected error")
+	}
+
+	if loc != 12 {
+		t.Error("Was expecting a new position of 12, but got: " + strconv.Itoa(loc))
+	}
+
+	if len(stringRefs) != 1 {
+		t.Error("Was expecting the string const ref array to have 1 entry, but it has: " + strconv.Itoa(len(methodRefs)))
+	}
+
+	sre := stringRefs[0]
+	if sre.index != 32 {
+		t.Error("Was expecting a method ref classIndex of 32, but got: " + strconv.Itoa(sre.index))
 	}
 
 	if len(cpool) != 2 {
