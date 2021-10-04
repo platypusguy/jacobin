787af531a0290f157d84bb442191ae2043af4c81
platypusguy <920630+platypusguy@users.noreply.github.com>
2021-10-04 13:07:01 -0700

    JACOBIN-70 Added test to methodParser.go for valid Code attribute

--- src/classloader/methodParser_test.go
+++ src/classloader/methodParser_test.go
@@ -16,6 +16,82 @@ import (
 	"testing"
 )
 
+// test a valid Code attribute of a method
+func TestValidCodeMethodAttribute(t *testing.T) {
+	globals.InitGlobals("test")
+	log.Init()
+	log.SetLogLevel(log.FINEST)
+
+	// redirect stderr & stdout to capture results from stderr
+	normalStderr := os.Stderr
+	_, w, _ := os.Pipe()
+	os.Stderr = w
+
+	normalStdout := os.Stdout
+	_, wout, _ := os.Pipe()
+	os.Stdout = wout
+
+	// variables we'll need.
+	klass := parsedClass{}
+	klass.cpIndex = append(klass.cpIndex, cpEntry{})
+	klass.cpIndex = append(klass.cpIndex, cpEntry{UTF8, 0})
+	klass.cpIndex = append(klass.cpIndex, cpEntry{ClassRef, 0}) // points to classRef below, which points to the next CP entry
+	klass.cpIndex = append(klass.cpIndex, cpEntry{UTF8, 2})
+
+	klass.utf8Refs = append(klass.utf8Refs, utf8Entry{"Exceptions"})
+	klass.utf8Refs = append(klass.utf8Refs, utf8Entry{"testMethod"})
+	klass.utf8Refs = append(klass.utf8Refs, utf8Entry{"java/io/IOException"})
+
+	klass.classRefs = append(klass.classRefs, 3) // classRef[0] points to CP entry #4, which points to UTF #3
+
+	klass.cpCount = 4
+
+	// method
+	meth := method{}
+	meth.name = 2 // points to UTF8 entry: "testMethod"
+
+	attrib := attr{}
+	attrib.attrName = 1
+	attrib.attrSize = 4
+	attrib.attrContent = []byte{
+		0, 4, // maxstack = 4
+		0, 3, // maxlocals = 3
+		0, 0, 0, 2, // code length = 2
+		0x11, 0x16, // the two code bytes (randomly chosen)
+		0, 0, // number of exceptions = 0 (exception handling is done elsewhere)
+		0, 0, // attribute count of Code attribute (line number, etc.) = 0
+	}
+
+	err := parseCodeAttribute(attrib, &meth, &klass)
+	if err != nil {
+		t.Error("Unexpected error in processing valid Exceptions attribute of method")
+	}
+
+	// restore stderr and stdout to what they were before
+	_ = w.Close()
+	// out, _ := ioutil.ReadAll(r)
+	os.Stderr = normalStderr
+
+	// msg := string(out[:])
+
+	_ = wout.Close()
+	os.Stdout = normalStdout
+
+	if len(meth.codeAttr.code) != 2 {
+		t.Error("Expected code length of 2. Got: " + strconv.Itoa(len(meth.codeAttr.code)))
+	}
+
+	if meth.codeAttr.maxStack != 4 {
+		t.Error("Expected maxStack of 4. Got: " + strconv.Itoa(meth.codeAttr.maxStack))
+	}
+
+	if meth.codeAttr.maxLocals != 3 {
+		t.Error("Expected maxLocals of 3. Got: " + strconv.Itoa(meth.codeAttr.maxLocals))
+	}
+	if len(meth.codeAttr.attributes) != 0 {
+		t.Error("Expected 0 attributes of Code attribute. Got: " + strconv.Itoa(len(meth.codeAttr.attributes)))
+	}
+}
 func TestValidExceptionsMethodAttribute(t *testing.T) {
 	globals.InitGlobals("test")
 	log.Init()
