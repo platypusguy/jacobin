9f135e38b098799c85f63f876db04485affc1e1d
platypusguy <920630+platypusguy@users.noreply.github.com>
2021-12-17 23:54:34 -0800

    JACOBIN-135 Beginning code on instantiation of objects

--- src/instantiate.go
+++ src/instantiate.go
@@ -7,8 +7,10 @@
 package main
 
 import (
+	"fmt"
 	"jacobin/classloader"
 	"jacobin/log"
+	"os"
 )
 
 func instantiateClass(classname string) (interface{}, error) {
@@ -28,12 +30,30 @@ recheck:
 	if len(k.Data.Fields) > 0 {
 		for i := 0; i < len(k.Data.Fields); i++ {
 			f := k.Data.Fields[i]
-			initializeField(f)
+			initializeField(f, &k.Data.CP)
 		}
 	}
 	return nil, nil
 }
 
-func initializeField(f classloader.Field) {
-	//
+func initializeField(f classloader.Field, cp *classloader.CPool) {
+	name := cp.Utf8Refs[int(f.Name)]
+	desc := cp.Utf8Refs[int(f.Desc)]
+	var attr string = ""
+	if len(f.Attributes) > 0 {
+		for i := 0; i < len(f.Attributes); i++ {
+			attr = cp.Utf8Refs[int(f.Attributes[i].AttrName)]
+			if attr == "ConstantValue" {
+				// valueIndex := int(f.Attributes[i].AttrContent[0])*256 +
+				//     int(f.Attributes[i].AttrContent[1])
+				// // valueType := cp.CpIndex[valueIndex].Type
+				// // valueSlot := cp.CpIndex[valueIndex].Slot
+
+			}
+		}
+	}
+	fmt.Fprintf(os.Stdout, "Field to initialize: %s, type: %s\n", name, desc)
+	if attr != "" {
+		fmt.Fprintf(os.Stdout, "Attribute name: %s\n", attr)
+	}
 }
