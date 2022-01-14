307c53575c17b7f7293b3ed0390f8536f8096b32
platypusguy <920630+platypusguy@users.noreply.github.com>
2022-01-13 23:37:07 -0800

    JACOBIN-145 Added tests for Hello3.class

---
+++ src/wholeClassTests/Hello3_test.go
@@ -0,0 +1,280 @@
+/*
+ * Jacobin VM - A Java virtual machine
+ * Copyright (c) 2022 by Andrew Binstock. All rights reserved.
+ * Licensed under Mozilla Public License 2.0 (MPL 2.0)
+ */
+
+package wholeClassTests
+
+import (
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+// Test for Hello3 class, which has loop and function calls 3 levels deep. Source code:
+//
+// class Hello3 {
+//
+// 	public static void main( String[] args) {
+// 		int x;
+// 		for( int i = 0; i < 10; i++) {
+// 			x = addTwo(i, i-1);
+// 			System.out.println( x );
+// 		}
+// 	}
+//
+// 	static int addTwo(int j, int k) {
+// 		int m = multTwo(j, k);
+// 		return m+1;
+// 	}
+//
+// 	static int multTwo(int m, int n){
+// 		return m*n;
+// }
+// }
+//
+// These tests check the output with various options for verbosity and features set on the command line.
+
+var _jACOBIN = "d:\\GoogleDrive\\Dev\\jacobin\\src\\jacobin.exe"
+var _jVM_ARGS = ""
+var _tESTCLASS = "d:\\GoogleDrive\\Dev\\jacobin\\testdata\\Hello3.class" // the class to test
+var _aPP_ARGS = ""
+
+func TestRunHello3(t *testing.T) {
+	var cmd *exec.Cmd
+
+	if testing.Short() { // don't run if running quick tests only. (Used primarily so GitHub doesn't run and bork)
+		t.Skip()
+	}
+
+	// test that executable exists
+	if _, err := os.Stat(_jACOBIN); err != nil {
+		t.Errorf("Missing Jacobin executable, which was specified as %s", _jACOBIN)
+	}
+
+	// run the various combinations of args. This is necessary b/c the empty string is viewed as
+	// an actual specified option on the command line.
+	if len(_jVM_ARGS) > 0 {
+		if len(_aPP_ARGS) > 0 {
+			cmd = exec.Command(_jACOBIN, _jVM_ARGS, _tESTCLASS, _aPP_ARGS)
+		} else {
+			cmd = exec.Command(_jACOBIN, _jVM_ARGS, _tESTCLASS)
+		}
+	} else {
+		if len(_aPP_ARGS) > 0 {
+			cmd = exec.Command(_jACOBIN, _tESTCLASS, _aPP_ARGS)
+		} else {
+			cmd = exec.Command(_jACOBIN, _tESTCLASS)
+		}
+	}
+
+	// get the stdout and stderr contents from the file execution
+	stderr, err := cmd.StderrPipe()
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// run the command
+	if err = cmd.Start(); err != nil {
+		t.Errorf("Got error running Jacobin: %s", err.Error())
+	}
+
+	// Here begin the actual tests on the output to stderr and stdout
+	slurp, _ := io.ReadAll(stderr)
+	slurpErr := string(slurp)
+	if len(slurp) != 0 {
+		t.Errorf("Got unexpected output to stderr: %s", slurpErr)
+	}
+
+	slurp, _ = io.ReadAll(stdout)
+	slurpOut := string(slurp)
+	if !strings.HasPrefix(slurpOut, "Jacobin VM") {
+		t.Errorf("Stdout did not begin with Jacobin copyright, instead: %s", slurpOut)
+	}
+
+	if !strings.Contains(slurpOut, "57") && !strings.Contains(string(slurp), "73") {
+		t.Errorf("Did not get expected output to stdout. Got: %s", slurpOut)
+	}
+}
+
+func TestRunHello3VerboseClass(t *testing.T) {
+	var cmd *exec.Cmd
+
+	if testing.Short() { // don't run if running quick tests only. (Used primarily so GitHub doesn't run and bork)
+		t.Skip()
+	}
+
+	// test that executable exists
+	if _, err := os.Stat(_jACOBIN); err != nil {
+		t.Errorf("Missing Jacobin executable, which was specified as %s", _jACOBIN)
+	}
+
+	_jVM_ARGS = "-verbose:class"
+	// run the various combinations of args. This is necessary b/c the empty string is viewed as
+	// an actual specified option on the command line.
+	if len(_jVM_ARGS) > 0 {
+		if len(_aPP_ARGS) > 0 {
+			cmd = exec.Command(_jACOBIN, _jVM_ARGS, _tESTCLASS, _aPP_ARGS)
+		} else {
+			cmd = exec.Command(_jACOBIN, _jVM_ARGS, _tESTCLASS)
+		}
+	} else {
+		if len(_aPP_ARGS) > 0 {
+			cmd = exec.Command(_jACOBIN, _tESTCLASS, _aPP_ARGS)
+		} else {
+			cmd = exec.Command(_jACOBIN, _tESTCLASS)
+		}
+	}
+
+	// get the stdout and stderr contents from the file execution
+	stderr, err := cmd.StderrPipe()
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// run the command
+	if err = cmd.Start(); err != nil {
+		t.Errorf("Got error running Jacobin: %s", err.Error())
+	}
+
+	// Here begin the actual tests on the output to stderr and stdout
+	slurp, _ := io.ReadAll(stderr)
+	slurpErr := string(slurp)
+	if !strings.Contains(slurpErr, "Class: Hello3, loader: bootstrap") {
+		t.Errorf("Got unexpected output to stderr: %s", slurpErr)
+	}
+
+	slurp, _ = io.ReadAll(stdout)
+	slurpOut := string(slurp)
+	if !strings.HasPrefix(string(slurp), "Jacobin VM") {
+		t.Errorf("Stdout did not begin with Jacobin copyright, instead: %s", slurpOut)
+	}
+
+	if !strings.Contains(slurpOut, "31") && !strings.Contains(slurpOut, "43") {
+		t.Errorf("Did not get expected output to stdout. Got: %s", slurpOut)
+	}
+}
+
+func TestRunHello3VerboseFinest(t *testing.T) {
+	var cmd *exec.Cmd
+
+	if testing.Short() { // don't run if running quick tests only. (Used primarily so GitHub doesn't run and bork)
+		t.Skip()
+	}
+
+	// test that executable exists
+	if _, err := os.Stat(_jACOBIN); err != nil {
+		t.Errorf("Missing Jacobin executable, which was specified as %s", _jACOBIN)
+	}
+
+	_jVM_ARGS = "-verbose:finest"
+	// run the various combinations of args. This is necessary b/c the empty string is viewed as
+	// an actual specified option on the command line.
+	if len(_jVM_ARGS) > 0 {
+		if len(_aPP_ARGS) > 0 {
+			cmd = exec.Command(_jACOBIN, _jVM_ARGS, _tESTCLASS, _aPP_ARGS)
+		} else {
+			cmd = exec.Command(_jACOBIN, _jVM_ARGS, _tESTCLASS)
+		}
+	} else {
+		if len(_aPP_ARGS) > 0 {
+			cmd = exec.Command(_jACOBIN, _tESTCLASS, _aPP_ARGS)
+		} else {
+			cmd = exec.Command(_jACOBIN, _tESTCLASS)
+		}
+	}
+
+	// get the stdout and stderr contents from the file execution
+	stderr, err := cmd.StderrPipe()
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// run the command
+	if err = cmd.Start(); err != nil {
+		t.Errorf("Got error running Jacobin: %s", err.Error())
+	}
+
+	// Here begin the actual tests on the output to stderr and stdout
+	slurp, _ := io.ReadAll(stderr)
+	slurpErr := string(slurp)
+	if !strings.Contains(slurpErr, "Class Hello3 has been format-checked.") {
+		t.Errorf("Got unexpected output to stderr: %s", slurpErr)
+	}
+
+	slurp, _ = io.ReadAll(stdout)
+	slurpOut := string(slurp)
+	if !strings.HasPrefix(slurpOut, "Jacobin VM") {
+		t.Errorf("Stdout did not begin with Jacobin copyright, instead: %s", slurpOut)
+	}
+
+	if !strings.Contains(slurpOut, "21") {
+		t.Errorf("Did not get expected output to stdout. Got: %s", slurpOut)
+	}
+}
+
+func TestRunHello3TraceInst(t *testing.T) {
+	var cmd *exec.Cmd
+
+	if testing.Short() { // don't run if running quick tests only. (Used primarily so GitHub doesn't run and bork)
+		t.Skip()
+	}
+
+	// test that executable exists
+	if _, err := os.Stat(_jACOBIN); err != nil {
+		t.Errorf("Missing Jacobin executable, which was specified as %s", _jACOBIN)
+	}
+
+	_jVM_ARGS = "-trace:inst"
+	// run the various combinations of args. This is necessary b/c the empty string is viewed as
+	// an actual specified option on the command line.
+	if len(_jVM_ARGS) > 0 {
+		if len(_aPP_ARGS) > 0 {
+			cmd = exec.Command(_jACOBIN, _jVM_ARGS, _tESTCLASS, _aPP_ARGS)
+		} else {
+			cmd = exec.Command(_jACOBIN, _jVM_ARGS, _tESTCLASS)
+		}
+	} else {
+		if len(_aPP_ARGS) > 0 {
+			cmd = exec.Command(_jACOBIN, _tESTCLASS, _aPP_ARGS)
+		} else {
+			cmd = exec.Command(_jACOBIN, _tESTCLASS)
+		}
+	}
+
+	// get the stdout and stderr contents from the file execution
+	stderr, err := cmd.StderrPipe()
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// run the command
+	if err = cmd.Start(); err != nil {
+		t.Errorf("Got error running Jacobin: %s", err.Error())
+	}
+
+	// Here begin the actual tests on the output to stderr and stdout
+	slurp, _ := io.ReadAll(stderr)
+	slurpErr := string(slurp)
+	if !strings.Contains(slurpErr, "class: Hello3, meth: main, pc: 29, inst: RETURN, tos: -1") {
+		t.Errorf("Got unexpected output to stderr: %s", slurpErr)
+	}
+
+	slurp, _ = io.ReadAll(stdout)
+	slurpOut := string(slurp)
+	if !strings.HasPrefix(slurpOut, "Jacobin VM") {
+		t.Errorf("Stdout did not begin with Jacobin copyright, instead: %s", slurpOut)
+	}
+
+	if !strings.Contains(slurpOut, "13") {
+		t.Errorf("Did not get expected output to stdout. Got: %s", slurpOut)
+	}
+}
