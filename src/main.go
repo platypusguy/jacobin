298c288736115a03ddd3a9894b42b24434ab38aa
platypusguy <920630+platypusguy@users.noreply.github.com>
2022-01-20 15:22:17 -0800

    JACOBIN-143 Updated copyright.

--- src/main.go
+++ src/main.go
@@ -1,5 +1,5 @@
 /* Jacobin VM -- A Java virtual machine
- * © Copyright 2021 by Andrew Binstock. All rights reserved
+ * © Copyright 2021-2 by Andrew Binstock. All rights reserved
  * Licensed under Mozilla Public License 2.0 (MPL-2.0)
  */
 
@@ -13,8 +13,6 @@ import (
 )
 
 var Global globals.Globals
-
-// Global := globals.Globals{}
 
 // where everything begins
 func main() {
