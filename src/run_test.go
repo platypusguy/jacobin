ae0a5da531c7d83b7a3b33e446333a859a1c6b60
platypusguy <920630+platypusguy@users.noreply.github.com>
2022-02-08 18:28:24 -0800

    JACOBIN-157 Added processing of if_icmpeq bytecode (with tests)

--- src/run_test.go
+++ src/run_test.go
@@ -434,101 +434,22 @@ func TestIadd(t *testing.T) {
 	}
 }
 
-func TestIconstN1(t *testing.T) {
-	f := newFrame(ICONST_N1)
-	fs := createFrameStack()
-	fs.PushFront(&f) // push the new frame
-	_ = runFrame(fs)
-	if f.tos != 0 {
-		t.Errorf("Top of stack, expected 0, got: %d", f.tos)
-	}
-	value := pop(&f)
-	if value != -1 {
-		t.Errorf("ICONST_N1: Expected popped value to be -1, got: %d", value)
-	}
-}
+// IF_ICMPEQ: jump if val1 == val2 (both ints, both popped off stack)
+func TestIfIcmpeq(t *testing.T) {
+	f := newFrame(IF_ICMPEQ)
+	push(&f, 9) // pushed two equal values, so jump should be made.
+	push(&f, 9)
 
-func TestIconst0(t *testing.T) {
-	f := newFrame(ICONST_0)
+	f.meth = append(f.meth, 0) // where we are jumping to, byte 4 = ICONST2
+	f.meth = append(f.meth, 4)
+	f.meth = append(f.meth, NOP)
+	f.meth = append(f.meth, ICONST_2)
 	fs := createFrameStack()
 	fs.PushFront(&f) // push the new frame
 	_ = runFrame(fs)
-	if f.tos != 0 {
-		t.Errorf("Top of stack, expected 0, got: %d", f.tos)
-	}
-	value := pop(&f)
-	if value != 0 {
-		t.Errorf("ICONST_0: Expected popped value to be 0, got: %d", value)
-	}
-}
-
-func TestIconst1(t *testing.T) {
-	f := newFrame(ICONST_1)
-	fs := createFrameStack()
-	fs.PushFront(&f) // push the new frame
-	_ = runFrame(fs)
-	if f.tos != 0 {
-		t.Errorf("Top of stack, expected 0, got: %d", f.tos)
-	}
-	value := pop(&f)
-	if value != 1 {
-		t.Errorf("ICONST_1: Expected popped value to be 1, got: %d", value)
-	}
-}
-
-func TestIconst2(t *testing.T) {
-	f := newFrame(ICONST_2)
-	fs := createFrameStack()
-	fs.PushFront(&f) // push the new frame
-	_ = runFrame(fs)
-	if f.tos != 0 {
-		t.Errorf("Top of stack, expected 0, got: %d", f.tos)
-	}
-	value := pop(&f)
-	if value != 2 {
-		t.Errorf("ICONST_2: Expected popped value to be 2, got: %d", value)
-	}
-}
-
-func TestIconst3(t *testing.T) {
-	f := newFrame(ICONST_3)
-	fs := createFrameStack()
-	fs.PushFront(&f) // push the new frame
-	_ = runFrame(fs)
-	if f.tos != 0 {
-		t.Errorf("Top of stack, expected 0, got: %d", f.tos)
-	}
-	value := pop(&f)
-	if value != 3 {
-		t.Errorf("ICONST_3: Expected popped value to be 3, got: %d", value)
-	}
-}
-
-func TestIconst4(t *testing.T) {
-	f := newFrame(ICONST_4)
-	fs := createFrameStack()
-	fs.PushFront(&f) // push the new frame
-	_ = runFrame(fs)
-	if f.tos != 0 {
-		t.Errorf("Top of stack, expected 0, got: %d", f.tos)
-	}
-	value := pop(&f)
-	if value != 4 {
-		t.Errorf("ICONST_4: Expected popped value to be 4, got: %d", value)
-	}
-}
-
-func TestIconst5(t *testing.T) {
-	f := newFrame(ICONST_5)
-	fs := createFrameStack()
-	fs.PushFront(&f) // push the new frame
-	_ = runFrame(fs)
-	if f.tos != 0 {
-		t.Errorf("Top of stack, expected 0, got: %d", f.tos)
-	}
-	value := pop(&f)
-	if value != 5 {
-		t.Errorf("ICONST_5: Expected popped value to be 5, got: %d", value)
+	if f.meth[f.pc-1] != ICONST_2 { // -1 b/c the run loop adds 1 before exiting
+		t.Errorf("ICMPEQ: expecting a jump to ICONST_2 instuction, got: %s",
+			BytecodeNames[f.pc])
 	}
 }
 
@@ -681,6 +602,104 @@ func TestIfIcmpltFail(t *testing.T) {
 	if f.meth[f.pc] != RETURN { // b/c we return directly, we don't subtract 1 from pc
 		t.Errorf("ICMPLT: expecting fall-through to RETURN instuction, got: %s",
 			BytecodeNames[f.pc])
+	}
+}
+
+func TestIconstN1(t *testing.T) {
+	f := newFrame(ICONST_N1)
+	fs := createFrameStack()
+	fs.PushFront(&f) // push the new frame
+	_ = runFrame(fs)
+	if f.tos != 0 {
+		t.Errorf("Top of stack, expected 0, got: %d", f.tos)
+	}
+	value := pop(&f)
+	if value != -1 {
+		t.Errorf("ICONST_N1: Expected popped value to be -1, got: %d", value)
+	}
+}
+
+func TestIconst0(t *testing.T) {
+	f := newFrame(ICONST_0)
+	fs := createFrameStack()
+	fs.PushFront(&f) // push the new frame
+	_ = runFrame(fs)
+	if f.tos != 0 {
+		t.Errorf("Top of stack, expected 0, got: %d", f.tos)
+	}
+	value := pop(&f)
+	if value != 0 {
+		t.Errorf("ICONST_0: Expected popped value to be 0, got: %d", value)
+	}
+}
+
+func TestIconst1(t *testing.T) {
+	f := newFrame(ICONST_1)
+	fs := createFrameStack()
+	fs.PushFront(&f) // push the new frame
+	_ = runFrame(fs)
+	if f.tos != 0 {
+		t.Errorf("Top of stack, expected 0, got: %d", f.tos)
+	}
+	value := pop(&f)
+	if value != 1 {
+		t.Errorf("ICONST_1: Expected popped value to be 1, got: %d", value)
+	}
+}
+
+func TestIconst2(t *testing.T) {
+	f := newFrame(ICONST_2)
+	fs := createFrameStack()
+	fs.PushFront(&f) // push the new frame
+	_ = runFrame(fs)
+	if f.tos != 0 {
+		t.Errorf("Top of stack, expected 0, got: %d", f.tos)
+	}
+	value := pop(&f)
+	if value != 2 {
+		t.Errorf("ICONST_2: Expected popped value to be 2, got: %d", value)
+	}
+}
+
+func TestIconst3(t *testing.T) {
+	f := newFrame(ICONST_3)
+	fs := createFrameStack()
+	fs.PushFront(&f) // push the new frame
+	_ = runFrame(fs)
+	if f.tos != 0 {
+		t.Errorf("Top of stack, expected 0, got: %d", f.tos)
+	}
+	value := pop(&f)
+	if value != 3 {
+		t.Errorf("ICONST_3: Expected popped value to be 3, got: %d", value)
+	}
+}
+
+func TestIconst4(t *testing.T) {
+	f := newFrame(ICONST_4)
+	fs := createFrameStack()
+	fs.PushFront(&f) // push the new frame
+	_ = runFrame(fs)
+	if f.tos != 0 {
+		t.Errorf("Top of stack, expected 0, got: %d", f.tos)
+	}
+	value := pop(&f)
+	if value != 4 {
+		t.Errorf("ICONST_4: Expected popped value to be 4, got: %d", value)
+	}
+}
+
+func TestIconst5(t *testing.T) {
+	f := newFrame(ICONST_5)
+	fs := createFrameStack()
+	fs.PushFront(&f) // push the new frame
+	_ = runFrame(fs)
+	if f.tos != 0 {
+		t.Errorf("Top of stack, expected 0, got: %d", f.tos)
+	}
+	value := pop(&f)
+	if value != 5 {
+		t.Errorf("ICONST_5: Expected popped value to be 5, got: %d", value)
 	}
 }
 
