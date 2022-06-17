e856e7d3fba47db740a58bc5a9b8ac084045f02b
Spencer Uresk <suresk@gmail.com>
2022-06-17 17:21:13 -0600

    Added test for no manifest. Also changed the way pipes get buffered so it doesn't hang

--- src/jvm/jvmStart_test.go
+++ src/jvm/jvmStart_test.go
@@ -7,10 +7,12 @@
 package jvm
 
 import (
-	"io/ioutil"
+	"bytes"
+	"io"
 	"jacobin/globals"
 	"jacobin/log"
 	"os"
+	"path/filepath"
 	"strings"
 	"testing"
 )
@@ -22,12 +24,20 @@ func TestNoExecutable(t *testing.T) {
 	g.StrictJDK = false
 
 	log.Init()
-	_ = log.SetLogLevel(log.FINE)
+	_ = log.SetLogLevel(log.INFO)
 
 	// redirect stderr & stdout to capture results from stderr
 	normalStderr := os.Stderr
 	r, w, _ := os.Pipe()
 	os.Stderr = w
+
+	errC := make(chan string)
+
+	go func() {
+		var buf bytes.Buffer
+		io.Copy(&buf, r)
+		errC <- buf.String()
+	}()
 
 	normalStdout := os.Stdout
 	_, wout, _ := os.Pipe()
@@ -37,10 +47,9 @@ func TestNoExecutable(t *testing.T) {
 
 	// restore stderr and stdout to what they were before
 	_ = w.Close()
-	out, _ := ioutil.ReadAll(r)
 	os.Stderr = normalStderr
 
-	errMsg := string(out[:])
+	errMsg := <-errC
 
 	_ = wout.Close()
 	os.Stdout = normalStdout
@@ -51,5 +60,51 @@ func TestNoExecutable(t *testing.T) {
 }
 
 func TestNoMainClassInJar(t *testing.T) {
-	
+	cwd, err := os.Getwd()
+
+	if err != nil {
+		t.Error("Error getting current working directory")
+		return
+	}
+
+	g := globals.GetGlobalRef()
+	globals.InitGlobals("test")
+	g.JacobinName = "test" // prevents a shutdown when the exception hits.
+	g.StartingJar = filepath.Join(cwd, "..", "..", "testdata", "nomanifest.jar")
+	g.StrictJDK = false
+
+	log.Init()
+	_ = log.SetLogLevel(log.INFO)
+
+	// redirect stderr & stdout to capture results from stderr
+	normalStderr := os.Stderr
+	r, w, _ := os.Pipe()
+	os.Stderr = w
+
+	errC := make(chan string)
+
+	go func() {
+		var buf bytes.Buffer
+		io.Copy(&buf, r)
+		errC <- buf.String()
+	}()
+
+	normalStdout := os.Stdout
+	_, wout, _ := os.Pipe()
+	os.Stdout = wout
+
+	JVMrun()
+
+	// restore stderr and stdout to what they were before
+	_ = w.Close()
+	os.Stderr = normalStderr
+
+	errMsg := <-errC
+
+	_ = wout.Close()
+	os.Stdout = normalStdout
+
+	if !strings.Contains(errMsg, "no main manifest attribute") {
+		t.Errorf("jvmRun() with a jar that has no manifest should have given no main manifest attribute error, got %s", errMsg)
+	}
 }
