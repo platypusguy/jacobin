b0d22e4532a92e14da133367cc5ba9f689df3e97
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2022-07-03 23:54:22 -0700

    JACOBIN-138 Added tests to util package

---
+++ src/util/execUtilities_test.go
@@ -0,0 +1,29 @@
+/*
+ * Jacobin VM - A Java virtual machine
+ * Copyright (c) 2022 by Andrew Binstock. All rights reserved.
+ * Licensed under Mozilla Public License 2.0 (MPL 2.0)
+ */
+
+package util
+
+import (
+	"testing"
+)
+
+// verify that a trailing slash in JAVA_HOME is removed
+func TestParseIncomingParamsFromMethType(t *testing.T) {
+	res1 := ParseIncomingParamsFromMethTypeString("(SBI)")
+	if string(res1) != "III" {
+		t.Errorf("Expected parse would return 3 values of 'I', got: %s", string(res1))
+	}
+
+	res2 := ParseIncomingParamsFromMethTypeString("FJDL")
+	if string(res2) != "FJDL" {
+		t.Errorf("Expected parse would return \"FJDL\", got: %s", string(res2))
+	}
+
+	res3 := ParseIncomingParamsFromMethTypeString("[[")
+	if string(res3) != "LL" {
+		t.Errorf("Expected parse would return value of \"LL\", got: %s", string(res3))
+	}
+}
