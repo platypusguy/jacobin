8f696d34649710e65865029d490a6c1d440fc2d7
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2022-11-27 15:26:33 -0800

    JACOBIN-199  Finish utility function to retrieve a specific CP entry for the runtime.

--- src/jvm/runTimeUtils.go
+++ src/jvm/runTimeUtils.go
@@ -15,20 +15,19 @@ import (
 
 // Look up an entry in a CP and return its type and its value.
 // The three return fields are:
-//  1. The CP entry type. If it's a Dummy (0), this indidates that
-//     an error occurred, as we would never look up a dummy entry.
+//  1. The CP entry type. Equals 0 if an error occurred.
 //  2. an int that identifies the type of the returned value. The
 //     options are:
 //     0 = error
-//     1 = int64 or address
+//     1 = address of item other than string
 //     2 = float64
-//     3 = a string address
+//     3 = int64
+//     4 = a string address
 //  3. the value itself
 func FetchCPentry(cpp *classloader.CPool, index int) (uint16, int, any) {
 	if cpp == nil {
 		return classloader.Dummy, 0, math.NaN()
 	}
-
 	cp := *cpp
 	// if index is out of range, return error
 	if index < 1 || index >= len(cp.CpIndex) {
@@ -41,13 +40,13 @@ func FetchCPentry(cpp *classloader.CPool, index int) (uint16, int, any) {
 	// integers
 	case classloader.IntConst:
 		retInt := int64(cp.IntConsts[entry.Slot])
-		return entry.Type, 1, retInt
+		return entry.Type, 3, retInt
 	case classloader.LongConst:
 		retInt := cp.LongConsts[entry.Slot]
-		return entry.Type, 1, retInt
+		return entry.Type, 3, retInt
 
 	case classloader.MethodType:
-		return entry.Type, 1, int64(cp.MethodTypes[entry.Slot])
+		return entry.Type, 3, int64(cp.MethodTypes[entry.Slot])
 
 	// floating point
 	case classloader.FloatConst:
@@ -57,10 +56,14 @@ func FetchCPentry(cpp *classloader.CPool, index int) (uint16, int, any) {
 		retFloat := cp.Doubles[entry.Slot]
 		return entry.Type, 2, retFloat
 
-	// addresses and references
+	// addresses of strings
 	case classloader.ClassRef: // points to a UTF-8 string
-		return entry.Type, 3, &(cp.Utf8Refs[entry.Slot])
+		return entry.Type, 4, &(cp.Utf8Refs[entry.Slot])
 
+	case classloader.UTF8:
+		return entry.Type, 4, &(cp.Utf8Refs[entry.Slot])
+
+	// addresses of structures or other elements
 	case classloader.Dynamic:
 		return entry.Type, 1, &(cp.Dynamics[entry.Slot])
 
@@ -79,10 +82,12 @@ func FetchCPentry(cpp *classloader.CPool, index int) (uint16, int, any) {
 	case classloader.NameAndType:
 		return entry.Type, 1, &(cp.NameAndTypes[entry.Slot])
 
-	// error: name of module would not normally be retrieved here
-	case classloader.Module, classloader.Package:
+	// error: name of module or package would
+	// not normally be retrieved here
+	case classloader.Module,
+		classloader.Package:
 		return 0, 0, math.NaN()
 	}
 
-	return classloader.Dummy, 0, math.NaN()
+	return 0, 0, math.NaN()
 }
