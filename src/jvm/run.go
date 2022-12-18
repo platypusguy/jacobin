2cfb7becd513f80a6f972e056ee04b6416b19693
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2022-12-17 22:58:11 -0800

    Slight simplification to INEG implementation.

--- src/jvm/run.go
+++ src/jvm/run.go
@@ -510,11 +510,12 @@ func runFrame(fs *list.List) error {
 		case IFEQ: // 0x99 pop int, if it's == 0, go to the jump location
 			// specified in the next two bytes
 			value := pop(f).(int64)
-			jumpTo := (int16(f.Meth[f.PC+1]) * 256) + int16(f.Meth[f.PC+2])
 			if value == 0 {
+				jumpTo := (int16(f.Meth[f.PC+1]) * 256) + int16(f.Meth[f.PC+2])
 				f.PC = f.PC + int(jumpTo) - 1
+			} else {
+				f.PC += 2
 			}
-			f.PC += 2
 		case IF_ICMPEQ: //  0x9F 	(jump if top two ints are equal)
 			val2 := pop(f).(int64)
 			val1 := pop(f).(int64)
