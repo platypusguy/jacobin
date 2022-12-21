f764c47e1f149ed81ddb84fb4da47dd6e5285c33
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2022-12-21 11:56:11 -0800

    JACOBIN-200 Added tests for IFGE handling

--- src/jvm/run_test.go
+++ src/jvm/run_test.go
@@ -902,6 +902,42 @@ func TestIfeqFallThrough(t *testing.T) {
 	}
 }
 
+// IFGE: jump if int popped off TOS is >= 0
+func TestIfge(t *testing.T) {
+	f := newFrame(IFGE)
+	push(&f, int64(66)) // pushed 66, so jump should be made.
+
+	f.Meth = append(f.Meth, 0) // where we are jumping to, byte 4 = ICONST2
+	f.Meth = append(f.Meth, 4)
+	f.Meth = append(f.Meth, NOP)
+	f.Meth = append(f.Meth, ICONST_2)
+	fs := frames.CreateFrameStack()
+	fs.PushFront(&f) // push the new frame
+	_ = runFrame(fs)
+	if f.Meth[f.PC-1] != ICONST_2 { // -1 b/c the run loop adds 1 before exiting
+		t.Errorf("IFGE: expecting a jump to ICONST_2 instuction, got: %s",
+			BytecodeNames[f.PC])
+	}
+}
+
+// IFGE: jump if int popped off TOS is >= 0; here < 0
+func TestIfgeFallThrough(t *testing.T) {
+	f := newFrame(IFGE)
+	push(&f, int64(-1)) // pushed -1, so jump should not be made.
+
+	f.Meth = append(f.Meth, 0) // where we are jumping to, byte 4 = ICONST2
+	f.Meth = append(f.Meth, 4)
+	f.Meth = append(f.Meth, RETURN)
+	f.Meth = append(f.Meth, ICONST_2)
+	fs := frames.CreateFrameStack()
+	fs.PushFront(&f) // push the new frame
+	_ = runFrame(fs)
+	if f.Meth[f.PC-1] == ICONST_2 { // -1 b/c the run loop adds 1 before exiting
+		t.Errorf("IFGE: Invalid fall-through, got: %s",
+			BytecodeNames[f.PC])
+	}
+}
+
 // IFNE: jump if int popped off TOS is != 0
 func TestIfne(t *testing.T) {
 	f := newFrame(IFNE)
