10dbffbec21e968c11d5f1474db6d66882e7aff1
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2022-12-21 12:16:02 -0800

    JACOBIN-200 Added tests for IFLT handling

--- src/jvm/run_test.go
+++ src/jvm/run_test.go
@@ -977,7 +977,7 @@ func TestIfgt(t *testing.T) {
 // IFGT: jump if int popped off TOS is > 0; here = 0
 func TestIfgtFallThrough(t *testing.T) {
 	f := newFrame(IFGT)
-	push(&f, int64(0)) // pushed -1, so jump should not be made.
+	push(&f, int64(0)) // pushed 0, so jump should not be made.
 
 	f.Meth = append(f.Meth, 0)
 	f.Meth = append(f.Meth, 4)
@@ -988,6 +988,42 @@ func TestIfgtFallThrough(t *testing.T) {
 	_ = runFrame(fs)
 	if f.Meth[f.PC-1] == ICONST_2 { // -1 b/c the run loop adds 1 before exiting
 		t.Errorf("IFGT: Invalid fall-through, got: %s",
+			BytecodeNames[f.PC])
+	}
+}
+
+// IFLT: jump if int popped off TOS is > 0
+func TestIflt(t *testing.T) {
+	f := newFrame(IFLT)
+	push(&f, int64(-66)) // pushed -66, so jump should be made.
+
+	f.Meth = append(f.Meth, 0) // where we are jumping to, byte 4 = ICONST2
+	f.Meth = append(f.Meth, 4)
+	f.Meth = append(f.Meth, NOP)
+	f.Meth = append(f.Meth, ICONST_2)
+	fs := frames.CreateFrameStack()
+	fs.PushFront(&f) // push the new frame
+	_ = runFrame(fs)
+	if f.Meth[f.PC-1] != ICONST_2 { // -1 b/c the run loop adds 1 before exiting
+		t.Errorf("IFLT: expecting a jump to ICONST_2 instuction, got: %s",
+			BytecodeNames[f.PC])
+	}
+}
+
+// IFLT: jump if int popped off TOS is < 0; here = 0
+func TestIfltFallThrough(t *testing.T) {
+	f := newFrame(IFLT)
+	push(&f, int64(0)) // pushed 0, so jump should not be made.
+
+	f.Meth = append(f.Meth, 0)
+	f.Meth = append(f.Meth, 4)
+	f.Meth = append(f.Meth, RETURN)
+	f.Meth = append(f.Meth, ICONST_2)
+	fs := frames.CreateFrameStack()
+	fs.PushFront(&f) // push the new frame
+	_ = runFrame(fs)
+	if f.Meth[f.PC-1] == ICONST_2 { // -1 b/c the run loop adds 1 before exiting
+		t.Errorf("IFLT: Invalid fall-through, got: %s",
 			BytecodeNames[f.PC])
 	}
 }
