33529a20ef1a46f67e558aaa6b8f57ef8f515526
Spencer Uresk <suresk@gmail.com>
2022-12-29 18:25:06 -0700

    Initial double ops

--- src/jvm/run.go
+++ src/jvm/run.go
@@ -349,6 +349,18 @@ func runFrame(fs *list.List) error {
 			f.Locals[2] = pop(f).(float64)
 		case FSTORE_3: // 0x46
 			f.Locals[3] = pop(f).(float64)
+		case DSTORE_0: // 0x47
+			pop(f)
+			f.Locals[0] = pop(f).(float64)
+		case DSTORE_1: // 0x48
+			pop(f)
+			f.Locals[1] = pop(f).(float64)
+		case DSTORE_2: // 0x49
+			pop(f)
+			f.Locals[2] = pop(f).(float64)
+		case DSTORE_3: // 0x4A
+			pop(f)
+			f.Locals[3] = pop(f).(float64)
 		case ASTORE_0: //	0x4B	(pop reference into local variable 0)
 			f.Locals[0] = pop(f).(int64)
 		case ASTORE_1: //   0x4C	(pop reference into local variable 1)
@@ -392,6 +404,14 @@ func runFrame(fs *list.List) error {
 			lhs := float32(pop(f).(float64))
 			rhs := float32(pop(f).(float64))
 			push(f, float64(lhs+rhs))
+		case DADD: // 0x63
+			lhs := pop(f).(float64)
+			pop(f)
+			rhs := pop(f).(float64)
+			pop(f)
+			res := add(lhs, rhs)
+			push(f, res)
+			push(f, res)
 		case ISUB: //  0x64	(subtract top 2 integers on operand stack, push result)
 			i2 := pop(f).(int64)
 			i1 := pop(f).(int64)
@@ -429,6 +449,14 @@ func runFrame(fs *list.List) error {
 			val1 := float32(pop(f).(float64))
 			val2 := float32(pop(f).(float64))
 			push(f, float64(val1*val2))
+		case DMUL: // 0x6B
+			val1 := pop(f).(float64)
+			pop(f)
+			val2 := pop(f).(float64)
+			pop(f)
+			res := multiply(val1, val2)
+			push(f, res)
+			push(f, res)
 		case IDIV: //  0x6C (integer divide tos-1 by tos)
 			val1 := pop(f).(int64)
 			if val1 == 0 {
@@ -464,6 +492,24 @@ func runFrame(fs *list.List) error {
 				}
 			} else {
 				push(f, float64(float32(val2)/float32(val1)))
+			}
+		case DDIV: // 0x6F
+			val1 := pop(f).(float64)
+			pop(f)
+			val2 := pop(f).(float64)
+			pop(f)
+			if val1 == 0.0 {
+				if val2 == 0.0 {
+					push(f, math.NaN())
+				} else if math.Signbit(val1) {
+					push(f, math.Inf(1))
+				} else {
+					push(f, math.Inf(-1))
+				}
+			} else {
+				res := val2 / val1
+				push(f, res)
+				push(f, res)
 			}
 		case LREM: // 	0x71	(remainder after long division)
 			val2 := pop(f).(int64)
@@ -580,6 +626,9 @@ func runFrame(fs *list.List) error {
 			dblVal := float64(longVal)
 			push(f, dblVal)
 			push(f, dblVal)
+		case D2I: // 0xBE
+			pop(f)
+			fallthrough
 		case F2I: // 0x8B
 			floatVal := pop(f).(float64)
 			push(f, int64(math.Trunc(floatVal)))
@@ -587,11 +636,18 @@ func runFrame(fs *list.List) error {
 			floatVal := pop(f).(float64)
 			push(f, floatVal)
 			push(f, floatVal)
-		case F2L: // 	0x8C convert float to long
+		case D2L: // 0x8F convert double to long
+			pop(f)
+			fallthrough
+		case F2L: // 0x8C convert float to long
 			floatVal := pop(f).(float64)
 			truncated := int64(math.Trunc(floatVal))
 			push(f, truncated)
 			push(f, truncated)
+		case D2F: // 0x90 Double to float
+			floatVal := float32(pop(f).(float64))
+			pop(f)
+			push(f, float64(floatVal))
 		case I2S: //	0x93 convert int to short
 			intVal := pop(f).(int64)
 			shortVal := int32(intVal)
@@ -607,6 +663,25 @@ func runFrame(fs *list.List) error {
 				push(f, int64(1))
 			} else {
 				push(f, int64(-1))
+			}
+		case DCMPL, DCMPG: // 0x98, 0x97 - double comparison - they only differ in NaN treatment
+			value2 := pop(f).(float64)
+			pop(f)
+			value1 := pop(f).(float64)
+			pop(f)
+
+			if math.IsNaN(value1) || math.IsNaN(value2) {
+				if f.Meth[f.PC] == DCMPG {
+					push(f, int64(1))
+				} else {
+					push(f, int64(-1))
+				}
+			} else if value1 > value2 {
+				push(f, int64(1))
+			} else if value1 < value2 {
+				push(f, int64(-1))
+			} else {
+				push(f, int64(0))
 			}
 		case IFEQ: // 0x99 pop int, if it's == 0, go to the jump location
 			// specified in the next two bytes
--- src/jvm/run_test.go
+++ src/jvm/run_test.go
@@ -36,6 +36,12 @@ var zerof = float64(0)
 
 var maxFloatDiff = .00001
 
+func validateFloatingPoint(t *testing.T, op string, expected float64, actual float64) {
+	if math.Abs(expected-actual) > maxFloatDiff {
+		t.Errorf("%s: expected a result of %f, but got: %f", op, expected, actual)
+	}
+}
+
 // ---- tests ----
 
 // test load of reference in locals[index] on to stack
@@ -254,6 +260,191 @@ func TestBipush(t *testing.T) {
 	}
 }
 
+// D2F: test convert double to float
+func TestD2f(t *testing.T) {
+	f := newFrame(D2F)
+	push(&f, 2.9)
+	push(&f, 2.9)
+
+	fs := frames.CreateFrameStack()
+	fs.PushFront(&f) // push the new frame
+	_ = runFrame(fs)
+
+	val := pop(&f).(float64)
+	if math.Abs(val-2.9) > maxFloatDiff {
+		t.Errorf("D2F: expected a result of 2.9, but got: %f", val)
+	}
+	if f.TOS != -1 {
+		t.Errorf("D2F: Expected stack with 0 items, but got a TOS of: %d", f.TOS)
+	}
+}
+
+// D2I: test convert double to int, positive
+func TestD2iPositive(t *testing.T) {
+	f := newFrame(D2I)
+	push(&f, 2.9)
+	push(&f, 2.9)
+
+	fs := frames.CreateFrameStack()
+	fs.PushFront(&f) // push the new frame
+	_ = runFrame(fs)
+
+	val := pop(&f).(int64)
+	if val != 2 {
+		t.Errorf("D2I: expected a result of 2, but got: %d", val)
+	}
+	if f.TOS != -1 {
+		t.Errorf("D2I: Expected stack with 0 items, but got a TOS of: %d", f.TOS)
+	}
+}
+
+// D2I: test convert double to int, negative
+func TestD2iNegative(t *testing.T) {
+	f := newFrame(D2I)
+	push(&f, -2.9)
+	push(&f, -2.9)
+
+	fs := frames.CreateFrameStack()
+	fs.PushFront(&f) // push the new frame
+	_ = runFrame(fs)
+
+	val := pop(&f).(int64)
+	if val != -2 {
+		t.Errorf("D2I: expected a result of -2, but got: %d", val)
+	}
+	if f.TOS != -1 {
+		t.Errorf("D2I: Expected stack with 0 items, but got a TOS of: %d", f.TOS)
+	}
+}
+
+// D2L: test convert double to long, positive
+func TestD2lPositive(t *testing.T) {
+	f := newFrame(D2L)
+	push(&f, 2.9)
+	push(&f, 2.9)
+
+	fs := frames.CreateFrameStack()
+	fs.PushFront(&f) // push the new frame
+	_ = runFrame(fs)
+
+	pop(&f)
+	val := pop(&f).(int64)
+	if val != 2 {
+		t.Errorf("D2L: expected a result of 2, but got: %d", val)
+	}
+	if f.TOS != -1 {
+		t.Errorf("D2L: Expected stack with 0 items, but got a TOS of: %d", f.TOS)
+	}
+}
+
+// D2L: test convert double to long, negative
+func TestD2lNegative(t *testing.T) {
+	f := newFrame(D2L)
+	push(&f, -2.9)
+	push(&f, -2.9)
+
+	fs := frames.CreateFrameStack()
+	fs.PushFront(&f) // push the new frame
+	_ = runFrame(fs)
+
+	pop(&f)
+	val := pop(&f).(int64)
+	if val != -2 {
+		t.Errorf("D2L: expected a result of -2, but got: %d", val)
+	}
+	if f.TOS != -1 {
+		t.Errorf("D2L: Expected stack with 0 items, but got a TOS of: %d", f.TOS)
+	}
+}
+
+// DADD: test add two doubles
+func TestDadd(t *testing.T) {
+	f := newFrame(DADD)
+	push(&f, 15.3)
+	push(&f, 15.3)
+	push(&f, 22.1)
+	push(&f, 22.1)
+
+	fs := frames.CreateFrameStack()
+	fs.PushFront(&f)
+	_ = runFrame(fs)
+
+	pop(&f)
+	val := pop(&f).(float64)
+	validateFloatingPoint(t, "DADD", 37.4, val)
+	if f.TOS != -1 {
+		t.Errorf("DADD: Expected stack with 0 items, but got a TOS of: %d", f.TOS)
+	}
+}
+
+// DADD: test add two doubles, one is NaN
+func TestDaddNan(t *testing.T) {
+	f := newFrame(DADD)
+	push(&f, 15.3)
+	push(&f, 15.3)
+	push(&f, math.NaN())
+	push(&f, math.NaN())
+
+	fs := frames.CreateFrameStack()
+	fs.PushFront(&f)
+	_ = runFrame(fs)
+
+	pop(&f)
+	val := pop(&f).(float64)
+
+	if !math.IsNaN(val) {
+		t.Errorf("DADD: Expected NaN, got: %f", val)
+	}
+
+	if f.TOS != -1 {
+		t.Errorf("DADD: Expected stack with 0 items, but got a TOS of: %d", f.TOS)
+	}
+}
+
+// DADD: test add two doubles, one is Inf
+func TestDaddInf(t *testing.T) {
+	f := newFrame(DADD)
+	push(&f, 15.3)
+	push(&f, 15.3)
+	push(&f, math.Inf(1))
+	push(&f, math.Inf(1))
+
+	fs := frames.CreateFrameStack()
+	fs.PushFront(&f)
+	_ = runFrame(fs)
+
+	pop(&f)
+	val := pop(&f).(float64)
+
+	if !math.IsInf(val, 1) {
+		t.Errorf("DADD: Expected Inf, got: %f", val)
+	}
+
+	if f.TOS != -1 {
+		t.Errorf("DADD: Expected stack with 0 items, but got a TOS of: %d", f.TOS)
+	}
+}
+
+// DDIV: double divide of.TOS-1 by tos, push result
+func TestDdiv(t *testing.T) {
+	f := newFrame(DDIV)
+	push(&f, 3.0)
+	push(&f, 3.0)
+	push(&f, 2.0)
+	push(&f, 2.0)
+
+	fs := frames.CreateFrameStack()
+	fs.PushFront(&f)
+	_ = runFrame(fs)
+
+	pop(&f)
+	value := pop(&f).(float64)
+	validateFloatingPoint(t, "DDIV", 1.5, value)
+	if f.TOS != -1 {
+		t.Errorf("DDIV: Expected stack with 0 items, but got a TOS of: %d", f.TOS)
+	}
+}
+
 // DLOAD: test load of double in locals[index] on to stack
 func TestDload(t *testing.T) {
 	f := newFrame(DLOAD)
@@ -277,6 +468,104 @@ func TestDload(t *testing.T) {
 	}
 	if f.PC != 2 {
 		t.Errorf("DLOAD: Expected pc to be pointing at byte 2, got: %d", f.PC)
+	}
+}
+
+// DLOAD_0: load of double in locals[0] onto stack
+func TestDload0(t *testing.T) {
+	f := newFrame(DLOAD_0)
+	f.Locals = append(f.Locals, 1.2)
+
+	fs := frames.CreateFrameStack()
+	fs.PushFront(&f) // push the new frame
+	_ = runFrame(fs)
+
+	pop(&f)
+	value := pop(&f).(float64)
+	validateFloatingPoint(t, "DLOAD_0", 1.2, value)
+
+	if f.TOS != -1 {
+		t.Errorf("DLOAD_0: Expected empty stack, got: %d", f.TOS)
+	}
+}
+
+// DLOAD_1: load of double in locals[1] onto stack
+func TestDload1(t *testing.T) {
+	f := newFrame(DLOAD_1)
+	f.Locals = append(f.Locals, 1.3)
+	f.Locals = append(f.Locals, 1.2)
+
+	fs := frames.CreateFrameStack()
+	fs.PushFront(&f) // push the new frame
+	_ = runFrame(fs)
+
+	pop(&f)
+	value := pop(&f).(float64)
+	validateFloatingPoint(t, "DLOAD_1", 1.2, value)
+
+	if f.TOS != -1 {
+		t.Errorf("DLOAD_1: Expected empty stack, got: %d", f.TOS)
+	}
+}
+
+// DLOAD_2: load of double in locals[2] onto stack
+func TestDload2(t *testing.T) {
+	f := newFrame(DLOAD_2)
+	f.Locals = append(f.Locals, 1.3)
+	f.Locals = append(f.Locals, 1.3)
+	f.Locals = append(f.Locals, 1.2)
+
+	fs := frames.CreateFrameStack()
+	fs.PushFront(&f) // push the new frame
+	_ = runFrame(fs)
+
+	pop(&f)
+	value := pop(&f).(float64)
+	validateFloatingPoint(t, "DLOAD_2", 1.2, value)
+
+	if f.TOS != -1 {
+		t.Errorf("DLOAD_2: Expected empty stack, got: %d", f.TOS)
+	}
+}
+
+// DLOAD_3: load of double in locals[3] onto stack
+func TestDload3(t *testing.T) {
+	f := newFrame(DLOAD_3)
+	f.Locals = append(f.Locals, 1.3)
+	f.Locals = append(f.Locals, 1.3)
+	f.Locals = append(f.Locals, 1.3)
+	f.Locals = append(f.Locals, 1.2)
+
+	fs := frames.CreateFrameStack()
+	fs.PushFront(&f) // push the new frame
+	_ = runFrame(fs)
+
+	pop(&f)
+	value := pop(&f).(float64)
+	validateFloatingPoint(t, "DLOAD_3", 1.2, value)
+
+	if f.TOS != -1 {
+		t.Errorf("DLOAD_3: Expected empty stack, got: %d", f.TOS)
+	}
+}
+
+// Test DMUL (pop 2 doubles, multiply them, push result)
+func TestDmul(t *testing.T) {
+	f := newFrame(DMUL)
+	push(&f, 1.5)
+	push(&f, 1.5)
+	push(&f, 2.0)
+	push(&f, 2.0)
+
+	fs := frames.CreateFrameStack()
+	fs.PushFront(&f) // push the new frame
+	_ = runFrame(fs)
+
+	pop(&f)
+	validateFloatingPoint(t, "DMUL", 3.0, pop(&f).(float64))
+
+	if f.TOS != -1 {
+		t.Errorf("FMUL, Top of stack, expected 0, got: %d", f.TOS)
 	}
 }
 
