863c887b626a8ca939fb6eace08d03e3275cf392
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2023-01-10 17:58:56 -0800

    JACOBIN-203 Continued initial work on arrays.

--- src/jvm/arrays.go
+++ src/jvm/arrays.go
@@ -35,6 +35,19 @@ const (
 	BYTE  = iota
 )
 
+// the primitive types as specified in the
+// JVM instructions // for arrays
+const (
+	T_BOOLEAN = 4
+	T_CHAR    = 5
+	T_FLOAT   = 6
+	T_DOUBLE  = 7
+	T_BYTE    = 8
+	T_SHORT   = 9
+	T_INT     = 10
+	T_LONG    = 11
+)
+
 type ArrType interface {
 	int64 | byte | float64
 }
@@ -42,4 +55,20 @@ type ArrType interface {
 type ArrayHolder[T ArrType] struct {
 	Type ArrayType
 	Arr  []T
+}
+
+// converts one the of the JDK values indicating the primitive
+// used in the elements of an array into one of the values used
+// by Jacobin in array creation. Returns zero on error.
+func jdkArrayTypeToJacobinType(jdkType byte) int {
+	switch jdkType {
+	case T_BOOLEAN, T_BYTE:
+		return BYTE
+	case T_CHAR, T_SHORT, T_INT, T_LONG:
+		return INT
+	case T_FLOAT, T_DOUBLE:
+		return FLOAT
+	default: // this would indicate an error
+		return 0
+	}
 }
