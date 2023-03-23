6f5521ed5771447d1e5310e0fab8cb2808f7ac80
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2023-03-22 22:43:14 -0700

    JACOBIN-219 Added regression test for array lengths using @texactyl's Java class. Placed in wholeClassTests

---
+++ src/wholeClassTests/ArrayLength_test.go
@@ -0,0 +1,134 @@
+/*
+ * Jacobin VM - A Java virtual machine
+ * Copyright (c) 2022 by the Jacobin authors. All rights reserved.
+ * Licensed under Mozilla Public License 2.0 (MPL 2.0)
+ */
+
+package wholeClassTests
+
+import (
+    "fmt"
+    "io"
+    "log"
+    "os"
+    "os/exec"
+    "strings"
+    "testing"
+)
+
+/* Regression test for JACOBIN-219:
+ * Tests for arrlen.class, which tests the length of primitive and reference arrays
+ * public class arrlen {
+ *
+ *   public static void main(String args[]) {
+ *       System.out.println("Begin");
+ *
+ *      int[] arr_ints = new int[3];
+ *      int size1 = arr_ints.length;
+ *      System.out.print("arr_ints length: ");
+ *      System.out.println(size1);
+ *
+ *      String[] arr_strings = new String[3];
+ *      int size2 = arr_strings.length;
+ *      System.out.print("arr_strings length: ");
+ *      System.out.println(size2);
+ *
+ *      System.out.println("End");
+ *   }
+ * }
+ *
+ * When working correctly, the output should be:
+ *       Begin
+ *       arr_ints length: 3
+ *       arr_strings length: 3
+ *       End
+ */
+
+// To run your class, enter its name in _TESTCLASS, any args in their respective variables and then run the tests.
+// This test harness expects that environmental variable JACOBIN_EXE gives the full name and path of the executable
+// we're running the tests on. The folder which contains the test class should be specified in the environmental
+// variable JACOBIN_TESTDATA (without a terminating slash).
+func initVarsArrlen() error {
+    if testing.Short() { // don't run if running quick tests only. (Used primarily so GitHub doesn't run and bork)
+        return fmt.Errorf("test not run due to -short")
+    }
+
+    _JACOBIN = os.Getenv("JACOBIN_EXE") // returns "" if JACOBIN_EXE has not been specified.
+    _JVM_ARGS = ""
+    _TESTCLASS = "arrlen.class" // the class to test
+    _APP_ARGS = ""
+
+    if _JACOBIN == "" {
+        return fmt.Errorf("test failure due to missing Jacobin executable. Please specify it in JACOBIN_EXE")
+    } else if _, err := os.Stat(_JACOBIN); err != nil {
+        return fmt.Errorf("missing Jacobin executable, which was specified as %s", _JACOBIN)
+    }
+
+    if _TESTCLASS != "" {
+        testClass := os.Getenv("JACOBIN_TESTDATA") + string(os.PathSeparator) + _TESTCLASS
+        if _, err := os.Stat(testClass); err != nil {
+            return fmt.Errorf("missing class to test, which was specified as %s", testClass)
+        } else {
+            _TESTCLASS = testClass
+        }
+    }
+    return nil
+}
+
+func TestRunArrlen(t *testing.T) {
+    if testing.Short() { // don't run if running quick tests only. (Used primarily so GitHub doesn't run and bork)
+        t.Skip()
+    }
+
+    initErr := initVarsArrlen()
+    if initErr != nil {
+        t.Fatalf("Test failure due to: %s", initErr.Error())
+    }
+
+    var cmd *exec.Cmd
+    // run the various combinations of args. This is necessary b/c the empty string is viewed as
+    // an actual specified option on the command line.
+    if len(_JVM_ARGS) > 0 {
+        if len(_APP_ARGS) > 0 {
+            cmd = exec.Command(_JACOBIN, _JVM_ARGS, _TESTCLASS, _APP_ARGS)
+        } else {
+            cmd = exec.Command(_JACOBIN, _JVM_ARGS, _TESTCLASS)
+        }
+    } else {
+        if len(_APP_ARGS) > 0 {
+            cmd = exec.Command(_JACOBIN, _TESTCLASS, _APP_ARGS)
+        } else {
+            cmd = exec.Command(_JACOBIN, _TESTCLASS)
+        }
+    }
+
+    // get the stdout and stderr contents from the file execution
+    stderr, err := cmd.StderrPipe()
+    stdout, err := cmd.StdoutPipe()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // run the command
+    if err = cmd.Start(); err != nil {
+        t.Errorf("Got error running Jacobin: %s", err.Error())
+    }
+
+    // Here begin the actual tests on the output to stderr and stdout
+    slurp, _ := io.ReadAll(stderr)
+    if len(slurp) != 0 {
+        t.Errorf("Got unexpected output to stderr: %s", string(slurp))
+    }
+
+    slurp, _ = io.ReadAll(stdout)
+
+    if !strings.Contains(string(slurp), "arr_ints length: 3") {
+        t.Errorf("Did not get expected output (arr_ints length: 3) to stdout. Got: %s",
+            string(slurp))
+    }
+
+    if !strings.Contains(string(slurp), "arr_strings length: 3") {
+        t.Errorf("Did not get expected output (arr_strings length: 3) to stdout. Got: %s",
+            string(slurp))
+    }
+}
