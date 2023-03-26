0522df1d844e5ca512dffc7b2e9fe75ea3d2b2d8
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2023-03-25 18:48:26 -0700

    JACOBIN-226 Move project to go 1.20

--- src/go.mod
+++ src/go.mod
@@ -1,6 +1,7 @@
 module jacobin
 
 // go 1.18
-go 1.19  //  updated to 1.19 with v. 0.2.1, Aug. 2022
+// go 1.19  //  updated to 1.19 with v. 0.2.1, Aug. 2022
+go 1.20  // as of 25-iii-2023
 
 
--- src/wholeClassTests/ArrayLength_test.go
+++ src/wholeClassTests/ArrayLength_test.go
@@ -7,13 +7,13 @@
 package wholeClassTests
 
 import (
-    "fmt"
-    "io"
-    "log"
-    "os"
-    "os/exec"
-    "strings"
-    "testing"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+	"testing"
 )
 
 /* Regression test for JACOBIN-219:
@@ -49,86 +49,86 @@ import (
 // we're running the tests on. The folder which contains the test class should be specified in the environmental
 // variable JACOBIN_TESTDATA (without a terminating slash).
 func initVarsArrlen() error {
-    if testing.Short() { // don't run if running quick tests only. (Used primarily so GitHub doesn't run and bork)
-        return fmt.Errorf("test not run due to -short")
-    }
+	if testing.Short() { // don't run if running quick tests only. (Used primarily so GitHub doesn't run and bork)
+		return fmt.Errorf("test not run due to -short")
+	}
 
-    _JACOBIN = os.Getenv("JACOBIN_EXE") // returns "" if JACOBIN_EXE has not been specified.
-    _JVM_ARGS = ""
-    _TESTCLASS = "arrlen.class" // the class to test
-    _APP_ARGS = ""
+	_JACOBIN = os.Getenv("JACOBIN_EXE") // returns "" if JACOBIN_EXE has not been specified.
+	_JVM_ARGS = ""
+	_TESTCLASS = "arrlen.class" // the class to test
+	_APP_ARGS = ""
 
-    if _JACOBIN == "" {
-        return fmt.Errorf("test failure due to missing Jacobin executable. Please specify it in JACOBIN_EXE")
-    } else if _, err := os.Stat(_JACOBIN); err != nil {
-        return fmt.Errorf("missing Jacobin executable, which was specified as %s", _JACOBIN)
-    }
+	if _JACOBIN == "" {
+		return fmt.Errorf("test failure due to missing Jacobin executable. Please specify it in JACOBIN_EXE")
+	} else if _, err := os.Stat(_JACOBIN); err != nil {
+		return fmt.Errorf("missing Jacobin executable, which was specified as %s", _JACOBIN)
+	}
 
-    if _TESTCLASS != "" {
-        testClass := os.Getenv("JACOBIN_TESTDATA") + string(os.PathSeparator) + _TESTCLASS
-        if _, err := os.Stat(testClass); err != nil {
-            return fmt.Errorf("missing class to test, which was specified as %s", testClass)
-        } else {
-            _TESTCLASS = testClass
-        }
-    }
-    return nil
+	if _TESTCLASS != "" {
+		testClass := os.Getenv("JACOBIN_TESTDATA") + string(os.PathSeparator) + _TESTCLASS
+		if _, err := os.Stat(testClass); err != nil {
+			return fmt.Errorf("missing class to test, which was specified as %s", testClass)
+		} else {
+			_TESTCLASS = testClass
+		}
+	}
+	return nil
 }
 
 func TestRunArrlen(t *testing.T) {
-    if testing.Short() { // don't run if running quick tests only. (Used primarily so GitHub doesn't run and bork)
-        t.Skip()
-    }
+	if testing.Short() { // don't run if running quick tests only. (Used primarily so GitHub doesn't run and bork)
+		t.Skip()
+	}
 
-    initErr := initVarsArrlen()
-    if initErr != nil {
-        t.Fatalf("Test failure due to: %s", initErr.Error())
-    }
+	initErr := initVarsArrlen()
+	if initErr != nil {
+		t.Fatalf("Test failure due to: %s", initErr.Error())
+	}
 
-    var cmd *exec.Cmd
-    // run the various combinations of args. This is necessary b/c the empty string is viewed as
-    // an actual specified option on the command line.
-    if len(_JVM_ARGS) > 0 {
-        if len(_APP_ARGS) > 0 {
-            cmd = exec.Command(_JACOBIN, _JVM_ARGS, _TESTCLASS, _APP_ARGS)
-        } else {
-            cmd = exec.Command(_JACOBIN, _JVM_ARGS, _TESTCLASS)
-        }
-    } else {
-        if len(_APP_ARGS) > 0 {
-            cmd = exec.Command(_JACOBIN, _TESTCLASS, _APP_ARGS)
-        } else {
-            cmd = exec.Command(_JACOBIN, _TESTCLASS)
-        }
-    }
+	var cmd *exec.Cmd
+	// run the various combinations of args. This is necessary b/c the empty string is viewed as
+	// an actual specified option on the command line.
+	if len(_JVM_ARGS) > 0 {
+		if len(_APP_ARGS) > 0 {
+			cmd = exec.Command(_JACOBIN, _JVM_ARGS, _TESTCLASS, _APP_ARGS)
+		} else {
+			cmd = exec.Command(_JACOBIN, _JVM_ARGS, _TESTCLASS)
+		}
+	} else {
+		if len(_APP_ARGS) > 0 {
+			cmd = exec.Command(_JACOBIN, _TESTCLASS, _APP_ARGS)
+		} else {
+			cmd = exec.Command(_JACOBIN, _TESTCLASS)
+		}
+	}
 
-    // get the stdout and stderr contents from the file execution
-    stderr, err := cmd.StderrPipe()
-    stdout, err := cmd.StdoutPipe()
-    if err != nil {
-        log.Fatal(err)
-    }
+	// get the stdout and stderr contents from the file execution
+	stderr, err := cmd.StderrPipe()
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    // run the command
-    if err = cmd.Start(); err != nil {
-        t.Errorf("Got error running Jacobin: %s", err.Error())
-    }
+	// run the command
+	if err = cmd.Start(); err != nil {
+		t.Errorf("Got error running Jacobin: %s", err.Error())
+	}
 
-    // Here begin the actual tests on the output to stderr and stdout
-    slurp, _ := io.ReadAll(stderr)
-    if len(slurp) != 0 {
-        t.Errorf("Got unexpected output to stderr: %s", string(slurp))
-    }
+	// Here begin the actual tests on the output to stderr and stdout
+	slurp, _ := io.ReadAll(stderr)
+	if len(slurp) != 0 {
+		t.Errorf("Got unexpected output to stderr: %s", string(slurp))
+	}
 
-    slurp, _ = io.ReadAll(stdout)
+	slurp, _ = io.ReadAll(stdout)
 
-    if !strings.Contains(string(slurp), "arr_ints length: 3") {
-        t.Errorf("Did not get expected output (arr_ints length: 3) to stdout. Got: %s",
-            string(slurp))
-    }
+	if !strings.Contains(string(slurp), "arr_ints length: 3") {
+		t.Errorf("Did not get expected output (arr_ints length: 3) to stdout. Got: %s",
+			string(slurp))
+	}
 
-    if !strings.Contains(string(slurp), "arr_strings length: 3") {
-        t.Errorf("Did not get expected output (arr_strings length: 3) to stdout. Got: %s",
-            string(slurp))
-    }
+	if !strings.Contains(string(slurp), "arr_strings length: 3") {
+		t.Errorf("Did not get expected output (arr_strings length: 3) to stdout. Got: %s",
+			string(slurp))
+	}
 }
