0f88c54dad6aaba3f0c9a190a90821d58ee01fec
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2023-04-18 13:29:16 -0700

    JACOBIN-240 Changed object type from byte to string.

--- src/object/object.go
+++ src/object/object.go
@@ -7,7 +7,7 @@
 package object
 
 import (
-	"jacobin/classloader"
+    "jacobin/classloader"
 )
 
 // With regard to the layout of a created object in Jacobin, note that
@@ -15,9 +15,9 @@ import (
 // that insures that the fields that follow the oops (the mark word and
 // the class pointer) are aligned in memory for maximal performance.
 type Object struct {
-	Mark   MarkWord
-	Klass  *classloader.Klass // pointer to the loaded class
-	Fields []Field            // slice containing the fields
+    Mark   MarkWord
+    Klass  *classloader.Klass // pointer to the loaded class
+    Fields []Field            // slice containing the fields
 }
 
 // These mark word contains values for different purposes. Here,
@@ -25,14 +25,14 @@ type Object struct {
 // from the address of the object. The 'misc' field will eventually
 // contain other values, such as locking and monitoring items.
 type MarkWord struct {
-	Hash uint32 // contains hash code which is the lower 32 bits of the address
-	Misc uint32 // at present unused
+    Hash uint32 // contains hash code which is the lower 32 bits of the address
+    Misc uint32 // at present unused
 }
 
 // We need to know the type of the field only to tell whether
 // it occupies one or two slots on the stack when getfield and
 // putfield bytecodes are executed.
 type Field struct {
-	Ftype  byte // what type of value is stored in the field
-	Fvalue any  // the actual value
+    Ftype  string // what type of value is stored in the field
+    Fvalue any    // the actual value
 }
