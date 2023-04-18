091c74e41acd3a70a16f20795ae1ab673616d0e8
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2023-04-18 15:05:09 -0700

    JACOBIN-240 experimenting with instance fields in Strings template

--- src/object/String.go
+++ src/object/String.go
@@ -12,7 +12,7 @@ import "jacobin/classloader"
 // to have a means of creating them quickly, rather than building
 // them from scratch each time by walking through the constant pool.
 
-func New() *Object {
+func NewString() *Object {
     s := new(Object)
     s.Mark.Hash = 0
     k := classloader.Classes["java/lang/String"]
@@ -20,8 +20,12 @@ func New() *Object {
 
     // ==== now the fields ====
 
-    // field 01 -- value: the content of the string as array of bytes
-    s.Fields = append(s.Fields, Field{Ftype: "[B]", Fvalue: make([]byte, 0)})
+    // field 01 -- value: the content of the string as array of chars
+    // Note: Post JDK9, this field is an array of bytes, so as to
+    // enable compact strings. Here, for better compatibility with
+    // go, for the nonce we make it an array of Java chars's
+    // equivalent in go: runes.
+    s.Fields = append(s.Fields, Field{Ftype: "[C", Fvalue: make([]rune, 0)})
 
     // field 02 -- coder LATIN(=bytes, for compact strings) is 0; UTF16 is 1
     s.Fields = append(s.Fields, Field{Ftype: "B", Fvalue: 1})
@@ -36,6 +40,22 @@ func New() *Object {
     s.Fields = append(s.Fields, Field{Ftype: "L", Fvalue: nil})
 
     // field 06 -- ISO_8859_1.INSTANCE ptr to encoder
+    s.Fields = append(s.Fields, Field{Ftype: "L", Fvalue: nil})
+
+    // field 07 -- sun/nio/cs/US_ASCII.INSTANCE
+    s.Fields = append(s.Fields, Field{Ftype: "L", Fvalue: nil})
+
+    // field 08 -- java/nio/charset/CodingErrorAction.REPLACE
+    s.Fields = append(s.Fields, Field{Ftype: "L", Fvalue: nil})
+
+    // field 09 -- java/lang/String.CASE_INSENSITIVE_ORDER
+    // points to a comparator. Will be useful to fill in later
+    s.Fields = append(s.Fields, Field{Ftype: "L", Fvalue: nil})
+
+    // field 10 -- hashIsZero (only true in rare case where hash is 0
+    s.Fields = append(s.Fields, Field{Ftype: "Z", Fvalue: false})
+
+    // field 11 -- serialPersistentFields
     s.Fields = append(s.Fields, Field{Ftype: "L", Fvalue: nil})
 
     return s
