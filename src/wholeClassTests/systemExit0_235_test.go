9fde38aa250bb996039186153f43f52cc7f30829
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2023-04-22 14:43:12 -0700

    JACOBIN-235 Added test class to wholeClassTests

---
+++ src/wholeClassTests/systemExit0_235_test.go
@@ -0,0 +1,109 @@
+/*
+ * Jacobin VM - A Java virtual machine
+ * Copyright (c) 2022 by the Jacobin authors. All rights reserved.
+ * Licensed under Mozilla Public License 2.0 (MPL 2.0)
+ */
+
+package wholeClassTests
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"testing"
+)
+
+// Test for class that a class consisting of a single call to
+// System.exit(0) works correctly and produces no output to
+// stdout or stderr. Consult JACOBIN-235
+//
+// Source code:
+//
+//	public class SystemExit0_JACOBIN235 {
+//	   public static void main(String[] args) throws Exception {
+//	       System.exit(0);
+//	   }
+//	}
+func initVarsPrint_235() error {
+	if testing.Short() { // don't run if running quick tests only. (Used primarily so GitHub doesn't run and bork)
+		return fmt.Errorf("test not run due to -short")
+	}
+
+	_JACOBIN = os.Getenv("JACOBIN_EXE") // returns "" if JACOBIN_EXE has not been specified.
+	_JVM_ARGS = ""
+	_TESTCLASS = "SystemExit0_JACOBIN235.class" // the class to test
+	_APP_ARGS = ""
+
+	if _JACOBIN == "" {
+		return fmt.Errorf("test failure due to missing Jacobin executable. Please specify it in JACOBIN_EXE")
+	} else if _, err := os.Stat(_JACOBIN); err != nil {
+		return fmt.Errorf("missing Jacobin executable, which was specified as %s", _JACOBIN)
+	}
+
+	if _TESTCLASS != "" {
+		testClass := os.Getenv("JACOBIN_TESTDATA") + string(os.PathSeparator) + _TESTCLASS
+		if _, err := os.Stat(testClass); err != nil {
+			return fmt.Errorf("missing class to test, which was specified as %s", testClass)
+		} else {
+			_TESTCLASS = testClass
+		}
+	}
+	return nil
+}
+
+func TestRun235(t *testing.T) {
+	if testing.Short() { // don't run if running quick tests only. (Used primarily so GitHub doesn't run and bork)
+		t.Skip()
+	}
+
+	initErr := initVarsPrint_235()
+	if initErr != nil {
+		t.Fatalf("Test failure due to: %s", initErr.Error())
+	}
+
+	var cmd *exec.Cmd
+
+	// run the various combinations of args. This is necessary b/c the empty string is viewed as
+	// an actual specified option on the command line.
+	if len(_JVM_ARGS) > 0 {
+		if len(_APP_ARGS) > 0 {
+			cmd = exec.Command(_JACOBIN, _JVM_ARGS, _TESTCLASS, _APP_ARGS)
+		} else {
+			cmd = exec.Command(_JACOBIN, _JVM_ARGS, _TESTCLASS)
+		}
+	} else {
+		if len(_APP_ARGS) > 0 {
+			cmd = exec.Command(_JACOBIN, _TESTCLASS, _APP_ARGS)
+		} else {
+			cmd = exec.Command(_JACOBIN, _TESTCLASS)
+		}
+	}
+
+	// get the stdout and stderr contents from the file execution
+	stderr, err := cmd.StderrPipe()
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// run the command
+	if err = cmd.Start(); err != nil {
+		t.Errorf("Got error running Jacobin: %s", err.Error())
+	}
+
+	// Here begin the actual tests on the output to stderr and stdout
+	slurp, _ := io.ReadAll(stderr)
+	slurpErr := string(slurp)
+	if len(slurp) != 0 {
+		t.Errorf("Got unexpected output to stderr: %s", slurpErr)
+	}
+
+	slurp, _ = io.ReadAll(stdout)
+	slurpOut := string(slurp)
+
+	if len(slurpOut) != 0 {
+		t.Errorf("Got unexpected output to stdout: %s", slurpErr)
+	}
+}
