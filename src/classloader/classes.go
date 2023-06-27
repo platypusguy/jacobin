e7651cdb6004dcae83beea608f1a91f2058a3f88
Richard Elkins <richard.elkins@gmail.com>
2023-06-27 10:28:05 -0500

    additional shutdowns in FetchMethodAndCP

--- src/classloader/classes.go
+++ src/classloader/classes.go
@@ -218,14 +218,14 @@ func FetchMethodAndCP(class, meth string, methType string) (MTentry, error) {
 		if k == nil {
 			msg := fmt.Sprintf("FetchMethodAndCP: MethAreaFetch could not find class {%s}", class)
 			_ = log.Log(msg, log.SEVERE)
-			return MTentry{}, errors.New(msg)
+			shutdown.Exit(shutdown.JVM_EXCEPTION)
 		}
 
 		if k.Loader == "" { // if class is not found, the zero value struct is returned
 			// TODO: check superclasses if method not found
 			msg := "FetchMethodAndCP: Could not find class: " + class
 			_ = log.Log(msg, log.SEVERE)
-			return MTentry{}, errors.New(msg)
+			shutdown.Exit(shutdown.JVM_EXCEPTION)
 		}
 
 		// the class has been found (k) so now go down the list of methods until
@@ -261,7 +261,7 @@ func FetchMethodAndCP(class, meth string, methType string) (MTentry, error) {
 		}
 	}
 
-	// if we got this far, the class was not found
+	// if we got this far, something went wrong with locating the method
 
 	if meth == "main" { // to be consistent with the JDK, we print this peculiar error message when main() is missing
 		_ = log.Log("Error: Main method not found in class "+class+", please define the main method as:\n"+
@@ -270,7 +270,8 @@ func FetchMethodAndCP(class, meth string, methType string) (MTentry, error) {
 		_ = log.Log("Found class: "+class+", but it did not contain method: "+meth, log.SEVERE)
 	}
 
-	return MTentry{}, errors.New("method not found")
+	shutdown.Exit(shutdown.JVM_EXCEPTION)
+	return MTentry{}, errors.New("method not found") // dummy return
 }
 
 // FetchUTF8stringFromCPEntryNumber fetches the UTF8 string using the CP entry number
