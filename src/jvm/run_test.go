a199ed7df131784378a0987c1d50fd3e96e9401e
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2023-07-01 21:31:26 -0700

    JACOBIN-289 Added test for fix to previous defect in CHECKCAST

--- src/jvm/run_test.go
+++ src/jvm/run_test.go
@@ -316,51 +316,51 @@ func TestBipushNeg(t *testing.T) {
 	}
 }
 
-// // CHECKCAST: This bytecode uses the same logic as INSTANCEOF, except how
-// // it handles exceptional conditions.
-// func TestCheckcastOfString(t *testing.T) {
-// 	g := globals.GetGlobalRef()
-// 	globals.InitGlobals("test")
-// 	g.JacobinName = "test" // prevents a shutdown when the exception hits.
-// 	log.Init()
-//
-// 	classloader.Init()
-// 	// classloader.LoadBaseClasses()
-// 	classloader.MethAreaInsert("java/lang/String",
-// 		&(classloader.Klass{
-// 			Status: 'X', // use a status that's not subsequently tested for.
-// 			Loader: "bootstrap",
-// 			Data:   nil,
-// 		}))
-// 	s := classloader.NewStringFromGoString("hello world")
-//
-// 	f := newFrame(CHECKCAST)
-// 	f.Meth = append(f.Meth, 0) // point to entry [2] in CP
-// 	f.Meth = append(f.Meth, 2) // " "
-//
-// 	// now create the CP. First entry is perforce 0
-// 	// [1] entry points to a UTF8 entry with the class name
-// 	// [2] is a ClassRef that points to the UTF8 string in [1]
-// 	CP := classloader.CPool{}
-// 	CP.CpIndex = make([]classloader.CpEntry, 10, 10)
-// 	CP.CpIndex[0] = classloader.CpEntry{Type: 0, Slot: 0}
-// 	CP.CpIndex[1] = classloader.CpEntry{Type: classloader.UTF8, Slot: 0}
-// 	CP.CpIndex[2] = classloader.CpEntry{Type: classloader.ClassRef, Slot: 1}
-// 	CP.ClassRefs = append(CP.ClassRefs, 0) // point to record 0 in Utf8Refs
-// 	CP.Utf8Refs = append(CP.Utf8Refs, "java/lang/String")
-// 	f.CP = &CP
-//
-// 	push(&f, s)
-//
-// 	fs := frames.CreateFrameStack()
-// 	fs.PushFront(&f) // push the new frame
-// 	_ = runFrame(fs)
-//
-// 	value := pop(&f).(int64)
-// 	if value != 1 { // a 1 = it's a match between class and object
-// 		t.Errorf("CHECKCAST: Expected string to return a 1, got %d", value)
-// 	}
-// }
+// CHECKCAST: This bytecode uses the same logic as INSTANCEOF, except how
+// it handles exceptional conditions.
+func TestCheckcastOfString(t *testing.T) {
+	g := globals.GetGlobalRef()
+	globals.InitGlobals("test")
+	g.JacobinName = "test" // prevents a shutdown when the exception hits.
+	log.Init()
+
+	classloader.Init()
+	// classloader.LoadBaseClasses()
+	classloader.MethAreaInsert("java/lang/String",
+		&(classloader.Klass{
+			Status: 'X', // use a status that's not subsequently tested for.
+			Loader: "bootstrap",
+			Data:   nil,
+		}))
+	s := classloader.NewStringFromGoString("hello world")
+
+	f := newFrame(CHECKCAST)
+	f.Meth = append(f.Meth, 0) // point to entry [2] in CP
+	f.Meth = append(f.Meth, 2) // " "
+
+	// now create the CP. First entry is perforce 0
+	// [1] entry points to a UTF8 entry with the class name
+	// [2] is a ClassRef that points to the UTF8 string in [1]
+	CP := classloader.CPool{}
+	CP.CpIndex = make([]classloader.CpEntry, 10, 10)
+	CP.CpIndex[0] = classloader.CpEntry{Type: 0, Slot: 0}
+	CP.CpIndex[1] = classloader.CpEntry{Type: classloader.UTF8, Slot: 0}
+	CP.CpIndex[2] = classloader.CpEntry{Type: classloader.ClassRef, Slot: 0}
+	CP.ClassRefs = append(CP.ClassRefs, 1) // point to record 1 in CP: Utf8 for class name
+	CP.Utf8Refs = append(CP.Utf8Refs, "java/lang/String")
+	f.CP = &CP
+
+	push(&f, s)
+
+	fs := frames.CreateFrameStack()
+	fs.PushFront(&f) // push the new frame
+	_ = runFrame(fs)
+
+	value := pop(&f).(int64)
+	if value != 1 { // a 1 = it's a match between class and object
+		t.Errorf("CHECKCAST: Expected string to return a 1, got %d", value)
+	}
+}
 
 // D2F: test convert double to float
 func TestD2f(t *testing.T) {
