a6b7f12989761619a6505930cf20e1da3ac20a20
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2023-08-03 12:32:35 -0700

    JACOBIN-325 Segregated field-creation code into its own function

--- src/jvm/instantiate.go
+++ src/jvm/instantiate.go
@@ -72,96 +72,115 @@ func instantiateClass(classname string) (*object.Object, error) {
 
 	// handle the fields. If the object has no superclass other than Object,
 	// the fields are in an array in the order they're declared in the CP.
-	// If the object has a non-Object superclass, then those field's and
-	// the present object's field are stored in a map--indexed by the field
-	// name. Eventually, we might coalesce on a single approach for both
-	// kinds of objects.
-	if len(k.Data.Fields) > 0 {
-		for i := 0; i < len(k.Data.Fields); i++ {
-			f := k.Data.Fields[i]
-			desc := k.Data.CP.Utf8Refs[f.Desc]
-			name := k.Data.CP.Utf8Refs[f.Name]
-			if log.Level == log.FINE {
-				reciteField := fmt.Sprintf("Class: %s ield[%d] name: %s, type: %s", k.Data.Name, i,
-					name, desc)
-				_ = log.Log(reciteField, log.FINE)
-			}
+	// If the object has a non-Object superclass, then the superclass fields
+	// and the present object's field are stored in a map--indexed by the
+	// field name. Eventually, we might coalesce on a single approach for
+	// both kinds of objects.
+	if len(k.Data.Fields) == 0 {
+		return &obj, nil
+	}
 
-			fieldToAdd := new(object.Field)
-			fieldToAdd.Ftype = desc
-			switch string(fieldToAdd.Ftype[0]) {
-			case "L", "[": // it's a reference
-				fieldToAdd.Fvalue = nil
-			case "B", "C", "I", "J", "S", "Z":
-				fieldToAdd.Fvalue = int64(0)
-			case "D", "F":
-				fieldToAdd.Fvalue = 0.0
-			default:
-				_ = log.Log("error creating field in: "+classname+
-					" Invalid type: "+fieldToAdd.Ftype, log.SEVERE)
-				return nil, classloader.CFE("invalid field type")
-			}
+	// if k.Data.Superclass != "java/lang/Object"{
+	for i := 0; i < len(k.Data.Fields); i++ {
+		f := k.Data.Fields[i]
+		desc := k.Data.CP.Utf8Refs[f.Desc]
+		name := k.Data.CP.Utf8Refs[f.Name]
+		if log.Level == log.FINE {
+			reciteField := fmt.Sprintf("Class: %s ield[%d] name: %s, type: %s", k.Data.Name, i,
+				name, desc)
+			_ = log.Log(reciteField, log.FINE)
+		}
 
-			presentType := fieldToAdd.Ftype
-			if f.IsStatic {
-				// in the instantiated class, add an 'X' before the
-				// type, which notifies future users that the field
-				// is static and should be fetched from the Statics
-				// table.
-				fieldToAdd.Ftype = "X" + presentType
-			}
-
-			// static fields can have ConstantValue attributes,
-			// which specify their initial value.
-			if len(f.Attributes) > 0 {
-				for j := 0; j < len(f.Attributes); j++ {
-					attr := k.Data.CP.Utf8Refs[int(f.Attributes[j].AttrName)]
-					if attr == "ConstantValue" && f.IsStatic { // only statics can have ConstantValue attribute
-						valueIndex := int(f.Attributes[j].AttrContent[0])*256 +
-							int(f.Attributes[j].AttrContent[1])
-						valueType := k.Data.CP.CpIndex[valueIndex].Type
-						valueSlot := k.Data.CP.CpIndex[valueIndex].Slot
-						switch valueType {
-						case classloader.IntConst:
-							fieldToAdd.Fvalue = int64(k.Data.CP.IntConsts[valueSlot])
-						case classloader.LongConst:
-							fieldToAdd.Fvalue = k.Data.CP.LongConsts[valueSlot]
-						case classloader.FloatConst:
-							fieldToAdd.Fvalue = float64(k.Data.CP.Floats[valueSlot])
-						case classloader.DoubleConst:
-							fieldToAdd.Fvalue = k.Data.CP.Doubles[valueSlot]
-						case classloader.StringConst:
-							str := k.Data.CP.Utf8Refs[valueSlot]
-							fieldToAdd.Fvalue = object.NewStringFromGoString(str)
-						default:
-							errMsg := fmt.Sprintf(
-								"Unexpected ConstantValue type in instantiate: %d", valueType)
-							_ = log.Log(errMsg, log.SEVERE)
-							return nil, errors.New(errMsg)
-						} // end of ConstantValue type switch
-					} // end of ConstantValue attribute processing
-				} // end of processing attributes
-			} // end of search through attributes
-
-			if f.IsStatic {
-				s := classloader.Static{
-					Type:  presentType, // we use the type without the 'X" prefix in the statics table.
-					Value: fieldToAdd.Fvalue,
-				}
-				// add the field to the Statics table
-				fieldName := k.Data.CP.Utf8Refs[f.Name]
-				fullFieldName := classname + "." + fieldName
-
-				_, alreadyPresent := classloader.Statics[fullFieldName]
-				if !alreadyPresent { // add only if field has not been pre-loaded
-					_ = classloader.AddStatic(fullFieldName, s)
-				}
-			}
-			obj.Fields = append(obj.Fields, *fieldToAdd)
-		} // loop through the fields if any
-	} // test if there are any declared fields
+		fieldToAdd, err := createField(f, k, classname)
+		if err != nil {
+			return nil, err
+		}
+		obj.Fields = append(obj.Fields, *fieldToAdd)
+	} // loop through the fields if any
+	// } // test if there are any declared fields
 
 	return &obj, nil
+}
+
+func createField(f classloader.Field, k *classloader.Klass, classname string) (*object.Field, error) {
+	desc := k.Data.CP.Utf8Refs[f.Desc]
+	name := k.Data.CP.Utf8Refs[f.Name]
+	if log.Level == log.FINE {
+		reciteField := fmt.Sprintf("Class: %s field name: %s, type: %s", k.Data.Name, name, desc)
+		_ = log.Log(reciteField, log.FINE)
+	}
+
+	fieldToAdd := new(object.Field)
+	fieldToAdd.Ftype = desc
+	switch string(fieldToAdd.Ftype[0]) {
+	case "L", "[": // it's a reference
+		fieldToAdd.Fvalue = nil
+	case "B", "C", "I", "J", "S", "Z":
+		fieldToAdd.Fvalue = int64(0)
+	case "D", "F":
+		fieldToAdd.Fvalue = 0.0
+	default:
+		_ = log.Log("error creating field in: "+classname+
+			" Invalid type: "+fieldToAdd.Ftype, log.SEVERE)
+		return nil, classloader.CFE("invalid field type")
+	}
+
+	presentType := fieldToAdd.Ftype
+	if f.IsStatic {
+		// in the instantiated class, add an 'X' before the
+		// type, which notifies future users that the field
+		// is static and should be fetched from the Statics
+		// table.
+		fieldToAdd.Ftype = "X" + presentType
+	}
+
+	// static fields can have ConstantValue attributes,
+	// which specify their initial value.
+	if len(f.Attributes) > 0 {
+		for j := 0; j < len(f.Attributes); j++ {
+			attr := k.Data.CP.Utf8Refs[int(f.Attributes[j].AttrName)]
+			if attr == "ConstantValue" && f.IsStatic { // only statics can have ConstantValue attribute
+				valueIndex := int(f.Attributes[j].AttrContent[0])*256 +
+					int(f.Attributes[j].AttrContent[1])
+				valueType := k.Data.CP.CpIndex[valueIndex].Type
+				valueSlot := k.Data.CP.CpIndex[valueIndex].Slot
+				switch valueType {
+				case classloader.IntConst:
+					fieldToAdd.Fvalue = int64(k.Data.CP.IntConsts[valueSlot])
+				case classloader.LongConst:
+					fieldToAdd.Fvalue = k.Data.CP.LongConsts[valueSlot]
+				case classloader.FloatConst:
+					fieldToAdd.Fvalue = float64(k.Data.CP.Floats[valueSlot])
+				case classloader.DoubleConst:
+					fieldToAdd.Fvalue = k.Data.CP.Doubles[valueSlot]
+				case classloader.StringConst:
+					str := k.Data.CP.Utf8Refs[valueSlot]
+					fieldToAdd.Fvalue = object.NewStringFromGoString(str)
+				default:
+					errMsg := fmt.Sprintf(
+						"Unexpected ConstantValue type in instantiate: %d", valueType)
+					_ = log.Log(errMsg, log.SEVERE)
+					return nil, errors.New(errMsg)
+				} // end of ConstantValue type switch
+			} // end of ConstantValue attribute processing
+		} // end of processing attributes
+	} // end of search through attributes
+
+	if f.IsStatic {
+		s := classloader.Static{
+			Type:  presentType, // we use the type without the 'X" prefix in the statics table.
+			Value: fieldToAdd.Fvalue,
+		}
+		// add the field to the Statics table
+		fieldName := k.Data.CP.Utf8Refs[f.Name]
+		fullFieldName := classname + "." + fieldName
+
+		_, alreadyPresent := classloader.Statics[fullFieldName]
+		if !alreadyPresent { // add only if field has not been pre-loaded
+			_ = classloader.AddStatic(fullFieldName, s)
+		}
+	}
+	return fieldToAdd, nil
 }
 
 // Loads the class (if it's not already loaded) and makes sure it's accessible in the method area
