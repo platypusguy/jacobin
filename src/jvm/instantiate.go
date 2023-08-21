b2e5626e4b975c08062c0ba4ebd8d6b7db196532
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2023-08-21 16:28:07 -0700

    JACOBIN-339 Initial logic for handling <clinit>

--- src/jvm/instantiate.go
+++ src/jvm/instantiate.go
@@ -10,10 +10,14 @@ import (
 	"errors"
 	"fmt"
 	"jacobin/classloader"
+	"jacobin/frames"
+	"jacobin/globals"
 	"jacobin/log"
 	"jacobin/object"
 	"jacobin/shutdown"
+	"jacobin/thread"
 	"jacobin/types"
+	"strconv"
 	"strings"
 	"unsafe"
 )
@@ -160,9 +164,58 @@ func instantiateClass(classname string) (*object.Object, error) {
 // executed as well.
 //
 // CURR: Implement the above logic here.
-func runInitializationBlock(k *classloader.Klass, idx int) {
-	msg := fmt.Sprintf("***************** <clinit> found in %s, method %d **********\n", k.Data.Name, idx)
+func runInitializationBlock(k *classloader.Klass, idx int) error {
+
+	msg := fmt.Sprintf("<clinit> found in %s, method #%d\n", k.Data.Name, idx)
+	_ = log.Log(msg, log.FINE)
 	fmt.Print(msg)
+
+	className := k.Data.Name
+	me, err := classloader.FetchMethodAndCP(className, "<clinit>", "()V")
+	if err != nil {
+		return errors.New("Class not found: " + className + "<clinit>()")
+	}
+
+	m := me.Meth.(classloader.JmEntry)
+	f := frames.CreateFrame(m.MaxStack) // create a new frame
+	f.MethName = "<clinit>"
+	f.ClName = className
+	f.CP = m.Cp                        // add its pointer to the class CP
+	for i := 0; i < len(m.Code); i++ { // copy the bytecodes over
+		f.Meth = append(f.Meth, m.Code[i])
+	}
+
+	// allocate the local variables
+	for j := 0; j < m.MaxLocals; j++ {
+		f.Locals = append(f.Locals, 0)
+	}
+
+	// create the first thread and place its first frame on it
+	glob := globals.GetGlobalRef()
+	clInitThread := thread.CreateThread()
+	clInitThread.Stack = frames.CreateFrameStack()
+	clInitThread.ID = thread.AddThreadToTable(&clInitThread, &glob.Threads)
+
+	clInitThread.Trace = MainThread.Trace
+	f.Thread = clInitThread.ID
+
+	if frames.PushFrame(clInitThread.Stack, f) != nil {
+		_ = log.Log("Memory exceptions allocating frame on thread: "+strconv.Itoa(clInitThread.ID),
+			log.SEVERE)
+		return errors.New("outOfMemory Exception")
+	}
+
+	if clInitThread.Trace {
+		traceInfo := fmt.Sprintf("StartExec: f.MethName=%s, m.MaxStack=%d, m.MaxLocals=%d, len(m.Code)=%d",
+			f.MethName, m.MaxStack, m.MaxLocals, len(m.Code))
+		_ = log.Log(traceInfo, log.TRACE_INST)
+	}
+
+	err = runThread(&clInitThread)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 // creates a field for insertion into the object representation
