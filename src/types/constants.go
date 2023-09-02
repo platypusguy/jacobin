da4582058923f1031b6247ed0ca8a579c768b1db
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2023-09-01 22:11:54 -0700

    JACOBIN-345 Replaced magic numbers with constants with descriptive names

---
+++ src/types/constants.go
@@ -0,0 +1,14 @@
+/*
+ * Jacobin VM - A Java virtual machine
+ * Copyright (c) 2023 by  the Jacobin authors. Consult jacobin.org.
+ * Licensed under Mozilla Public License 2.0 (MPL 2.0) All rights reserved.
+ */
+
+package types
+
+// Grab bag of constants used in Jacobin
+
+// ---- <clInit> status bytes ----
+const NoClinit byte = 0x00
+const ClInitNotRun byte = 0x01
+const ClInitRun byte = 0x02
