467a26a77eec454cdf0f0f7c41beaa87165647f2
Richard Elkins <richard.elkins@gmail.com>
2023-09-12 14:17:08 -0500

    JACOBIN-352
    ```runJavaInitializer``` and ```runNativeInitializer``` returned errors were being ignored.

--- src/jvm/initializerBlock.go
+++ src/jvm/initializerBlock.go
@@ -60,9 +60,17 @@ func runInitializationBlock(k *classloader.Klass) error {
 		if err == nil {
 			switch me.MType {
 			case 'J': // it's a Java initializer (the most common case)
-				_ = runJavaInitializer(me.Meth, k)
+				// _ = runJavaInitializer(me.Meth, k)
+				err = runJavaInitializer(me.Meth, k)
+				if err != nil {
+					return err
+				}
 			case 'G': // it's a native (that is, golang) initializer
-				_ = runNativeInitializer(me.Meth, k)
+				// _ = runNativeInitializer(me.Meth, k)
+				err = runNativeInitializer(me.Meth, k)
+				if err != nil {
+					return err
+				}
 			}
 		}
 	}
