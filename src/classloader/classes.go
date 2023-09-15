b3763185aab55526840db0278257e64434a74fd9
Richard Elkins <richard.elkins@gmail.com>
2023-09-15 11:32:01 -0500

    JACOBIN-353

--- src/classloader/classes.go
+++ src/classloader/classes.go
@@ -303,9 +303,10 @@ func FetchMethodAndCP(className, methName, methType string) (MTentry, error) {
 	// }
 
 	// if we got this far, something went wrong with locating the method
-	_ = log.Log("FetchMethodAndCP: Found class "+className+", but it did not contain method: "+methName, log.SEVERE)
-	shutdown.Exit(shutdown.JVM_EXCEPTION)
-	return MTentry{}, errors.New("method not found") // dummy return needed for tests
+	msg := "FetchMethodAndCP: Found class " + className + ", but it did not contain method: " + methName
+	return MTentry{}, errors.New(msg)
+	//shutdown.Exit(shutdown.JVM_EXCEPTION)
+	//return MTentry{}, errors.New("method not found") // dummy return needed for tests
 }
 
 // error message when main() can't be found
