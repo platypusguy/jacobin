f9f86b650f96b6453404d4b167a1bd1ca13d2c39
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2023-09-27 19:04:55 -0700

    JACOBIN-360 Added a unit test for errors.go.

--- src/jvm/errors_test.go
+++ src/jvm/errors_test.go
@@ -9,12 +9,48 @@ package jvm
 import (
 	"container/list"
 	"io"
+	"jacobin/frames"
 	"jacobin/globals"
 	"jacobin/log"
 	"jacobin/thread"
 	"os"
 	"testing"
 )
+
+func TestShowFrameStackWhenPreviouslyShown(t *testing.T) {
+	g := globals.GetGlobalRef()
+	globals.InitGlobals("test")
+	g.JacobinName = "test"
+	g.StrictJDK = false
+
+	log.Init()
+	_ = log.SetLogLevel(log.INFO)
+
+	// redirect stderr & stdout to capture results from stderr
+	normalStderr := os.Stderr
+	r, w, _ := os.Pipe()
+	os.Stderr = w
+
+	normalStdout := os.Stdout
+	_, wout, _ := os.Pipe()
+	os.Stdout = wout
+
+	thread := thread.ExecThread{}
+	globals.GetGlobalRef().JvmFrameStackShown = true // should prevent any output
+	showFrameStack(&thread)
+
+	// restore stderr and stdout to what they were before
+	_ = w.Close()
+	os.Stderr = normalStderr
+	msg, _ := io.ReadAll(r)
+
+	_ = wout.Close()
+	os.Stdout = normalStdout
+
+	if string(msg) != "" {
+		t.Errorf("Got following output when expecting none: %s", string(msg))
+	}
+}
 
 func TestShowFrameStackWithEmptyStack(t *testing.T) {
 	g := globals.GetGlobalRef()
@@ -53,7 +89,7 @@ func TestShowFrameStackWithEmptyStack(t *testing.T) {
 	}
 }
 
-func TestShowFrameStackWhenPreviouslyShown(t *testing.T) {
+func TestShowFrameStackWithOneEntry(t *testing.T) {
 	g := globals.GetGlobalRef()
 	globals.InitGlobals("test")
 	g.JacobinName = "test"
@@ -71,9 +107,17 @@ func TestShowFrameStackWhenPreviouslyShown(t *testing.T) {
 	_, wout, _ := os.Pipe()
 	os.Stdout = wout
 
-	thread := thread.ExecThread{}
-	globals.GetGlobalRef().JvmFrameStackShown = true // should prevent any output
-	showFrameStack(&thread)
+	f := frames.CreateFrame(1) // create a new frame
+	f.MethName = "main"
+	f.ClName = "testClass"
+	f.PC = 42
+
+	th := thread.CreateThread()
+	th.Stack = frames.CreateFrameStack()
+	frames.PushFrame(th.Stack, f)
+
+	globals.GetGlobalRef().JvmFrameStackShown = false // should prevent any output
+	showFrameStack(&th)
 
 	// restore stderr and stdout to what they were before
 	_ = w.Close()
@@ -83,7 +127,8 @@ func TestShowFrameStackWhenPreviouslyShown(t *testing.T) {
 	_ = wout.Close()
 	os.Stdout = normalStdout
 
-	if string(msg) != "" {
-		t.Errorf("Got following output when expecting none: %s", string(msg))
+	errMsg := string(msg)
+	if errMsg != "Method: testClass.main                           PC: 042\n" {
+		t.Errorf("Got this when expecting 'Method: testClass.main                           PC: 042': %s", errMsg)
 	}
 }
