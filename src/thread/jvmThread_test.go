540636f547579aa19b0ec2247240b93842aace16
Richard Elkins <richard.elkins@gmail.com>
2023-10-26 08:15:47 -0500

    JACOBIN-385 part 2

--- src/thread/jvmThread_test.go
+++ src/thread/jvmThread_test.go
@@ -76,7 +76,8 @@ func TestAddingMultipleSimultaneousThreads(t *testing.T) {
 	}
 
 	wg.Wait()
-	size := len(globals.GetGlobalRef().Threads)
+	//size := len(globals.GetGlobalRef().Threads)
+	size := globals.GetGlobalRef().ThreadNumber
 	if size != expectedSize {
 		t.Errorf("Expecting thread table size of %d, got %d", expectedSize, size)
 	}
