953c0e4c721386d6894aaf8a899265d0b237fea6
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2023-11-15 23:58:35 -0800

    JACOBIN-300 Deleted extraneous file

--- src/opcodes.go
+++
@@ -1,7 +0,0 @@
-/*
- * Jacobin VM - A Java virtual machine
- * Copyright (c) 2023 by  the Jacobin authors. Consult jacobin.org.
- * Licensed under Mozilla Public License 2.0 (MPL 2.0) All rights reserved.
- */
-
-package main
