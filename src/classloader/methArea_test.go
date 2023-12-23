160e8dd92a9c8dc83498e6089509c69e3380ea29
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2023-12-23 10:29:28 -0800

    JACOBIN-418 Added unit test for failing fetch from MethodArea

--- src/classloader/methArea_test.go
+++ src/classloader/methArea_test.go
@@ -7,6 +7,11 @@
 package classloader
 
 import (
+	"io"
+	"jacobin/globals"
+	"jacobin/log"
+	"os"
+	"strings"
 	"sync"
 	"testing"
 )
@@ -75,5 +80,51 @@ func TestMethAreadDeleteNonExistentEntry(t *testing.T) {
 	if newLen != 1 {
 		t.Errorf("Expected post-deletion MethArea[] to have length of 1, got: %d",
 			newLen)
+	}
+}
+
+func TestMethAreadFetchNonExistentEntry(t *testing.T) {
+	// Testing the changes made as a result of JACOBIN-103
+	globals.InitGlobals("test")
+	log.Init()
+	_ = log.SetLogLevel(log.CLASS)
+
+	// redirect stderr to capture results from stderr
+	normalStderr := os.Stderr
+	r, w, _ := os.Pipe()
+	os.Stderr = w
+
+	MethArea = &sync.Map{}
+	methAreaSize = 0
+	currLen := MethAreaSize()
+	if currLen != 0 {
+		t.Errorf("Expecting MethArea size of 0, got: %d", currLen)
+	}
+
+	k := Klass{
+		Status: 0,
+		Loader: "",
+		Data:   &ClData{},
+	}
+	k.Data.Name = "testClass1"
+	k.Data.Superclass = "java/lang/Object"
+	k.Loader = "testloader"
+	k.Status = 'F'
+	MethAreaInsert("TestEntry", &k)
+
+	// fetching a non-entry should not cause an error, shiuld return nil
+	me := MethAreaFetch("NoSuchEntry")
+	if me != nil {
+		t.Errorf("Expected nil return from MethAreaFetch(), got: %v", me)
+	}
+
+	// restore stderr
+	_ = w.Close()
+	out, _ := io.ReadAll(r)
+	os.Stderr = normalStderr
+
+	msg := string(out[:])
+	if !strings.Contains(msg, "--> nil") {
+		t.Errorf("Expected different log message, got: %s", msg)
 	}
 }
