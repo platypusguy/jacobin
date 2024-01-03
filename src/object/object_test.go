5acfe5b1f91d5c6bad00afa267fb98a3916a77dd
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2024-01-03 11:48:04 -0800

    JACOBIN-418 Added unit test for creating a valid Double object

--- src/object/object_test.go
+++ src/object/object_test.go
@@ -33,3 +33,15 @@ func TestMakeValidPrimitiveByte(t *testing.T) {
 		t.Errorf("Value should be 0x61, got 0x%02x", value)
 	}
 }
+
+func TestMakeValidPrimitiveDouble(t *testing.T) {
+	objPtr := MakePrimitiveObject("java/lang/Double", types.Double, 42.0)
+	if *objPtr.Klass != "java/lang/Double" {
+		t.Errorf("Klass should be java/lang/Double, got %s", *objPtr.Klass)
+	}
+
+	value := objPtr.Fields[0].Fvalue.(float64)
+	if value != 42.0 {
+		t.Errorf("Value should be 0x42.0, got 0x%f", value)
+	}
+}
