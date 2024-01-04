86be6f8f5bde6f40939d5a8a4c49153002754352
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2024-01-03 20:21:05 -0800

    JACOBIN-418 Added complex unit test for valid Throwable

--- src/gfunction/javaLangThrowable_test.go
+++ src/gfunction/javaLangThrowable_test.go
@@ -7,8 +7,12 @@
 package gfunction
 
 import (
+	"container/list"
+	"jacobin/classloader"
+	"jacobin/frames"
 	"jacobin/globals"
 	"jacobin/log"
+	"jacobin/object"
 	"jacobin/statics"
 	"strings"
 	"testing"
@@ -54,4 +58,69 @@ func TestJavaLangThrowableFillInStackTraceWrongParmCount(t *testing.T) {
 	if !strings.HasPrefix(errMsg, "fillInStackTrace() expected two parameters") {
 		t.Errorf("did not get expected error message, got: %s", errMsg)
 	}
+}
+
+func TestJavaLangThrowableFillInStackTraceValid(t *testing.T) {
+	globals.InitGlobals("test")
+	log.Init()
+	_ = log.SetLogLevel(log.SEVERE)
+
+	classloader.InitMethodArea()
+
+	// we need to pass in a pointer to a valid JVM stack, so put one together here
+	f := frames.CreateFrame(2) // create a new frame
+	f.Thread = 1
+	f.MethName = "main"
+	f.MethType = "([Ljava/lang/String;)V"
+
+	clData := classloader.ClData{
+		Name:        "",
+		Superclass:  "",
+		Module:      "test module",
+		Pkg:         "",
+		Interfaces:  nil,
+		Fields:      nil,
+		MethodTable: nil,
+		Methods:     nil,
+		Attributes:  nil,
+		SourceFile:  "testClass.java",
+		Bootstraps:  nil,
+		CP:          classloader.CPool{},
+		Access:      classloader.AccessFlags{},
+		ClInit:      0,
+	}
+	klass := classloader.Klass{Loader: "testLoader", Data: &clData}
+	classloader.MethAreaInsert("java/testClass", &klass)
+	f.ClName = "java/testClass"
+	f.MethName = "java/testClass.test"
+
+	jvmStack := frames.CreateFrameStack()
+	frames.PushFrame(jvmStack, f)
+
+	// create a Throwable object
+	throw := object.MakeEmptyObject()
+	klassType := "java/lang/Throwable"
+	throw.Klass = &klassType
+
+	// set up instantiate function
+	globPtr := globals.GetGlobalRef()
+
+	// Enable functions call InstantiateClass through a global function variable. (This avoids circularity issues.)
+	globPtr.FuncInstantiateClass = InstantiateFillIn
+
+	params := []interface{}{jvmStack, throw}
+	err := fillInStackTrace(params)
+
+	// var retVal error
+	switch err.(type) {
+	case error:
+		t.Errorf("JavaLangThrowableFillInStack threw an unexpected error: %s", err.(error).Error())
+	}
+
+}
+
+func InstantiateFillIn(name string, list *list.List) (any, error) {
+	o := object.MakeEmptyObject()
+	o.Klass = &name
+	return o, nil
 }
