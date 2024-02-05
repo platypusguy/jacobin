621bb3306f83c6b35f37227c88c566a07a3e80d1
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2024-02-04 23:44:00 -0800

    JACOBIN-442 Added whole class test for LDIV divide-by-zero exception

---
+++ testdata/ThrowLDIVexception.java
@@ -0,0 +1,8 @@
+// division by zero to throw exception
+public final class ThrowLDIVexception {
+    public static void main(String[] args) {
+        long n = 6;
+	long x = 0;
+	long y = n/x;
+    }
+}
