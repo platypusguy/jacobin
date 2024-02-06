e8989c30813f445316208535481ef2ff76391620
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2024-02-05 23:17:27 -0800

    JACOBIN-442 Added wholeClassTest for LDIV exception that adheres strictly to JDK wording

--- src/wholeClassTests/LDIVexception_test.go
+++ src/wholeClassTests/LDIVexception_test.go
@@ -115,3 +115,59 @@ func TestRunLDIVexception(t *testing.T) {
 		t.Errorf("Did not get expected output to stderr. Got: %s", string(slurp))
 	}
 }
+
+// same as previous test, but requests strict JDK wording on exception (via -strictJDK)
+func TestRunLDIVexceptionStrictJDK(t *testing.T) {
+	if testing.Short() { // don't run if running quick tests only. (Used primarily so GitHub doesn't run and bork)
+		t.Skip()
+	}
+
+	initErr := initVarsLDIVexception()
+	if initErr != nil {
+		t.Fatalf("Test failure due to: %s", initErr.Error())
+	}
+	var cmd *exec.Cmd
+
+	if testing.Short() { // don't run if running quick tests only. (Used primarily so GitHub doesn't run and bork)
+		t.Skip()
+	}
+
+	_JVM_ARGS = "-strictJDK"
+	// run the various combinations of args. This is necessary b/c the empty string is viewed as
+	// an actual specified option on the command line.
+	if len(_JVM_ARGS) > 0 {
+		if len(_APP_ARGS) > 0 {
+			cmd = exec.Command(_JACOBIN, _JVM_ARGS, _TESTCLASS, _APP_ARGS)
+		} else {
+			cmd = exec.Command(_JACOBIN, _JVM_ARGS, _TESTCLASS)
+		}
+	} else {
+		if len(_APP_ARGS) > 0 {
+			cmd = exec.Command(_JACOBIN, _TESTCLASS, _APP_ARGS)
+		} else {
+			cmd = exec.Command(_JACOBIN, _TESTCLASS)
+		}
+	}
+
+	// get the stdout and stderr contents from the file execution
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// run the command
+	if err = cmd.Start(); err != nil {
+		t.Errorf("Got error running Jacobin: %s", err.Error())
+	}
+
+	// Here begin the actual tests on the output to stderr and stdout
+	slurp, _ := io.ReadAll(stderr)
+	if len(slurp) == 0 {
+		t.Errorf("Did not get error output to stderr")
+	}
+
+	if !strings.Contains(string(slurp), "java.lang.ArithmeticException") &&
+		!strings.Contains(string(slurp), "/ by zero") { // JDK wording
+		t.Errorf("Did not get expected output to stderr. Got: %s", string(slurp))
+	}
+}
