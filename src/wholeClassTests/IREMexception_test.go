b44e4778ee3319dc743ca0dca6ed3a5393cae570
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2024-02-07 11:43:34 -0800

    JACOBIN-442 Added tests for IREM divide-by-zero exception

---
+++ src/wholeClassTests/IREMexception_test.go
@@ -0,0 +1,173 @@
+/*
+ * Jacobin VM - A Java virtual machine
+ * Copyright (c) 2024 by the Jacobin authors. All rights reserved.
+ * Licensed under Mozilla Public License 2.0 (MPL 2.0)
+ */
+
+package wholeClassTests
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+/*
+ * Tests for ThrowIREMexception.class. Source code:
+ *
+ *  // division by zero to throw exception
+ *  public final class ThrowLDIVexception {
+ *      public static void main(String[] args) {
+ *          int n = 6;
+ *	        int x = 0;
+ *	        int y = n%x;
+ *      }
+ *  }
+ *
+ * This test checks the output with various options for verbosity and features set on the command line.
+ */
+
+// To run your class, enter its name in _TESTCLASS, any args in their respective variables and then run the tests.
+// This test harness expects that environmental variable JACOBIN_EXE gives the full name and path of the executable
+// we're running the tests on. The folder which contains the test class should be specified in the environmental
+// variable JACOBIN_TESTDATA (without a terminating slash).
+func initVarsIREMexception() error {
+	if testing.Short() { // don't run if running quick tests only. (Used primarily so GitHub doesn't run and bork)
+		return fmt.Errorf("test not run due to -short")
+	}
+
+	_JACOBIN = os.Getenv("JACOBIN_EXE") // returns "" if JACOBIN_EXE has not been specified.
+	_JVM_ARGS = ""
+	_TESTCLASS = "ThrowIREMexception.class" // the class to test
+	_APP_ARGS = ""
+
+	if _JACOBIN == "" {
+		return fmt.Errorf("missing Jacobin executable. Please specify it in JACOBIN_EXE")
+	} else if _, err := os.Stat(_JACOBIN); err != nil {
+		return fmt.Errorf("missing Jacobin executable, which was specified as %s", _JACOBIN)
+	}
+
+	if _TESTCLASS != "" {
+		testClass := os.Getenv("JACOBIN_TESTDATA") + string(os.PathSeparator) + _TESTCLASS
+		if _, err := os.Stat(testClass); err != nil {
+			return fmt.Errorf("missing class to test, which was specified as %s", testClass)
+		} else {
+			_TESTCLASS = testClass
+		}
+	}
+	return nil
+}
+
+func TestRunIREMexception(t *testing.T) {
+	if testing.Short() { // don't run if running quick tests only. (Used primarily so GitHub doesn't run and bork)
+		t.Skip()
+	}
+
+	initErr := initVarsLDIVexception()
+	if initErr != nil {
+		t.Fatalf("Test failure due to: %s", initErr.Error())
+	}
+	var cmd *exec.Cmd
+
+	if testing.Short() { // don't run if running quick tests only. (Used primarily so GitHub doesn't run and bork)
+		t.Skip()
+	}
+
+	// run the various combinations of args. This is necessary b/c the empty string is viewed as
+	// an actual specified option on the command line.
+	if len(_JVM_ARGS) > 0 {
+		if len(_APP_ARGS) > 0 {
+			cmd = exec.Command(_JACOBIN, _JVM_ARGS, _TESTCLASS, _APP_ARGS)
+		} else {
+			cmd = exec.Command(_JACOBIN, _JVM_ARGS, _TESTCLASS)
+		}
+	} else {
+		if len(_APP_ARGS) > 0 {
+			cmd = exec.Command(_JACOBIN, _TESTCLASS, _APP_ARGS)
+		} else {
+			cmd = exec.Command(_JACOBIN, _TESTCLASS)
+		}
+	}
+
+	// get the stdout and stderr contents from the file execution
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// run the command
+	if err = cmd.Start(); err != nil {
+		t.Errorf("Got error running Jacobin: %s", err.Error())
+	}
+
+	// Here begin the actual tests on the output to stderr and stdout
+	slurp, _ := io.ReadAll(stderr)
+	if len(slurp) == 0 {
+		t.Errorf("Did not get error output to stderr")
+	}
+
+	if !strings.Contains(string(slurp), "java.lang.ArithmeticException") &&
+		!strings.Contains(string(slurp), "IREM") {
+		t.Errorf("Did not get expected output to stderr. Got: %s", string(slurp))
+	}
+}
+
+// same as previous test, but requests strict JDK wording on exception (via -strictJDK)
+func TestRunIREMexceptionStrictJDK(t *testing.T) {
+	if testing.Short() { // don't run if running quick tests only. (Used primarily so GitHub doesn't run and bork)
+		t.Skip()
+	}
+
+	initErr := initVarsLDIVexception()
+	if initErr != nil {
+		t.Fatalf("Test failure due to: %s", initErr.Error())
+	}
+	var cmd *exec.Cmd
+
+	if testing.Short() { // don't run if running quick tests only. (Used primarily so GitHub doesn't run and bork)
+		t.Skip()
+	}
+
+	_JVM_ARGS = "-strictJDK"
+	// run the various combinations of args. This is necessary b/c the empty string is viewed as
+	// an actual specified option on the command line.
+	if len(_JVM_ARGS) > 0 {
+		if len(_APP_ARGS) > 0 {
+			cmd = exec.Command(_JACOBIN, _JVM_ARGS, _TESTCLASS, _APP_ARGS)
+		} else {
+			cmd = exec.Command(_JACOBIN, _JVM_ARGS, _TESTCLASS)
+		}
+	} else {
+		if len(_APP_ARGS) > 0 {
+			cmd = exec.Command(_JACOBIN, _TESTCLASS, _APP_ARGS)
+		} else {
+			cmd = exec.Command(_JACOBIN, _TESTCLASS)
+		}
+	}
+
+	// get the stdout and stderr contents from the file execution
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// run the command
+	if err = cmd.Start(); err != nil {
+		t.Errorf("Got error running Jacobin: %s", err.Error())
+	}
+
+	// Here begin the actual tests on the output to stderr and stdout
+	slurp, _ := io.ReadAll(stderr)
+	if len(slurp) == 0 {
+		t.Errorf("Did not get error output to stderr")
+	}
+
+	if !strings.Contains(string(slurp), "java.lang.ArithmeticException") &&
+		!strings.Contains(string(slurp), "/ by zero") { // JDK wording
+		t.Errorf("Did not get expected output to stderr. Got: %s", string(slurp))
+	}
+}
---
+++ testdata/ThrowIREMexception.java
@@ -0,0 +1,8 @@
+// division by zero to throw exception
+public final class ThrowIREMexception {
+    public static void main(String[] args) {
+        int n = 6;
+	int x = 0;
+	int y = n%x;
+    }
+}
