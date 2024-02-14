6192b893951dfb5fab3a06a936bb58bba1f40044
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2024-02-13 23:55:30 -0800

    JACOBIN-418 Added unit test for gfunction error block

--- src/gfunction/javaLangString_test.go
+++ src/gfunction/javaLangString_test.go
@@ -7,16 +7,37 @@
 package gfunction
 
 import (
+	"jacobin/classloader"
+	"jacobin/exceptions"
 	"jacobin/object"
+	"strings"
 	"testing"
 )
 
+func TestStringClinit(t *testing.T) {
+	classloader.InitMethodArea()
+	retval := stringClinit(nil)
+	if retval == nil {
+		t.Error("Was expecting an error message, but got none.")
+	}
+	switch retval.(type) {
+	case *GErrBlk:
+		gErr := retval.(*GErrBlk)
+		if !strings.Contains(gErr.ErrMsg, "stringClinit: Could not find java/lang/String") {
+			t.Errorf("Unexpected error message. got %s", gErr.ErrMsg)
+		}
+		if gErr.ExceptionType != exceptions.VirtualMachineError {
+			t.Errorf("Unexpected exception type. got %d", gErr.ExceptionType)
+		}
+	default:
+		t.Errorf("Did not get expected error message, got %v", retval)
+	}
+}
 func TestStringToUpperCase(t *testing.T) {
 	originalString := "hello"
 	s := object.CreateCompactStringFromGoString(&originalString)
 	params := []interface{}{s}
 	s2 := toUpperCase(params)
-	// sUpper := s2.(*object.Object).Fields[0].Fvalue.(*[]byte)
 	sUpper := object.GetGoStringFromJavaStringPtr(s2.(*object.Object))
 	if string(sUpper) != "HELLO" {
 		t.Errorf("string ToUpperCase failed, got %s", sUpper)
