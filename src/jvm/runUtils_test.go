dd68e6c969446c5bbe52fa2f6d4f6c0c08c029c2
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2024-03-25 21:38:46 -0700

    JACOBIN-483 Added a test

---
+++ src/jvm/runUtils_test.go
@@ -0,0 +1,25 @@
+/*
+ * Jacobin VM - A Java virtual machine
+ * Copyright (c) 2024 by the Jacobin Authors. All rights reserved.
+ * Licensed under Mozilla Public License 2.0 (MPL 2.0)  Consult jacobin.org.
+ */
+
+package jvm
+
+import "testing"
+
+// tests for runUtils.go. Note that most functions are tested inside the tests for run.go,
+// but several benefit from standalone testing. Those are tested here
+
+func TestConvertBoolByteToInt64(t *testing.T) {
+	var bite = byte(0x01)
+	res := convertByteOrBoolToInt64(bite)
+	if res != 1 {
+		t.Errorf("convertBoolByteToInt64(byte), expected = 1, got %d", res)
+	}
+
+	yesNo := true
+	if convertByteOrBoolToInt64(yesNo) != 1 {
+		t.Errorf("convertBoolByteToInt64(bool) != 1 (true), got %d", res)
+	}
+}
