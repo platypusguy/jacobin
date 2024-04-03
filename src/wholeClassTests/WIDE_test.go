086d457548fd9b41f59deffaa1b12de8a2572b37
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2024-04-02 23:01:50 -0700

    JACOBIN-487 Added wholeClassTest for WIDE bytecode.

---
+++ src/wholeClassTests/WIDE_test.go
@@ -0,0 +1,105 @@
+/*
+ * Jacobin VM - A Java virtual machine
+ * Copyright (c) 2022 by the Jacobin authors. All rights reserved.
+ * Licensed under Mozilla Public License 2.0 (MPL 2.0)
+ */
+
+package wholeClassTests
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+/*
+ * Tests for WIDE bytecode. It's a lot of Javacode, so not reproduced here but available in testWIDE.java
+ */
+
+// To run your class, enter its name in _TESTCLASS, any args in their respective variables and then run the tests.
+// This test harness expects that environmental variable JACOBIN_EXE gives the full name and path of the executable
+// we're running the tests on. The folder which contains the test class should be specified in the environmental
+// variable JACOBIN_TESTDATA (without a terminating slash).
+
+func initVarsWIDE() error {
+	if testing.Short() { // don't run if running quick tests only. (Used primarily so GitHub doesn't run and bork)
+		return fmt.Errorf("test not run due to -short")
+	}
+
+	_JACOBIN = os.Getenv("JACOBIN_EXE") // returns "" if JACOBIN_EXE has not been specified.
+	_JVM_ARGS = ""
+	_TESTCLASS = "testWIDE.class" // the class to test
+	_APP_ARGS = ""
+
+	if _JACOBIN == "" {
+		return fmt.Errorf("test failure due to missing Jacobin executable. Please specify it in JACOBIN_EXE")
+	} else if _, err := os.Stat(_JACOBIN); err != nil {
+		return fmt.Errorf("missing Jacobin executable, which was specified as %s", _JACOBIN)
+	}
+
+	if _TESTCLASS != "" {
+		testClass := os.Getenv("JACOBIN_TESTDATA") + string(os.PathSeparator) + _TESTCLASS
+		if _, err := os.Stat(testClass); err != nil {
+			return fmt.Errorf("missing class to test, which was specified as %s", testClass)
+		} else {
+			_TESTCLASS = testClass
+		}
+	}
+	return nil
+}
+
+func TestRunWIDE(t *testing.T) {
+	if testing.Short() { // don't run if running quick tests only. (Used primarily so GitHub doesn't run and bork)
+		t.Skip()
+	}
+
+	initErr := initVarsWIDE()
+	if initErr != nil {
+		t.Fatalf("Test failure due to: %s", initErr.Error())
+	}
+
+	var cmd *exec.Cmd
+	// run the various combinations of args. This is necessary b/c the empty string is viewed as
+	// an actual specified option on the command line.
+	if len(_JVM_ARGS) > 0 {
+		if len(_APP_ARGS) > 0 {
+			cmd = exec.Command(_JACOBIN, _JVM_ARGS, _TESTCLASS, _APP_ARGS)
+		} else {
+			cmd = exec.Command(_JACOBIN, _JVM_ARGS, _TESTCLASS)
+		}
+	} else {
+		if len(_APP_ARGS) > 0 {
+			cmd = exec.Command(_JACOBIN, _TESTCLASS, _APP_ARGS)
+		} else {
+			cmd = exec.Command(_JACOBIN, _TESTCLASS)
+		}
+	}
+
+	// get the stdout and stderr contents from the file execution
+	stderr, err := cmd.StderrPipe()
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// run the command
+	if err = cmd.Start(); err != nil {
+		t.Errorf("Got error running Jacobin: %s", err.Error())
+	}
+
+	// Here begin the actual tests on the output to stderr and stdout
+	slurp, _ := io.ReadAll(stderr)
+	if len(slurp) != 0 {
+		t.Errorf("Got unexpected output to stderr: %s", string(slurp))
+	}
+
+	slurp, _ = io.ReadAll(stdout)
+
+	if !strings.Contains(string(slurp), "total should be 3, is 3") {
+		t.Errorf("Did not get expected output to stdout. Got: %s", string(slurp))
+	}
+}
