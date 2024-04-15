8eceabdb006902f1c2fde633ba15ab8a75f81118
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2024-04-14 20:38:39 -0700

    JACOBIN-476 Setting up file structure for Java implementation of java.system.arraycopy()

---
+++ src/lib/java_lang/system.go
@@ -0,0 +1,13 @@
+/*
+ * Jacobin VM - A Java virtual machine
+ * Copyright (c) 2024 by the Jacobin Authors. All rights reserved.
+ * Licensed under Mozilla Public License 2.0 (MPL 2.0)  Consult jacobin.org.
+ */
+
+package java_lang
+
+// Contains java.lang.System methods that can be implemented in Java.
+// Note that the majority of this class's methods are native and implemented
+// in jacobin/gfuction/javaLangSystem.go.
+
+// TODO: arraycopy()
