c721aa3fd8e1046cb258de18f806e6319d727cf6
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2024-04-26 21:37:23 -0700

    JACOBIN-497 Added explanatory comments.

--- src/native/nativeFunctions.go
+++ src/native/nativeFunctions.go
@@ -6,17 +6,31 @@
 
 package native
 
-func IsUnsupportedNativeMethod(methodName string) bool {
-	return false
-}
+// In this file we handle how we flag native methods that Jacobin does not
+// or cannot run. Eventually, many of these will be replaced by functions
+// written in golang.
 
+// list where we add unsupported native methods and delete them as we implement them
 var unsupportedNativeMethodsList = []string{
-	"test.entry",
+	"test.entry", // for testing only
 	"java/util/zip/Adler32.update",
 }
 
+// UnsupportedNativeMethods is the look up table. The value field is unused.
+// We're only interested in looking up the key, whose presence in the table
+// identifies the method as native and unsupported.
 var UnsupportedNativeMethods = make(map[string]interface{})
 
+// IsUnsupportedNativeMethod looks up the method in the table of
+// unsupported native methods. If it's there it returns true, otherwise false.
+// Note that the method name includes the class: /class/name.method
+func IsUnsupportedNativeMethod(methodName string) bool {
+	_, ok := UnsupportedNativeMethods[methodName]
+	return ok
+}
+
+// LoadUnsupportedNativeMethods loads the list of unsupported methods into
+// the lookup table. Called at JVM start-up.
 func LoadUnsupportedNativeMethods() int64 {
 	for _, methodName := range unsupportedNativeMethodsList {
 		UnsupportedNativeMethods[methodName] = nil
