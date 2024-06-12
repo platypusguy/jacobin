d568c4f02b148e3406df4534c61376698d56d518
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2024-06-11 21:41:25 -0700

    JACOBIN-527 Continued table-driven logic to drive gfunction test template.

--- src/jvm/gfunctionExec_test.go
+++ src/jvm/gfunctionExec_test.go
@@ -124,7 +124,116 @@ func TestGfunWith0or1StringsTable(t *testing.T) {
 
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
+			var objClassName string // e.g. "java/io/PrintStream"
+			var methName string     // e.g. "println"
+			var methType string     // e.g. "(Ljava/lang/String;)V"
+			var stringParam string  // e.g. "test string"
+			var stderrExpected string
+			var stdoutExpected string
 
+			// ---------------------------------
+			objClassName = test.objName
+			methName = test.methName
+			methType = test.methType
+			stringParam = test.stringParam
+			stderrExpected = test.stderrText
+			stdoutExpected = test.stdoutText
+			// ---------------------------------
+
+			globals.InitGlobals("test")
+			log.Init()
+
+			// redirect stderr and stdout
+			normalStderr := os.Stderr
+			rerr, werr, _ := os.Pipe()
+			os.Stderr = werr
+
+			normalStdout := os.Stdout
+			rout, wout, _ := os.Pipe()
+			os.Stdout = wout
+
+			classloader.MTable = make(map[string]classloader.MTentry)
+			gfunction.MTableLoadGFunctions(&classloader.MTable)
+
+			CP := classloader.CPool{}
+			CP.CpIndex = make([]classloader.CpEntry, 10)
+			CP.CpIndex[0] = classloader.CpEntry{Type: 0, Slot: 0}
+			CP.CpIndex[1] = classloader.CpEntry{Type: classloader.MethodRef, Slot: 0}
+			CP.CpIndex[2] = classloader.CpEntry{Type: classloader.ClassRef, Slot: 0}
+			CP.CpIndex[3] = classloader.CpEntry{Type: classloader.NameAndType, Slot: 0}
+			CP.CpIndex[4] = classloader.CpEntry{Type: classloader.UTF8, Slot: 0}
+			CP.CpIndex[5] = classloader.CpEntry{Type: classloader.UTF8, Slot: 1}
+			CP.CpIndex[6] = classloader.CpEntry{Type: classloader.StringConst, Slot: 7}
+			CP.CpIndex[7] = classloader.CpEntry{Type: classloader.UTF8, Slot: 2} // point to UTF8[2]
+
+			CP.MethodRefs = make([]classloader.MethodRefEntry, 1)
+			methRef := classloader.MethodRefEntry{
+				ClassIndex:  2, // these are CP entries
+				NameAndType: 3,
+			}
+			CP.MethodRefs[0] = methRef
+
+			CP.ClassRefs = append(CP.ClassRefs, stringPool.GetStringIndex(&objClassName))
+
+			CP.Utf8Refs = append(CP.Utf8Refs, methName)
+			CP.Utf8Refs = append(CP.Utf8Refs, methType)
+			CP.Utf8Refs = append(CP.Utf8Refs, stringParam)
+			nAndT := classloader.NameAndTypeEntry{
+				NameIndex: uint16(4),
+				DescIndex: uint16(5),
+			}
+			CP.NameAndTypes = append(CP.NameAndTypes, nAndT)
+
+			var f frames.Frame
+			if stringParam != "" {
+				f = newFrame(opcodes.LDC)
+				f.Meth = append(f.Meth, uint8(6)) // point to the string constant parameter indexed by CPindex[6]
+				f.Meth = append(f.Meth, opcodes.INVOKEVIRTUAL)
+			} else {
+				f = newFrame(opcodes.INVOKEVIRTUAL)
+			}
+			f.Meth = append(f.Meth, 0x00)
+			f.Meth = append(f.Meth, 0x01) // Go to method referred to in 0x0001 of the CP
+
+			f.CP = &CP
+
+			// create the opStack
+			for j := 0; j < 10; j++ {
+				f.OpStack = append(f.OpStack, 0)
+			}
+			f.OpStack[0] = os.Stdout // <<<<<<<<<<< need to determine what to do here
+			f.TOS = 0
+
+			fs := frames.CreateFrameStack()
+			fs.PushFront(&f) // push the new frame
+			_ = runFrame(fs)
+
+			// get contents written by stderr and stdout, then
+			// restore stderr and stdout to what they were before the test
+			_ = werr.Close()
+			rawStderrMsg, _ := io.ReadAll(rerr)
+			os.Stderr = normalStderr
+
+			_ = wout.Close()
+			rawStdoutMsg, _ := io.ReadAll(rout)
+			os.Stdout = normalStdout
+
+			// convert the contents written to stderr and stdout into strings
+			// and run tests on those strings
+
+			errMsg := string(rawStderrMsg[:])
+			if len(errMsg) != 0 && errMsg != test.stderrText {
+				t.Errorf("gfunctionExec: Test %s, expected error msg: %s, got: %s",
+					name, stderrExpected, errMsg)
+			}
+
+			outMsg := string(rawStdoutMsg[:])
+			if len(outMsg) != 0 && outMsg != test.stdoutText {
+				if !strings.Contains(outMsg, test.stdoutText) {
+					t.Errorf("gfunctionExec: Test %s, expected output: %s, got: %s",
+						name, stdoutExpected, outMsg)
+				}
+			}
 		})
 	}
 }
