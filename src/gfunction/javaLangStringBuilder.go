5b3d968e0c06c670f32c26782013cde139d839b1
Richard Elkins <richard.elkins@gmail.com>
2024-07-17 17:06:17 -0500

    JACOBIN-553 remove justReturn calls in StringBuilder

--- src/gfunction/javaLangStringBuilder.go
+++ src/gfunction/javaLangStringBuilder.go
@@ -7,7 +7,6 @@
 package gfunction
 
 import (
-	"jacobin/object"
 	"jacobin/types"
 )
 
@@ -21,27 +20,6 @@ func Load_Lang_StringBuilder() {
 			GFunction:  isLatin1,
 		}
 
-	MethodSignatures["java/lang/StringBuilder.<clinit>()V"] =
-		GMeth{
-			ParamSlots: 0,
-			GFunction:  justReturn,
-		}
-
-	MethodSignatures["java/lang/StringBuilder.<init>()V"] =
-		GMeth{
-			ParamSlots: 0,
-			GFunction:  stringBuilderInitEmpty,
-		}
-
-}
-
-// Instantiate a new empty StringBuilder - "java/lang/StringBuilder.<init>()V"
-func stringBuilderInitEmpty(params []interface{}) interface{} {
-	// params[0] = target object for string (updated)
-	obj := params[0].(*object.Object)
-	bytes := make([]byte, 0)
-	object.UpdateValueFieldFromBytes(obj, bytes)
-	return nil
 }
 
 // "java/lang/StringBuilder.isLatin1()Z"
