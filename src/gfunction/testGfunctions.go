8ef4b2bf926e2929392e1e4c13844b5501355ea6
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2024-07-19 10:45:42 -0700

    JACOBIN-556 First file for holding test gfunctions

---
+++ src/gfunction/testGfunctions.go
@@ -0,0 +1,11 @@
+/*
+ * Jacobin VM - A Java virtual machine
+ * Copyright (c) 2024 by the Jacobin Authors. All rights reserved.
+ * Licensed under Mozilla Public License 2.0 (MPL 2.0)  Consult jacobin.org.
+ */
+
+package gfunction
+
+// This file contains test gfunctions for unit tests. They're primarily designed such
+// that you specify the variable types passed in and the return value. They do nothing
+// but accept the params and return what the signature promises
