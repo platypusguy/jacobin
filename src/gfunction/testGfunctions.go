3ec73ae44a918de28d00b05597483f4c5d380a8d
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2024-07-19 19:05:29 -0700

    JACOBIN-556 First cut at file for test gfunctions

--- src/gfunction/testGfunctions.go
+++ src/gfunction/testGfunctions.go
@@ -9,3 +9,60 @@ package gfunction
 // This file contains test gfunctions for unit tests. They're primarily designed such
 // that you specify the variable types passed in and the return value. They do nothing
 // but accept the params and return what the signature promises
+
+func Load_TestGfunctions() {
+
+	// === returning void
+	MethodSignatures["java/lang/Object.test(I)V"] =
+		GMeth{
+			ParamSlots: 1,
+			GFunction:  arrayCopy,
+		}
+
+	MethodSignatures["java/lang/Object.test(D)V"] =
+		GMeth{
+			ParamSlots: 1,
+			GFunction:  currentTimeMillis,
+		}
+
+	MethodSignatures["java/lang/Object.test(Ljava/lang/Object;)V"] =
+		GMeth{
+			ParamSlots: 1,
+			GFunction:  nanoTime,
+		}
+
+	// === returning int or double
+
+	MethodSignatures["java/lang/Object.test(I)I"] =
+		GMeth{
+			ParamSlots: 1,
+			GFunction:  exitI,
+		}
+
+	MethodSignatures["java/lang/Object.test(I)D"] =
+		GMeth{
+			ParamSlots: 0,
+			GFunction:  forceGC,
+		}
+
+	// === accepting reference to java/lang/Object and returning something
+
+	MethodSignatures["java/lang/Object.test(Ljava/lang/Object;)I"] =
+		GMeth{
+			ParamSlots: 1,
+			GFunction:  getProperty,
+		}
+
+	MethodSignatures["java/lang/Object.test(Ljava/lang/Object;)Ljava/lang/Object;"] =
+		GMeth{
+			ParamSlots: 0,
+			GFunction:  getConsole,
+		}
+
+	MethodSignatures["java/lang/Object.test(Ljava/lang/Object;)D"] =
+		GMeth{
+			ParamSlots: 0,
+			GFunction:  clinit,
+		}
+
+}
