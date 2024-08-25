51b015d88914bb720b75c8e5eb29f78c08d0891a
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2024-08-24 23:56:55 -0700

    JACOBIN-573 Explanatory comments

--- src/config/buildno.go
+++ src/config/buildno.go
@@ -1,10 +1,10 @@
 /*
  * Jacobin VM - A Java virtual machine
- * Copyright (c) 2024 by  the Jacobin Authors. All rights reserved.
+ * Copyright (c) 2024 by the Jacobin Authors. All rights reserved.
  * Licensed under Mozilla Public License 2.0 (MPL 2.0)  Consult jacobin.org.
  */
 
- // THIS FILE IS MACHINE GENERATED. DO NOT EDIT!
+// THIS FILE IS MACHINE GENERATED. DO NOT EDIT!
 
 package config
 
--- src/config/version.go
+++ src/config/version.go
@@ -1,6 +1,6 @@
 /*
  * Jacobin VM - A Java virtual machine
- * Copyright (c) 2024 by  the Jacobin Authors. All rights reserved.
+ * Copyright (c) 2024 by the Jacobin Authors. All rights reserved.
  * Licensed under Mozilla Public License 2.0 (MPL 2.0)  Consult jacobin.org.
  */
 
@@ -10,6 +10,9 @@ import "strconv"
 
 var JacobinVersion = "0.5.003"
 
+// GetJacobinVersion returns a manually updated version number and an
+// automatically updated build #. The latter being updated by bumpbuildno.go
+// on the build cycles of @alb's build system.
 func GetJacobinVersion() string {
 	return JacobinVersion + " Build " + strconv.Itoa(BuildNo)
 }
