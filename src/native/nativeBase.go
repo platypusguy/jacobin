777d0e572a721dc0982af94393f2fc27dbf23cb5
Richard Elkins <richard.elkins@gmail.com>
2024-09-11 14:09:05 -0500

    JACOBIN-582 add comments and fix bugs

--- src/native/nativeBase.go
+++ src/native/nativeBase.go
@@ -41,21 +41,41 @@ type NativeErrBlk struct {
 }
 
 func getFuncHandle(methodName, methodType string) interface{} {
+	var funcHandle uintptr
+	var xrefEntry typeNxref
+	var ok bool
+	var err error
+
+	// Form the key for nfuncTable.
 	functionKey := methodName + methodType
-	funcHandle, ok := nfuncTable[functionKey]
+	funcHandle, ok = nfuncTable[functionKey]
 	if !ok {
-		libHandle := xrefTable[functionKey].LibHandle
-		funcHandle, err := purego.Dlsym(libHandle, methodName)
-		if err != nil {
-			libPath := xrefTable[functionKey].LibPath
+
+		// Not yet registered in nfuncTable.
+		// Get the library handle. This should be already available.
+		xrefEntry, ok = xrefTable[functionKey]
+		if !ok {
+			errMsg := fmt.Sprintf("Function %s is not in the XREF", functionKey)
+			return NativeErrBlk{ExceptionType: excNames.VirtualMachineError, ErrMsg: errMsg}
+		}
+
+		// Get the function handle.
+		funcHandle, err = purego.Dlsym(xrefEntry.LibHandle, methodName)
+		if err != nil { // Purego detected an error.
+			libPath := xrefEntry.LibPath
 			errMsg := fmt.Sprintf("purego.Dlsym(%s : %s) failed, reason: %s", libPath, functionKey, err.Error())
 			return NativeErrBlk{ExceptionType: excNames.VirtualMachineError, ErrMsg: errMsg}
 		}
-		if funcHandle == 0 {
-			libPath := xrefTable[functionKey].LibPath
+		if funcHandle == 0 { // Purego could not find the function name.
+			libPath := xrefEntry.LibPath
 			errMsg := fmt.Sprintf("purego.Dlsym(%s : %s) function not found", libPath, functionKey)
 			return NativeErrBlk{ExceptionType: excNames.VirtualMachineError, ErrMsg: errMsg}
 		}
+
+		// Update nfuncTable with the function handle.
+		nfuncTable[functionKey] = funcHandle
 	}
+
+	// Return the function handle.
 	return funcHandle
 }
