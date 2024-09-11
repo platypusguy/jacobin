ed96eb1a05847e1340fd2a51b8f04caf5a023241
Richard Elkins <richard.elkins@gmail.com>
2024-09-11 13:26:15 -0500

    JACOBIN-582 added native table defs and getFuncHandle()

---
+++ src/native/nativeBase.go
@@ -0,0 +1,61 @@
+/*
+
+During initialization,
+* The xrefTable is built by either a POSIX loader or a Windows loader. Note that both the library path and handle are populated.
+* The methodsTable remains nil.
+
+At run-time, RunNativeFunction will do the following in order to get a native function handle:
+* Form the functionKey = methodName concatenated with methodType.
+* Look up the functionKey in the nfuncTable.
+* If not found,
+     - Look up functionKey in xrefTable.
+     - Not found ---> error.
+     - Use the libHandle to get the function handle.
+     - Failure (E.g. not found) ---> error.
+     - Store the function handle in nfuncTable.
+* Use the function handle for the function call.
+
+*/
+
+package native
+
+import (
+	"fmt"
+	"github.com/ebitengine/purego"
+	"jacobin/excNames"
+)
+
+var nfuncTable = map[string]uintptr{} // Functions encountered and therefore have a handle
+
+type typeNxref struct {
+	libPath   string
+	libHandle uintptr
+}
+
+var xrefTable = map[string]typeNxref{} // Function-to-library cross reference table
+
+// Native function error block.
+type NativeErrBlk struct {
+	ExceptionType int
+	ErrMsg        string
+}
+
+func getFuncHandle(methodName, methodType string) interface{} {
+	functionKey := methodName + methodType
+	funcHandle, ok := nfuncTable[functionKey]
+	if !ok {
+		libHandle := xrefTable[functionKey].libHandle
+		funcHandle, err := purego.Dlsym(libHandle, methodName)
+		if err != nil {
+			libPath := xrefTable[functionKey].libPath
+			errMsg := fmt.Sprintf("purego.Dlsym(%s : %s) failed, reason: %s", libPath, functionKey, err.Error())
+			return NativeErrBlk{ExceptionType: excNames.VirtualMachineError, ErrMsg: errMsg}
+		}
+		if funcHandle == 0 {
+			libPath := xrefTable[functionKey].libPath
+			errMsg := fmt.Sprintf("purego.Dlsym(%s : %s) function not found", libPath, functionKey)
+			return NativeErrBlk{ExceptionType: excNames.VirtualMachineError, ErrMsg: errMsg}
+		}
+	}
+	return funcHandle
+}
