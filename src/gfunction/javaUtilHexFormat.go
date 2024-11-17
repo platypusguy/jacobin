052da8b4467cad8ec3c66074a3f293501b721663
Richard Elkins <richard.elkins@gmail.com>
2024-11-17 11:48:33 -0600

    JACOBIN-610 added last 2 static fields: HEX_FORMAT and EMPTY_BYTES

--- src/gfunction/javaUtilHexFormat.go
+++ src/gfunction/javaUtilHexFormat.go
@@ -6,7 +6,11 @@
 
 package gfunction
 
-import "jacobin/statics"
+import (
+	"jacobin/object"
+	"jacobin/statics"
+	"jacobin/types"
+)
 
 // Implementation of some of the functions in in Java/lang/Class.
 
@@ -56,8 +60,7 @@ func Load_Util_HexFormat() {
 
 }
 
-// hashMapHash accepts a pointer to an object and returns
-// a uint64 MD5 hash value of the pointed-to thing
+// <clinit> for class HexFormat
 func hexMapClinit(params []interface{}) interface{} {
 	DIGITS := []uint8{
 		255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
@@ -94,5 +97,33 @@ func hexMapClinit(params []interface{}) interface{} {
 	sLowercaseDigits := statics.Static{Type: "[B", Value: LOWERCASE_DIGITS}
 	statics.AddStatic("java/util/HexFormat.LOWERCASE_DIGITS", sLowercaseDigits)
 
+	obj := mkHexFormatObject("", "", "", LOWERCASE_DIGITS)
+	sHexFormat := statics.Static{Type: "java/util/HexFormat", Value: obj}
+	statics.AddStatic("java/util/HexFormat.HEX_FORMAT", sHexFormat)
+
+	sEmptyBytes := statics.Static{Type: "[B", Value: []byte{}}
+	statics.AddStatic("java/util/HexFormat.EMPTY_BYTES", sEmptyBytes)
+
 	return nil
+}
+
+// Make a new HexFormat object and return it to caller.
+func mkHexFormatObject(delimiter, prefix, suffix string, digits []byte) *object.Object {
+	var fld object.Field
+	className := "java/util/HexFormat"
+	obj := object.MakeEmptyObjectWithClassName(&className)
+
+	fld = object.Field{Ftype: types.ByteArray, Fvalue: []byte(delimiter)}
+	obj.FieldTable["delimiter"] = fld
+
+	fld = object.Field{Ftype: types.ByteArray, Fvalue: []byte(prefix)}
+	obj.FieldTable["prefix"] = fld
+
+	fld = object.Field{Ftype: types.ByteArray, Fvalue: []byte(suffix)}
+	obj.FieldTable["suffix"] = fld
+
+	fld = object.Field{Ftype: types.ByteArray, Fvalue: digits}
+	obj.FieldTable["digits"] = fld
+
+	return obj
 }
