43dabd1503f0fb825a009d54161e9d04ce3a85e3
Richard Elkins <richard.elkins@gmail.com>
2024-11-28 09:15:25 -0600

    JACOBIN-626 fix for DumpObject where a field is static

--- src/object/format.go
+++ src/object/format.go
@@ -189,6 +189,7 @@ func (objPtr *Object) DumpObject(title string, indent int) {
 	obj := *objPtr
 	output := ""
 	var klassString string
+	var className string
 
 	// Emit BEGIN
 	if indent > 0 {
@@ -201,7 +202,8 @@ func (objPtr *Object) DumpObject(title string, indent int) {
 		output += strings.Repeat(" ", indent)
 	}
 	if obj.KlassName != types.InvalidStringIndex {
-		klassString = "\tClass: " + *(stringPool.GetStringPointer(obj.KlassName))
+		className = *(stringPool.GetStringPointer(obj.KlassName))
+		klassString = "\tClass: " + className
 	} else {
 		klassString = "\t<class MISSING>"
 	}
@@ -229,7 +231,7 @@ func (objPtr *Object) DumpObject(title string, indent int) {
 			if !ok {
 				output += fmt.Sprintf("\t\t<ERROR nil FieldTable[%s] ptr>\n", fieldName)
 			} else {
-				str := fmtHelper(obj.FieldTable[fieldName], klassString, fieldName)
+				str := fmtHelper(obj.FieldTable[fieldName], className, fieldName)
 				output += fmt.Sprintf("\t\tFld %s: (%s) %s\n", fieldName, obj.FieldTable[fieldName].Ftype, str)
 			}
 		}
