6ecc780c2475939b2ea31d23a1e25472e71e158d
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2024-12-16 23:51:40 -0800

    JACOBIN-613 Updated javaUtilHashMap.go. Thanks to @rjelkins

--- src/gfunction/javaUtilHashMap.go
+++ src/gfunction/javaUtilHashMap.go
@@ -1,6 +1,6 @@
 /*
  * Jacobin VM - A Java virtual machine
- * Copyright (c) 2023 by  the Jacobin authors. Consult jacobin.org.
+ * Copyright (c) 2023-4 by  the Jacobin authors. Consult jacobin.org.
  * Licensed under Mozilla Public License 2.0 (MPL 2.0) All rights reserved.
  */
 
@@ -31,14 +31,21 @@ func Load_Util_HashMap() {
 // a uint64 MD5 hash value of the pointed-to thing
 func hashMapHash(params []interface{}) interface{} {
 	var hashValue uint64 = 0
-	var bytes []byte
+	var bytes []byte // MUST BE uint8 for md5.Sum
 	switch params[0].(type) {
 	case *object.Object:
 		obj := params[0].(*object.Object) // force golang to treat it as the object we know it to be
 		fld := obj.FieldTable["value"]
 		switch fld.Ftype {
 		case types.ByteArray:
-			bytes = obj.FieldTable["value"].Fvalue.([]byte)
+			fvalue := obj.FieldTable["value"].Fvalue
+			switch fvalue.(type) {
+			case []byte:
+				bytes = fvalue.([]byte)
+			default:
+				bytes = object.GoByteArrayFromJavaByteArray(fvalue.([]types.JavaByte))
+			}
+
 		case types.Bool, types.Byte, types.Char, types.Int, types.Long, types.Short:
 			bytes := make([]byte, 8)
 			binary.BigEndian.PutUint64(bytes, uint64(fld.Fvalue.(int64)))
