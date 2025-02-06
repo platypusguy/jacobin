6a2afe74924da40c88adde91a972a4345cb6c0a7
Richard Elkins <richard.elkins@gmail.com>
2025-02-06 12:54:28 -0600

    JACOBIN-538 cleanup jdkInternalMiscUnsafe.go error messages, especially origin of message

--- src/gfunction/jdkInternalMiscUnsafe.go
+++ src/gfunction/jdkInternalMiscUnsafe.go
@@ -81,7 +81,7 @@ var classUnsafeName = "jdk/internal/misc/Unsafe"
 func arrayBaseOffset(params []interface{}) interface{} {
 	p := params[0]
 	if p == nil || p == object.Null {
-		errMsg := "Object is a null pointer"
+		errMsg := "arrayBaseOffset: Object is a null pointer"
 		return getGErrBlk(excNames.NullPointerException, errMsg)
 	}
 	return int64(0) // this should work...
