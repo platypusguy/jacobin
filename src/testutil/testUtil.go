85bd25d93e54af8088d8b714f4730ed0506099e9
Richard Elkins <richard.elkins@gmail.com>
2025-02-27 12:56:37 -0600

    JACOBIN-674 unit test utilities and self-tests

---
+++ src/testutil/testUtil.go
@@ -0,0 +1,141 @@
+package testutil
+
+import (
+	"fmt"
+	"jacobin/classloader"
+	"jacobin/frames"
+	"jacobin/gfunction"
+	"jacobin/globals"
+	"jacobin/object"
+	"jacobin/statics"
+	"os"
+	"testing"
+)
+
+var flagInit = false
+
+// Standard unit test initialisation without processing stderr and stdout.
+func UTinit(t *testing.T) {
+	if flagInit {
+		return
+	}
+	globals.InitGlobals("test")
+	statics.Statics = make(map[string]statics.Static)
+	statics.PreloadStatics()
+	err := classloader.Init()
+	if err != nil {
+		t.Fatalf("stdInit: classloader.Init() failed, err: %s", err.Error())
+	}
+	classloader.LoadBaseClasses()
+	classloader.MTable = make(map[string]classloader.MTentry)
+	gfunction.MTableLoadGFunctions(&classloader.MTable)
+	flagInit = true
+}
+
+// NOT THREAD-SAFE.
+var originalStderr *os.File
+var originalStdout *os.File
+var testStderr *os.File
+var testStdout *os.File
+
+// Save current stdout and stderr.
+// Substitute 2
+func UTnewConsole(t *testing.T) {
+	var err error
+
+	// Swap in a new stdout.
+	originalStdout = os.Stdout
+	_, testStdout, err = os.Pipe()
+	if err != nil {
+		os.Stdout = originalStdout // Restore original stdout.
+		t.Fatalf("stdTestBegin: os.Pipe()-->stdout failed, err: %s", err.Error())
+	}
+	os.Stdout = testStdout
+
+	// Swap in a new stderr.
+	originalStderr = os.Stderr
+	_, testStderr, err = os.Pipe()
+	if err != nil {
+		_ = testStdout.Close()
+		os.Stdout = originalStdout // Restore original stdout.
+		os.Stderr = originalStderr // Restore original stderr.
+		t.Fatalf("stdTestBegin: os.Pipe()-->stderr failed, err: %s", err.Error())
+	}
+	os.Stderr = testStderr
+}
+
+// Close testing stderr & stdout; restore the original stderr & stdout.
+func UTrestoreConsole(t *testing.T) {
+	_ = testStderr.Close()
+	_ = testStdout.Close()
+	os.Stdout = originalStdout
+	os.Stderr = originalStderr
+}
+
+// Execute a G function.
+// Return result to caller.
+func UTgfunc(t *testing.T, className, methodName, methodType string, obj *object.Object, args []interface{}) interface{} {
+
+	// String form of FQN.
+	fqn := fmt.Sprintf("%s.%s%s", className, methodName, methodType)
+
+	// Initialize Jacobin infrastructure and set up new stdout and stderr.
+	UTinit(t)
+	UTnewConsole(t)
+
+	// Create empty frame stack (fs).
+	fs := frames.CreateFrameStack()
+
+	// Create frame (fr).
+	fr := frames.CreateFrame(3)
+	fr.Thread = 0 // Mainthread
+	fr.FrameStack = fs
+	fr.ClName = className
+	fr.MethName = methodName
+	fr.MethType = methodType
+
+	// Add CP to the frame.
+	CP := classloader.CPool{}
+	CP.CpIndex = make([]classloader.CpEntry, 10)
+	CP.CpIndex[0] = classloader.CpEntry{Type: 0, Slot: 0}
+	CP.CpIndex[1] = classloader.CpEntry{Type: classloader.ClassRef, Slot: 0}
+	CP.FieldRefs = make([]classloader.ResolvedFieldEntry, 1)
+	CP.FieldRefs[0] = classloader.ResolvedFieldEntry{
+		ClName:  "testClass",
+		FldName: "testField",
+		FldType: "I",
+	}
+	fr.CP = &CP
+
+	// Push fr to front of fs.
+	_ = frames.PushFrame(fs, fr)
+
+	// Create mtEntry.
+	mtEntry := classloader.MTable[fqn]
+	if mtEntry.Meth == nil {
+		t.Fatalf("UTgfunc: classloader.MTable[%s] not found", fqn)
+	}
+
+	paramCount := len(args)
+
+	// params = args in reverse order (expected by RunGfunction).
+	params := make([]interface{}, paramCount)
+	for ix := 0; ix < paramCount; ix++ {
+		params[ix] = args[paramCount-1-ix]
+	}
+
+	// Add the object reference (Java class or file I/O).
+	if obj != nil {
+		params = append(params, obj)
+	}
+
+	// Run the G function.
+	result := gfunction.RunGfunction(mtEntry, fs, className, methodName, methodType, &params, true, false)
+
+	// Restore previous stderr and stdout.
+	UTrestoreConsole(t)
+
+	// Return the result to caller.
+	return result
+
+}
---
+++ src/testutil/testUtil_test.go
@@ -0,0 +1,185 @@
+package testutil
+
+import (
+	"jacobin/gfunction"
+	"jacobin/object"
+	"math"
+	"testing"
+)
+
+// Unit test the unit test utilities.
+
+const loopCount = 20
+const absTolerance = 1e-9
+const relTolerance = 1e-6
+
+func float64CloseEnough(tweedleDee, tweedleDum float64) bool {
+	diff := math.Abs(tweedleDee - tweedleDum)
+
+	// Absolute difference check for very small values.
+	if diff <= absTolerance {
+		return true
+	}
+
+	// Compute the maximum of the magnitudes.
+	maxMagnitude := math.Max(math.Abs(tweedleDee), math.Abs(tweedleDum))
+
+	// Relative difference check for larger values.
+	return diff <= maxMagnitude*relTolerance
+}
+
+func TestUTinit(t *testing.T) {
+	for ix := 0; ix < loopCount; ix++ {
+		UTinit(t)
+	}
+}
+
+func TestUTconsoles(t *testing.T) {
+	for ix := 0; ix < loopCount; ix++ {
+		UTnewConsole(t)
+		UTrestoreConsole(t)
+	}
+}
+
+func TestUTensemble(t *testing.T) {
+	for ix := 0; ix < loopCount; ix++ {
+		UTinit(t)
+		UTnewConsole(t)
+		UTrestoreConsole(t)
+	}
+}
+
+func TestMathSqrt_D_D(t *testing.T) {
+	var params []interface{}
+	params = append(params, 4.0)
+	result := UTgfunc(t, "java/lang/Math", "sqrt", "(D)D", nil, params)
+	switch result.(type) {
+	case float64:
+		if !float64CloseEnough(result.(float64), 2.0) {
+			t.Errorf("TestMathSqrt_D_D: Expected sqrt(4)=2, observed: %f", result.(float64))
+		}
+	case gfunction.GErrBlk:
+		t.Errorf("TestMathSqrt_D_D: gfunction.GErrBlk.ErrMsg: %s", result.(gfunction.GErrBlk).ErrMsg)
+	default:
+		t.Errorf("TestMathSqrt_D_D: Expected result of type float64, observed: %T", result)
+	}
+}
+
+func TestStrictMathCbrt_D_D(t *testing.T) {
+	var params []interface{}
+	params = append(params, 27.0)
+	result := UTgfunc(t, "java/lang/StrictMath", "cbrt", "(D)D", nil, params)
+	switch result.(type) {
+	case float64:
+		if !float64CloseEnough(result.(float64), 3.0) {
+			t.Errorf("TestStrictMathCbrt_D_D: Expected cbrt(27)=3, observed: %f", result.(float64))
+		}
+	case gfunction.GErrBlk:
+		t.Errorf("TestStrictMathCbrt_D_D: gfunction.GErrBlk.ErrMsg: %s", result.(gfunction.GErrBlk).ErrMsg)
+	default:
+		t.Errorf("TestStrictMathCbrt_D_D: Expected result of type float64, observed: %T", result)
+	}
+}
+
+func TestStringLength(t *testing.T) {
+	var params []interface{}
+	obj := object.StringObjectFromGoString("ABCDEF")
+	result := UTgfunc(t, "java/lang/String", "length", "()I", obj, params)
+	switch result.(type) {
+	case int64:
+		if result.(int64) != 6 {
+			t.Errorf("TestStringLength: Expected length(\"ABCDEF\")=6, observed: %d", result.(int64))
+		}
+	case gfunction.GErrBlk:
+		t.Errorf("TestStringLength: gfunction.GErrBlk.ErrMsg: %s", result.(gfunction.GErrBlk).ErrMsg)
+	default:
+		t.Errorf("TestStringLength: Expected result of type int64, observed: %T", result)
+	}
+}
+
+func TestStringRepeater(t *testing.T) {
+	var params []interface{}
+	objIn := object.StringObjectFromGoString("Beetlejuice")
+	params = append(params, int64(3))
+	expected := "BeetlejuiceBeetlejuiceBeetlejuice"
+	result := UTgfunc(t, "java/lang/String", "repeat", "(I)Ljava/lang/String;", objIn, params)
+	switch result.(type) {
+	case *object.Object:
+		observed := object.GoStringFromStringObject(result.(*object.Object))
+		if observed != expected {
+			t.Errorf("TestStringRepeater: Expected: \"%s\", observed: \"%s\"", expected, observed)
+		}
+	case gfunction.GErrBlk:
+		t.Errorf("TestStringRepeater: gfunction.GErrBlk.ErrMsg: %s", result.(gfunction.GErrBlk).ErrMsg)
+	default:
+		t.Errorf("TestStringRepeater: Expected result of type String object, observed: %T", result)
+	}
+}
+
+func TestStringToCharArray(t *testing.T) {
+	var params []interface{}
+
+	// Here's the base string and its bytes equivalent.
+	expected := "ABCDEF"
+	bytes := []byte(expected)
+
+	// Make a Java object of the string.
+	objIn := object.StringObjectFromGoString(expected)
+
+	// Make an int64 array of the bytes.
+	var iArray []int64
+	for ix := 0; ix < len(expected); ix++ {
+		iArray = append(iArray, int64(bytes[ix]))
+	}
+
+	// Try it.
+	result := UTgfunc(t, "java/lang/String", "toCharArray", "()[C", objIn, params)
+	switch result.(type) {
+	case *object.Object:
+		observed := object.ObjectFieldToString(result.(*object.Object), "value")
+		if observed != expected {
+			t.Errorf("TestStringToCharArray: Expected: \"%s\", observed: \"%s\"", expected, observed)
+		}
+	case gfunction.GErrBlk:
+		t.Errorf("TestStringToCharArray: gfunction.GErrBlk.ErrMsg: %s", result.(gfunction.GErrBlk).ErrMsg)
+	default:
+		t.Errorf("TestStringToCharArray: Expected result of type [C object, observed: %T", result)
+	}
+}
+
+func TestBigInteger(t *testing.T) {
+
+	var params []interface{}
+	biClassName := "java/math/BigInteger"
+	expected := int64(42)
+
+	// Need to do an early initialisation before object.MakeEmptyObjectWithClassName).
+	UTinit(t)
+
+	// Initialise the base object.
+	obj := object.MakeEmptyObjectWithClassName(&biClassName)
+	gfunction.InitBigIntegerField(obj, int64(0))
+
+	// Add the String value to params.
+	params = append(params, object.StringObjectFromGoString("42"))
+
+	// Try java/math/BigInteger.<init>.(Ljava/lang/String;)V
+	result := UTgfunc(t, "java/math/BigInteger", "<init>", "(Ljava/lang/String;)V", obj, params)
+	if result != nil {
+		t.Fatalf("TestBigInteger: <init>: Expected nil return")
+	}
+
+	// Try intValue()
+	params = nil
+	result = UTgfunc(t, "java/math/BigInteger", "intValue", "()I", obj, params)
+	switch result.(type) {
+	case int64:
+		if result.(int64) != expected {
+			t.Errorf("TestStringToCharArray: Expected: \"%d\", observed: \"%d\"", expected, result.(int64))
+		}
+	case gfunction.GErrBlk:
+		t.Errorf("TestStringToCharArray: gfunction.GErrBlk.ErrMsg: %s", result.(gfunction.GErrBlk).ErrMsg)
+	default:
+		t.Errorf("TestStringToCharArray: Expected result of type [C object, observed: %T", result)
+	}
+}
