ef6c01dbe9d051e0b4969f0fb4bb2c932136dffc
Richard Elkins <richard.elkins@gmail.com>
2025-03-01 10:36:15 -0600

    I forgot that jacobin is not in the PATH for current workflow on GA

--- src/testutil/runner_test.go
+++ src/testutil/runner_test.go
@@ -9,24 +9,24 @@ func TestRcFromRunner(t *testing.T) {
 	var rc int
 	var outstr string
 
-	rc, outstr = Runner("jacobin", "hello.class", 10, false)
+	rc, outstr = Runner("java", "hello", 10, false)
 	if rc != RcRunnerSuccess {
 		t.Errorf("TestRcFromRunner: expected rc=%d, observed rc=%d, outstr=%s", RcRunnerSuccess, rc, outstr)
 	}
 
-	rc, outstr = Runner("jacobin", "hello.class", 0, false)
+	rc, outstr = Runner("java", "hello", 0, false)
 	if rc != RcRunnerTimeout {
 		t.Errorf("TestRcFromRunner: expected rc=%d, observed rc=%d, outstr=%s", RcRunnerTimeout, rc, outstr)
-	}
-
-	rc, outstr = Runner("jacobin", "-NotAnOption!      hello.class", 10, false)
-	if rc != RcRunnerFailure || !strings.Contains(outstr, "Usage: jacobin") {
-		t.Errorf("TestRcFromRunner: expected rc=%d, observed rc=%d, outstr=%s", RcRunnerFailure, rc, outstr)
 	}
 
 	rc, outstr = Runner("go", "version", 10, false)
 	if rc != RcRunnerSuccess {
 		t.Errorf("TestRcFromRunner: expected rc=%d, observed rc=%d, outstr=%s", RcRunnerSuccess, rc, outstr)
+	}
+
+	rc, outstr = Runner("go", "-NotAnOption!      hello.class", 10, false)
+	if rc != RcRunnerFailure || !strings.Contains(outstr, "Usage:") {
+		t.Errorf("TestRcFromRunner: expected rc=%d, observed rc=%d, outstr=%s", RcRunnerFailure, rc, outstr)
 	}
 
 }
