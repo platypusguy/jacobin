83f3e92b4b3beba4d10ac98cd824249786bb2ed2
Richard Elkins <richard.elkins@gmail.com>
2025-03-01 15:13:03 -0600

    got rid of java stuff

--- src/testutil/runner_test.go
+++ src/testutil/runner_test.go
@@ -9,16 +9,6 @@ func TestRcFromRunner(t *testing.T) {
 	var rc int
 	var outstr string
 
-	rc, outstr = Runner("java", "hello", 10, false)
-	if rc != RcRunnerSuccess {
-		t.Errorf("TestRcFromRunner: expected rc=%d, observed rc=%d, outstr=%s", RcRunnerSuccess, rc, outstr)
-	}
-
-	rc, outstr = Runner("java", "hello", 0, false)
-	if rc != RcRunnerTimeout {
-		t.Errorf("TestRcFromRunner: expected rc=%d, observed rc=%d, outstr=%s", RcRunnerTimeout, rc, outstr)
-	}
-
 	rc, outstr = Runner("go", "version", 10, false)
 	if rc != RcRunnerSuccess {
 		t.Errorf("TestRcFromRunner: expected rc=%d, observed rc=%d, outstr=%s", RcRunnerSuccess, rc, outstr)
@@ -27,6 +17,11 @@ func TestRcFromRunner(t *testing.T) {
 	rc, outstr = Runner("go", "-NotAnOption!      hello.class", 10, false)
 	if rc != RcRunnerFailure || !strings.Contains(outstr, "Usage:") {
 		t.Errorf("TestRcFromRunner: expected rc=%d, observed rc=%d, outstr=%s", RcRunnerFailure, rc, outstr)
+	}
+	
+	rc, outstr = Runner("go", "version", 0, false)
+	if rc != RcRunnerTimeout {
+		t.Errorf("TestRcFromRunner: expected rc=%d, observed rc=%d, outstr=%s", RcRunnerTimeout, rc, outstr)
 	}
 
 }
