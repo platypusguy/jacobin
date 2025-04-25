7103a02fb64e9a2ae5a37121cce504ed1b64bf12
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2025-04-24 17:42:49 -0700

    JACOBIN-702 Added copyright header to hashing.go

--- src/config/buildno.go
+++ src/config/buildno.go
@@ -8,4 +8,4 @@
 
 package config
 
-var BuildNo = 3673
+var BuildNo = 3675
--- src/util/hashing.go
+++ src/util/hashing.go
@@ -1,3 +1,8 @@
+/*
+ * Jacobin VM - A Java virtual machine
+ * Copyright (c) 2025 by the Jacobin Authors. All rights reserved.
+ * Licensed under Mozilla Public License 2.0 (MPL 2.0)  Consult jacobin.org.
+ */
 package util
 
 import (
