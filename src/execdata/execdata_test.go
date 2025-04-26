7c275a443bd770e3a0153b119e35d11073cb543a
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2025-04-25 20:53:47 -0700

    JACOBIN-726 Added unit tests for execdata.go

---
+++ src/execdata/execdata_test.go
@@ -0,0 +1,46 @@
+/*
+ * Jacobin VM - A Java virtual machine
+ * Copyright (c) 2025 by the Jacobin Authors. All rights reserved.
+ * Licensed under Mozilla Public License 2.0 (MPL 2.0)  Consult jacobin.org.
+ */
+
+package execdata
+
+import (
+	"io"
+	"jacobin/globals"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestGetExecBuildInfo(t *testing.T) {
+	globals.InitGlobals("test")
+	g := globals.GetGlobalRef()
+	GetExecBuildInfo(g)
+	if g.JacobinBuildData == nil || len(g.JacobinBuildData) == 0 {
+		t.Error("Expected JacobinBuildData to be populated")
+	}
+	t.Log(g.JacobinBuildData)
+}
+
+func TestPrintJacobinBuildData(t *testing.T) {
+	globals.InitGlobals("test")
+	g := globals.GetGlobalRef()
+	g.JacobinBuildData = nil
+
+	normalStderr := os.Stderr
+	r, w, _ := os.Pipe()
+	os.Stderr = w
+
+	PrintJacobinBuildData(g)
+
+	_ = w.Close()
+	out, _ := io.ReadAll(r)
+	os.Stderr = normalStderr
+	msg := string(out[:])
+
+	if !strings.Contains(msg, "Jacobin executable:") {
+		t.Errorf("Expecting different output from PrintJacobinBuildData(), got: %s", msg)
+	}
+}
