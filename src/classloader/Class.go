797a0613af26049a602ce57f7589b63384e86d3d
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2025-05-31 00:31:32 -0700

    JACOBIN-720 Filling in implementation of java/lang/Class instance

--- src/classloader/Class.go
+++ src/classloader/Class.go
@@ -8,24 +8,31 @@
 // which is a representation of a Java class used for reflection. It is not
 // how loaded classes are represented in Jacobin.
 
-package object
+package classloader
 
-import "jacobin/stringPool"
+import (
+	"fmt"
+	"jacobin/excNames"
+	"jacobin/object"
+	"jacobin/stringPool"
+	"jacobin/trace"
+	"jacobin/types"
+)
 
 type JavaLangClass struct {
-	Name        *string                // Fully qualified name of the class
-	Modifiers   int                    // Access modifiers (e.g., public, private, etc.)
-	Fields      map[string]*ClassField // Map of field names to Field objects
-	Methods     map[string]*Method     // Map of method names to Method objects
-	Interfaces  []string               // List of implemented interfaces
-	SuperClass  *JavaLangClass         // Reference to the superclass
-	Annotations map[string]*Annotation // Map of annotations
-	IsPrimitive bool                   // Indicates if the class represents a primitive type
-	IsArray     bool                   // Indicates if the class represents an array type
-	IsEnum      bool                   // Indicates if the class represents an enum type
-	IsInterface bool                   // Indicates if the class represents an interface
-	ClassLoader *Object                // Reference to the class loader
-	PackageName string                 // Package name of the class
+	Name        *string                 // Fully qualified name of the class
+	Modifiers   int                     // Access modifiers (e.g., public, private, etc.)
+	Fields      map[string]*ClassField  // Map of field names to Field objects
+	Methods     map[string]*ClassMethod // Map of method names to Method objects
+	Interfaces  []string                // List of implemented interfaces
+	SuperClass  *JavaLangClass          // Reference to the superclass
+	Annotations map[string]*Annotation  // Map of annotations
+	IsPrimitive bool                    // Indicates if the class represents a primitive type
+	IsArray     bool                    // Indicates if the class represents an array type
+	IsEnum      bool                    // Indicates if the class represents an enum type
+	IsInterface bool                    // Indicates if the class represents an interface
+	ClassLoader *object.Object          // Reference to the class loader
+	PackageName string                  // Package name of the class
 }
 
 // == Componnents of a Class ==
@@ -34,7 +41,7 @@ type Annotation struct {
 	Attributes map[string]string // Map of attribute names to their values
 }
 
-type Method struct {
+type ClassMethod struct {
 	Name           string                 // Name of the method
 	ReturnType     string                 // Fully qualified name of the return type
 	ParameterTypes []string               // List of fully qualified names of parameter types
@@ -58,7 +65,7 @@ type ClassField struct {
 	IsTransient bool                   // Indicates if the field is transient
 }
 
-func ClassFromInstance(obj *Object) *JavaLangClass {
+func ClassFromInstance(obj *object.Object) *JavaLangClass {
 	// Create a new Class object from an instance of a class.
 	if obj == nil {
 		return nil
@@ -69,7 +76,7 @@ func ClassFromInstance(obj *Object) *JavaLangClass {
 		Name:        objName,
 		Modifiers:   0, // Default to 0, can be set later
 		Fields:      make(map[string]*ClassField),
-		Methods:     make(map[string]*Method),
+		Methods:     make(map[string]*ClassMethod),
 		Interfaces:  []string{},
 		SuperClass:  nil, // Default to nil, can be set later
 		Annotations: make(map[string]*Annotation),
@@ -79,5 +86,23 @@ func ClassFromInstance(obj *Object) *JavaLangClass {
 		IsInterface: false, // Default to false, can be set later
 		ClassLoader: nil,   // Default to nil, can be set later
 	}
+
+	klass := MethAreaFetch(*objName)
+	if klass == nil {
+		errMsg := fmt.Sprintf(
+			"%s\n\tClassFromInstance(): Class %s not found in method area",
+			excNames.JVMexceptionNamesJacobin[excNames.InternalException], *objName)
+		trace.Error(errMsg) // TODO: check for circularity circumventing ptr
+		return nil
+	}
+
+	value, ok := obj.FieldTable["value"]
+	if ok {
+		valueType := value.Ftype
+		if types.IsArray(valueType) {
+			cl.IsArray = true
+		}
+	}
+
 	return &cl
 }
