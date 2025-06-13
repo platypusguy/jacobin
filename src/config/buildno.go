2bfad969a601d1e1c4a858a54bdeca1720e17d64
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2025-06-12 17:10:26 -0700

    JACOBIN-736 Edited and clarified comments in jvm/jvmStart.go

--- src/config/buildno.go
+++ src/config/buildno.go
@@ -8,4 +8,4 @@
 
 package config
 
-var BuildNo = 3732
+var BuildNo = 3734
--- src/jvm/jvmStart.go
+++ src/jvm/jvmStart.go
@@ -1,6 +1,6 @@
 /*
  * Jacobin VM - A Java virtual machine
- * Copyright (c) 2022-4 by the Jacobin authors. All rights reserved.
+ * Copyright (c) 2022-5 by the Jacobin authors. All rights reserved.
  * Licensed under Mozilla Public License 2.0 (MPL 2.0)
  */
 
@@ -25,8 +25,9 @@ var globPtr *globals.Globals
 
 // JVMrun is where everything begins
 // The call to shutdown.Exit() exits the program (after some clean-up and logging); the reason
-// it is here returned is because in testing mode, the actual exit() call is side-stepped and
-// instead an int is returned (because calling exit() during testing exits the testing run as well).
+// it is here returned is because: in testing mode, the actual exit() call is side-stepped and
+// instead an int is returned. This is necessary because calling exit() during testing exits
+// the testing run as well).
 func JVMrun() int {
 
 	trace.Init()
@@ -51,7 +52,8 @@ func JVMrun() int {
 	}()
 
 	// if globals.JacobinName == "test", then we're in test mode, which means
-	// globals and log have been set in the testing function. So, don't reset them here.
+	// globals and log have been set in the testing function, likely to specific values,
+	// so, don't reset them here.
 	if globals.GetGlobalRef().JacobinName != "test" {
 		// Not a test!
 		_ = globals.InitGlobals(os.Args[0])
@@ -69,12 +71,13 @@ func JVMrun() int {
 	// load static variables. Needs to be here b/c CLI might modify their values
 	statics.PreloadStatics()
 
-	// handle the command-line interface (cli) -- i.e., process the args
+	// handle the command-line interface (CLI) -- i.e., process the args
 	LoadOptionsTable(*globPtr)
 	err := HandleCli(os.Args, globPtr)
 	if err != nil {
 		return shutdown.Exit(shutdown.JVM_EXCEPTION)
 	}
+
 	// some CLI options, like -version, show data and immediately exit. This tests for that.
 	if globPtr.ExitNow == true {
 		return shutdown.Exit(shutdown.OK)
@@ -88,7 +91,7 @@ func JVMrun() int {
 	classloader.LoadBaseClasses() // must follow classloader.Init()
 
 	var mainClassNameIndex uint32
-	if globPtr.StartingJar != "" {
+	if globPtr.StartingJar != "" { // if a jar file was specified, then load the main class from it
 		manifestClass, err := classloader.GetMainClassFromJar(classloader.BootstrapCL, globPtr.StartingJar)
 
 		if err != nil {
@@ -106,7 +109,7 @@ func JVMrun() int {
 		if err != nil { // the exceptions message will already have been shown to user
 			return shutdown.Exit(shutdown.JVM_EXCEPTION)
 		}
-	} else if globPtr.StartingClass != "" {
+	} else if globPtr.StartingClass != "" { // if a class file was specified, then load the main class from it
 		mainClassNameIndex, _, err = classloader.LoadClassFromFile(classloader.BootstrapCL, globPtr.StartingClass)
 		if err != nil { // the exceptions message will already have been shown to user
 			return shutdown.Exit(shutdown.JVM_EXCEPTION)
@@ -117,16 +120,16 @@ func JVMrun() int {
 		return shutdown.Exit(shutdown.APP_EXCEPTION)
 	}
 
-	// if assertions were enable on the command line for the program, then make sure
-	// that it's set in the Statics table w/ an entry corresponding to the main class
-	// Otherwise, it was previously set to disabled
+	// if assertions were enabled on the command line for the program, then make sure
+	// that the assertion status is set in the Statics table w/ an entry corresponding
+	// to the main class. 	// Otherwise, it was previously initialized to "disabled".
 	if globPtr.Options["-ea"].Set {
 		_ = statics.AddStatic("main.$assertionsDisabled",
 			statics.Static{Type: types.Int, Value: types.JavaBoolFalse})
 	}
 
-	// the following was commented out per JACOBIN-327.
-	// Likely to be reinstated at some later point
+	// the following was commented out per JACOBIN-327. Likely to be reinstated at some later point.
+	// Preload the main class and its dependencies.
 	// classloader.LoadReferencedClasses(mainClass)
 
 	// initialize the MTable (table caching methods) and load the gfunctions
@@ -137,7 +140,6 @@ func JVMrun() int {
 	MainThread = thread.CreateThread()
 	MainThread.AddThreadToTable(globPtr)
 
-	// begin execution
 	mainClass := stringPool.GetStringPointer(mainClassNameIndex)
 	if globals.TraceInit {
 		trace.Trace("Starting execution with: " + *mainClass)
@@ -150,12 +152,18 @@ func JVMrun() int {
 	// Consequently, if StartExec() finishes, no errors were encountered.
 	//
 	// To test for errors, trap stderr, as many of the unit tests do.
-
 	StartExec(*mainClass, &MainThread, globPtr)
 
 	return shutdown.Exit(shutdown.OK)
 }
 
+// InitGlobalFunctionPointers initializes the global function pointers in the globals package.
+// These circumvent circular dependencies. A JVM is a textbook example of circular dependencies:
+// For example, the intepreter necessarily needs to deal with objects and to call exceptions;
+// exceptions are objects in the JVM, and certain functions that the object package contains
+// can throw exceptions. A typical golang solution is to stuff objects and exceptions into the
+// same package, but we prefer to keep them separate for ease of comprehension and navigability,
+// so we use global function pointers.
 func InitGlobalFunctionPointers(globalPtr *globals.Globals) {
 	globalPtr.FuncInstantiateClass = InstantiateClass
 	globalPtr.FuncMinimalAbort = exceptions.MinimalAbort
