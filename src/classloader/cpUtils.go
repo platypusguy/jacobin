848c678df6ed1db75f1f6f60e4f41efd136da32d
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2025-07-01 18:24:57 -0700

    JACOBIN-742 Implemented use of resolved interface references (similarly to how we resolve methods).

--- src/classloader/cpUtils.go
+++ src/classloader/cpUtils.go
@@ -190,27 +190,29 @@ func GetMethInfoFromCPmethref(CP *CPool, cpIndex int) (string, string, string, s
 }
 
 func GetMethInfoFromCPinterfaceRef(CP *CPool, cpIndex int) (string, string, string) {
-	methodRef := CP.CpIndex[cpIndex].Slot
-	classIndex := CP.InterfaceRefs[methodRef].ClassIndex
+	/*
+		methodRef := CP.CpIndex[cpIndex].Slot
+		classIndex := CP.InterfaceRefs[methodRef].ClassIndex
 
-	classRefIdx := CP.CpIndex[classIndex].Slot
-	classIdx := CP.ClassRefs[classRefIdx]
-	classNamePtr := stringPool.GetStringPointer(uint32(classIdx))
-	className := *classNamePtr
+		classRefIdx := CP.CpIndex[classIndex].Slot
+		classIdx := CP.ClassRefs[classRefIdx]
+		classNamePtr := stringPool.GetStringPointer(uint32(classIdx))
+		className := *classNamePtr
 
-	// now get the method signature
-	nameAndTypeCPindex := CP.InterfaceRefs[methodRef].NameAndType
-	nameAndTypeIndex := CP.CpIndex[nameAndTypeCPindex].Slot
-	nameAndType := CP.NameAndTypes[nameAndTypeIndex]
-	methNameCPindex := nameAndType.NameIndex
-	methNameUTF8index := CP.CpIndex[methNameCPindex].Slot
-	methName := CP.Utf8Refs[methNameUTF8index]
+		// now get the method signature
+		nameAndTypeCPindex := CP.InterfaceRefs[methodRef].NameAndType
+		nameAndTypeIndex := CP.CpIndex[nameAndTypeCPindex].Slot
+		nameAndType := CP.NameAndTypes[nameAndTypeIndex]
+		methNameCPindex := nameAndType.NameIndex
+		methNameUTF8index := CP.CpIndex[methNameCPindex].Slot
+		methName := CP.Utf8Refs[methNameUTF8index]
 
-	// and get the method signature/description
-	methSigCPindex := nameAndType.DescIndex
-	methSigUTF8index := CP.CpIndex[methSigCPindex].Slot
-	methSig := CP.Utf8Refs[methSigUTF8index]
-
+		// and get the method signature/description
+		methSigCPindex := nameAndType.DescIndex
+		methSigUTF8index := CP.CpIndex[methSigCPindex].Slot
+		methSig := CP.Utf8Refs[methSigUTF8index]
+	*/
+	className, methName, methSig, _ := GetMethInfoFromCPmethref(CP, cpIndex)
 	return className, methName, methSig
 }
 
--- src/config/buildno.go
+++ src/config/buildno.go
@@ -8,4 +8,4 @@
 
 package config
 
-var BuildNo = 3763
+var BuildNo = 3765
