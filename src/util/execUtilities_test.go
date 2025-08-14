5d6677fff193d3629cea55e314eaf0399059a698
Richard Elkins <richard.elkins@gmail.com>
2025-08-14 13:26:50 -0500

    Junie regenerated util/execUtilities_test.go

--- src/util/execUtilities_test.go
+++ src/util/execUtilities_test.go
@@ -7,132 +7,76 @@
 package util
 
 import (
-	"fmt"
 	"jacobin/types"
 	"testing"
 )
 
-// verify that a trailing slash in JAVA_HOME is removed
-func TestParseIncomingParamsFromMethType(t *testing.T) {
-	res := ParseIncomingParamsFromMethTypeString("(SBI)")
-	if len(res) != 3 { // short, byte and int all become 'I'
-		t.Errorf("Expected 3 parsed parameters, got %d", len(res))
+// Table-driven, OS-independent tests for ParseIncomingParamsFromMethTypeString
+func TestParseIncomingParamsFromMethType_Basics(t *testing.T) {
+	tests := []struct {
+		name      string
+		meth      string
+		expCount  int
+		expConcat string
+	}{
+		{"empty", "", 0, ""},
+		{"primitives_group", "(SBI)", 3, "III"},
+		{"mix_with_array", "(S[BI)I", 3, "I[BI"},
+		{"single_int", "(I)V", 1, types.Int},
 	}
-
-	if res[0] != types.Int || res[1] != types.Int || res[2] != types.Int {
-		t.Errorf("Expected parse would return 3 values of 'I', got: %s%s%s",
-			res[0], res[1], res[2])
-	}
-
-	res = ParseIncomingParamsFromMethTypeString("(S[BI)I")
-	if len(res) != 3 { // short, byte and int all become 'I' aka types.Int
-		t.Errorf("Expected 3 parsed parameters, got %d", len(res))
-	}
-
-	if res[0] != types.Int || res[1] != types.ByteArray || res[2] != types.Int {
-		t.Errorf("Expected parse would return S [B I, got: %s %s %s",
-			res[0], res[1], res[2])
-	}
-
-	res = ParseIncomingParamsFromMethTypeString("")
-	if len(res) != 0 {
-		t.Errorf("Expected parse would return value an empty string array, got: %s", res)
-	}
-}
-
-// test that pointer/reference in the params is handled correctly
-// especially, that references (start with L and with ;) are correctly
-// parsed and represented in the output
-
-// Parameter-driven checker
-func checker(t *testing.T, caller int, methType string, expCount int, expString string) {
-	res := ParseIncomingParamsFromMethTypeString(methType)
-	if len(res) != expCount { // short, byte and int all become 'I'
-		t.Errorf("[%d] Expected %d parsed parameters, got %d", caller, expCount, len(res))
-		for ii := 0; ii < len(res); ii++ {
-			fmt.Printf("Parameter %d: %v\n", ii, res[ii])
+	for _, tc := range tests {
+		res := ParseIncomingParamsFromMethTypeString(tc.meth)
+		if len(res) != tc.expCount {
+			t.Fatalf("%s: expected %d parsed parameters, got %d", tc.name, tc.expCount, len(res))
+		}
+		var got string
+		for _, r := range res {
+			got += r
+		}
+		if got != tc.expConcat {
+			t.Fatalf("%s: expected concat '%s', got '%s'", tc.name, tc.expConcat, got)
 		}
 	}
+}
 
-	var paramString string
-	for ii := 0; ii < len(res); ii++ {
-		paramString += res[ii]
+func TestParseIncomingParamsFromMethType_Table(t *testing.T) {
+	tests := []struct {
+		name      string
+		meth      string
+		expCount  int
+		expConcat string
+	}{
+		{"refs_and_longs", "(LString;Ljava/lang/Integer;JJ)V", 4, "LLJJ"},
+		{"two_strings", "(Ljava/lang/String;Ljava/lang/String;)Ljava/nio/file/Path;", 2, "LL"},
+		{"string_and_array_of_strings", "(Ljava/lang/String;[Ljava/lang/String;)Ljava/nio/file/Path;", 2, "L[L"},
+		{"array_of_strings_only", "([Ljava/lang/String;)V", 1, "[L"},
+		{"array_string_long_string", "([Ljava/lang/String;JLjava/lang/String;)V", 3, "[LJL"},
+		{"array_string_long_array_string", "([Ljava/lang/String;J[Ljava/lang/String;)V", 3, "[LJ[L"},
+		{"float_arrays_string_long_array_string_double", "(F[Ljava/lang/String;J[Ljava/lang/String;D)V", 5, "F[LJ[LD"},
+		{"deep_array_strings", "(F[[[[[Ljava/lang/String;J[Ljava/lang/String;D)V", 5, "F[[[[[LJ[LD"},
+		{"invalid_missing_semicolon_ref", "(Labc)V", 0, ""},
+		{"invalid_missing_semicolon_array_ref", "([Labc)V", 0, ""},
+		{"invalid_missing_semicolon_multi_array_ref", "([[[Labc)V", 0, ""},
+		{"invalid_unended_arrays", "([[[[)V", 0, ""},
+		{"valid_4d_int_array", "([[[[I)V", 1, "[[[[I"},
+		{"mix_with_deep_arrays", "(JF[[[[[Ljava/lang/String;[[[J)V", 4, "JF[[[[[L[[[J"},
+		{"invalid_illegal_char", "(JD[[[[[Ljava/lang/String;[[[J%)V", 0, ""},
+		{"valid_mixed_arrays_refs", "(JD[[I[[[Ljava/lang/String;[[[J)V", 5, "JD[[I[[[L[[[J"},
+		{"invalid_illegal_char_mid", "(JD[I[F[[[Ljava/lang/String;%[[[J)V", 0, ""},
+		{"valid_mixed_prims_and_refs", "(JD[I[F[[[Ljava/lang/String;[[[J)V", 6, "JD[I[F[[[L[[[J"},
 	}
-	if paramString != expString {
-		t.Errorf("[%d] Expected param string of '%s', got: '%s'", caller, expString, paramString)
+	for _, tc := range tests {
+		res := ParseIncomingParamsFromMethTypeString(tc.meth)
+		if len(res) != tc.expCount {
+			t.Errorf("%s: expected %d parsed parameters, got %d", tc.name, tc.expCount, len(res))
+			continue
+		}
+		var got string
+		for _, r := range res {
+			got += r
+		}
+		if got != tc.expConcat {
+			t.Errorf("%s: expected concat '%s', got '%s'", tc.name, tc.expConcat, got)
+		}
 	}
-}
-
-// Individual tests for ParseIncomingParamsFromMethTypeString
-
-func TestParseIncomingReferenceParamsFromMethType1(t *testing.T) {
-	checker(t, 1, "(LString;Ljava/lang/Integer;JJ)V", 4, "LLJJ")
-}
-
-func TestParseIncomingReferenceParamsFromMethType2(t *testing.T) {
-	checker(t, 2, "(Ljava/lang/String;Ljava/lang/String;)Ljava/nio/file/Path;", 2, "LL")
-}
-
-func TestParseIncomingReferenceParamsFromMethType3(t *testing.T) {
-	checker(t, 3, "(Ljava/lang/String;[Ljava/lang/String;)Ljava/nio/file/Path;", 2, "L[L")
-}
-
-func TestParseIncomingReferenceParamsFromMethType4(t *testing.T) {
-	checker(t, 4, "([Ljava/lang/String;)V", 1, "[L")
-}
-
-func TestParseIncomingReferenceParamsFromMethType5(t *testing.T) {
-	checker(t, 5, "([Ljava/lang/String;JLjava/lang/String;)V", 3, "[LJL")
-}
-
-func TestParseIncomingReferenceParamsFromMethType6(t *testing.T) {
-	checker(t, 6, "([Ljava/lang/String;J[Ljava/lang/String;)V", 3, "[LJ[L")
-}
-
-func TestParseIncomingReferenceParamsFromMethType7(t *testing.T) {
-	checker(t, 7, "(F[Ljava/lang/String;J[Ljava/lang/String;D)V", 5, "F[LJ[LD")
-}
-
-func TestParseIncomingReferenceParamsFromMethType8(t *testing.T) {
-	checker(t, 8, "(F[[[[[Ljava/lang/String;J[Ljava/lang/String;D)V", 5, "F[[[[[LJ[LD")
-}
-
-func TestParseIncomingReferenceParamsFromMethType9(t *testing.T) {
-	checker(t, 9, "(Labc)V", 0, "")
-}
-
-func TestParseIncomingReferenceParamsFromMethType10(t *testing.T) {
-	checker(t, 10, "([Labc)V", 0, "")
-}
-
-func TestParseIncomingReferenceParamsFromMethType11(t *testing.T) {
-	checker(t, 11, "([[[Labc)V", 0, "")
-}
-
-func TestParseIncomingReferenceParamsFromMethType12(t *testing.T) {
-	checker(t, 12, "([[[[)V", 0, "")
-}
-
-func TestParseIncomingReferenceParamsFromMethType13(t *testing.T) {
-	checker(t, 13, "([[[[I)V", 1, "[[[[I")
-}
-
-func TestParseIncomingReferenceParamsFromMethType14(t *testing.T) {
-	checker(t, 14, "(JF[[[[[Ljava/lang/String;[[[J)V", 4, "JF[[[[[L[[[J")
-}
-
-func TestParseIncomingReferenceParamsFromMethType15(t *testing.T) {
-	checker(t, 15, "(JD[[[[[Ljava/lang/String;[[[J%)V", 0, "")
-}
-
-func TestParseIncomingReferenceParamsFromMethType16(t *testing.T) {
-	checker(t, 16, "(JD[[I[[[Ljava/lang/String;[[[J)V", 5, "JD[[I[[[L[[[J")
-}
-
-func TestParseIncomingReferenceParamsFromMethType17(t *testing.T) {
-	checker(t, 17, "(JD[I[F[[[Ljava/lang/String;%[[[J)V", 0, "")
-}
-
-func TestParseIncomingReferenceParamsFromMethType18(t *testing.T) {
-	checker(t, 18, "(JD[I[F[[[Ljava/lang/String;[[[J)V", 6, "JD[I[F[[[L[[[J")
 }
