805fe0a1df5ed6cc3fe32c8176d5a987704406a9
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2025-12-03 00:18:03 -0800

    JACOBIN-720 minor code clean-up

--- src/gfunction/javaLangClass.go
+++ src/gfunction/javaLangClass.go
@@ -1,6 +1,6 @@
 /*
  * Jacobin VM - A Java virtual machine
- * Copyright (c) 2023 by  the Jacobin authors. Consult jacobin.org.
+ * Copyright (c) 2023-5 by  the Jacobin authors. Consult jacobin.org.
  * Licensed under Mozilla Public License 2.0 (MPL 2.0) All rights reserved.
  */
 
@@ -229,7 +229,7 @@ func getName(params []interface{}) interface{} {
 	return object.StringObjectFromGoString(name)
 }
 
-// "java/lang/Class.getName()Ljava/lang/String;"
+// "java/lang/Class.isArray()Ljava/lang/String;"
 func classIsArray(params []interface{}) interface{} {
 	obj := params[0].(*object.Object)
 	fldType := obj.FieldTable["value"].Ftype
