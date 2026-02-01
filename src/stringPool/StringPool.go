077dbb1ec31149de373a00fee307f799c7c7bfd7
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2026-02-01 14:24:45 -0800

    JACOBIN-794 updated explanatory comments and resequenced entries into alpha order

--- src/stringPool/StringPool.go
+++ src/stringPool/StringPool.go
@@ -1,63 +1,10 @@
 /*
  * Jacobin VM - A Java virtual machine
- * Copyright (c) 2024 by the Jacobin Authors. All rights reserved.
+ * Copyright (c) 2024-6 by the Jacobin Authors. All rights reserved.
  * Licensed under Mozilla Public License 2.0 (MPL 2.0)  Consult jacobin.org.
  */
 
 package stringPool
-
-/*
-Overview of the String Pool Functions
-
-The purpose of the String Pool is to provide a mechanism for Jacobin whereby string values are not duplicated.
-When a request is made to add a second instance of a string which is already in the Pool (GetStringIndex),
-the existing instance of the string will be used to satisfy the request.
-It is transparent to the caller of String Pool functions whether the presented string argument is
-pre-existing or is new to the Pool.
-
-String Pool components, in the globals package, common across all frames and threads:
--------------------------------------------------------------------------------------
-
-stringTable = map[string] --> uint32, an index to stringList (initially empty)
-stringList []string - the array of unique strings (initially empty)
-stringNext uint32 - the index of the next available stringList entry (initially 0)
-stringLock sync.Mutex - control modifications to this Pool (initially unlocked)
-
-Mid-level Functions:
---------------------
-
-MakeEmptyStringObject() *Object
-  - Create an object.Object for a java/lang/string with an empty FieldTable.
-  - Return a pointer to the object.
-
-NewPoolStringFromGoString(str string) *Object
-
-	Given a Go string,
-	* Store the string in the pool.
-	* Create an object.Object containing the index to the pool string.
-	* Return a pointer to the object.
-
-GetGoStringFromObject(strPtr *Object) string
-
-	Given a pointer to an object.Object containing an index to a pool string, return a Go string.
-
-Primitive Functions:
---------------------
-
-GetStringIndex(arg *string) uint32 -
-  - Given a pointer to a Go string, add the string to the pool if the string is not already present.
-  - Whether new or existing, return the index for the string for subsequent direct retrievals using stringList.
-
-GetStringPoolSize() uint32
-
-	Get the current string Pool size.
-
-EmptyStringPool() - Put the string Pool into an initial state. Useful in testing!
-
-DumpStringPool(context string) -
-  - Dump the contents of the string Pool.
-  - If the context parameter is not "", the context string will be shown at the beginning of the dump.
-*/
 
 import (
 	"fmt"
@@ -67,12 +14,58 @@ import (
 	"sort"
 )
 
-/*
------------------------------------
-The string pool primitive functions
------------------------------------
-*/
+// Overview of the String Pool Functions
+//
+// The purpose of the String Pool is to provide a mechanism for Jacobin whereby string values are not
+// duplicated. When a request is made to add a second instance of a string which is already in the Pool
+// (via GetStringIndex), the existing instance of the string will be used to satisfy the request.
+// It is transparent to the caller of String Pool functions whether the presented string argument is
+// pre-existing or is new to the Pool.
+//
+// String Pool components, in the globals package, common across all frames and threads:
+// -------------------------------------------------------------------------------------
+//
+// stringTable = map[string] --> uint32, an index to stringList (initially empty)
+// stringList []string - the array of unique strings (initially empty)
+// stringNext uint32 - the index of the next available stringList entry (initially 0)
+// stringLock sync.Mutex - control modifications to this Pool (initially unlocked)
 
+// Functions are in alpha order
+
+// DumpStringPool(dumpContext string) -
+// - Dump the contents of the string Pool.
+// - If the context parameter is not "", the context string will be shown at the beginning of the dump.
+func DumpStringPool(dumpContext string) {
+	globals.StringPoolLock.RLock()
+	defer globals.StringPoolLock.RUnlock()
+	if len(dumpContext) > 0 {
+		_, _ = fmt.Fprintf(os.Stdout, "\n===== DumpStringPool BEGIN context: %s\n", dumpContext)
+	} else {
+		_, _ = fmt.Fprintln(os.Stdout, "\n===== DumpStringPool BEGIN")
+	}
+	// Create an array of keys.
+	keys := make([]string, 0, len(globals.StringPoolTable))
+	for key := range globals.StringPoolTable {
+		keys = append(keys, key)
+	}
+	// Sort the keys.
+	// All the upper case entries precede all the lower case entries.
+	sort.Strings(keys)
+	// In key sequence order, display the key and its value.
+	for _, key := range keys {
+		_, _ = fmt.Fprintf(os.Stdout, "%d\t%s\n", globals.StringPoolTable[key], key)
+	}
+	_, _ = fmt.Fprintln(os.Stdout, "===== DumpStringPool END")
+}
+
+// EmptyStringPool is used exclusively for testing. If used in production, remove this comment.
+func EmptyStringPool() {
+	globals.InitStringPool()
+}
+
+// GetStringIndex(arg *string) uint32 returns the string index for the string pointed to by arg.
+// If the string is not already present in the pool, it is added. In fact, this is the primary
+// function for adding strings to the pool.
 func GetStringIndex(arg *string) uint32 {
 	if arg == nil {
 		nilString := ""
@@ -112,39 +105,13 @@ func GetStringPointer(index uint32) *string {
 	}
 }
 
+// GetStringPoolSize() uint32: Get the current string Pool size.
 func GetStringPoolSize() uint32 {
 	// glob := globals.GetGlobalRef()
 	return globals.StringPoolNext
 }
 
-// EmptyStringPool is used exclusively for testing. If used in production, remove this comment.
-func EmptyStringPool() {
-	globals.InitStringPool()
-}
-
-func DumpStringPool(context string) {
-	globals.StringPoolLock.RLock()
-	defer globals.StringPoolLock.RUnlock()
-	if len(context) > 0 {
-		_, _ = fmt.Fprintf(os.Stdout, "\n===== DumpStringPool BEGIN context: %s\n", context)
-	} else {
-		_, _ = fmt.Fprintln(os.Stdout, "\n===== DumpStringPool BEGIN")
-	}
-	// Create an array of keys.
-	keys := make([]string, 0, len(globals.StringPoolTable))
-	for key := range globals.StringPoolTable {
-		keys = append(keys, key)
-	}
-	// Sort the keys.
-	// All the upper case entries precede all the lower case entries.
-	sort.Strings(keys)
-	// In key sequence order, display the key and its value.
-	for _, key := range keys {
-		_, _ = fmt.Fprintf(os.Stdout, "%d\t%s\n", globals.StringPoolTable[key], key)
-	}
-	_, _ = fmt.Fprintln(os.Stdout, "===== DumpStringPool END")
-}
-
+// PreloadArrayClassesToStringPool() adds the names of the array classes for primitives
 func PreloadArrayClassesToStringPool() {
 	arrayClassesToPreload := []string{
 		types.BoolArray,
