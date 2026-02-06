705f1a5185b51e8e103bbe51c97855c568aed3d5
Richard Elkins <richard.elkins@gmail.com>
2026-02-06 15:03:09 -0600

    RSA private key class

--- src/gfunction/gfunction.go
+++ src/gfunction/gfunction.go
@@ -110,6 +110,7 @@ func MTableLoadGFunctions(MTable *classloader.MT) {
 	javaSecurity.Load_Security()
 	javaSecurity.Load_Security_Interfaces_RSAKey()
 	javaSecurity.Load_Security_Interfaces_RSAPublicKey()
+	javaSecurity.Load_Security_Interfaces_RSAPrivateKey()
 	javaSecurity.Load_Security_MessageDigest()
 	javaSecurity.Load_Security_Provider()
 	javaSecurity.Load_Security_Provider_Service()
