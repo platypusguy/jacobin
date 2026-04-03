2dd625b1ee2cd2d0a3bd8b0577bed2adef32b3bd
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2026-04-02 22:10:25 -0700

    JACOBIN-886 Getting rid of unneeded code and updating comments

--- src/classloader/jlc.go
+++ src/classloader/jlc.go
@@ -11,58 +11,13 @@ import (
 	"jacobin/src/types"
 )
 
-/*
-// instances of java/lang/Class as stored in global.JLCmap
-type Jlc struct {
-	Lock        sync.RWMutex
-	Statics     []string // list of all static fields
-	Name        string
-	IsPrimitive bool
-	KlassPtr    *ClData // points back to the class's data in the method area
-}
-
-// JLCmap is a map of java/lang/Class instances for statics and introspection.
-// Key is the FQN class name (e.g. "java/lang/String").
-// Value is a pointer to the internal Jlc struct.
-var JLCmap map[string]*Jlc
-var JlcMapLock sync.RWMutex
-
-// InitJlcMap initializes the JLCmap.
-// This should be called during classloader initialization.
-func InitJlcMap() {
-	JlcMapLock.Lock()
-	defer JlcMapLock.Unlock()
-	JLCmap = make(map[string]*Jlc, 2000)
-}
-
-// GetJlcEntry returns the JLC entry for a class.
-func GetJlcEntry(className string) *Jlc {
-	JlcMapLock.RLock()
-	defer JlcMapLock.RUnlock()
-	return JLCmap[className]
-}
-
-// GetJlcObject fetches the JLC entry for a class and converts
-// it into a java/lang/Class object
-func GetJlcObject(className string) *object.Object {
-	JlcMapLock.RLock()
-	defer JlcMapLock.RUnlock()
-	jlc := JLCmap[className]
-	if jlc == nil {
-		return nil
-	}
-
-	o := object.MakeEmptyObject()
-	o.KlassName = types.StringPoolJavaLangClassIndex
-	o.FieldTable["name"] = object.Field{Ftype: types.GolangString,
-		Fvalue: object.StringObjectFromGoString(jlc.Name)}
-	o.FieldTable["$klass"] = object.Field{Ftype: types.RawGoPointer,
-		Fvalue: jlc.KlassPtr} // points to the Klass object in metadata
-	o.FieldTable["$statics"] = object.Field{Ftype: types.Array,
-		Fvalue: jlc.Statics} // array of static field names for this class
-	return o
-}
-*/
+// A java/lang/Class object (or JLC object) is a standard object with the
+// following fields:
+//   name: *object.Object, which is the name of the class as a Java string object
+//      note: the object's KlassName is always java/lang/Class
+//   $klass: Klass, points to the ClData metadata in the method area for the named class
+//   $statics: []string, the names of the static fields of the class. These
+//      can be used as keys into the JVM's statics table.'
 
 // Makes a JLC object for a class and fills in only the name
 func MakeJlcObject(classname string) *object.Object {
@@ -76,21 +31,3 @@ func MakeJlcObject(classname string) *object.Object {
 		Fvalue: []string{}} // array of static field names for this class
 	return o
 }
-
-/*
-
-// MakeJlcEntry creates a new JLC entry for a class.
-func MakeJlcEntry(className string, primitive bool) *Jlc {
-	jlc := Jlc{}
-	jlc.Name = className
-	klass := MethAreaFetch(className)
-	if klass != nil {
-		jlc.KlassPtr = klass.Data
-	} else {
-		jlc.KlassPtr = nil
-	}
-	jlc.IsPrimitive = primitive
-	jlc.Statics = make([]string, 0)
-	return &jlc
-}
-*/
--- src/config/buildno.go
+++ src/config/buildno.go
@@ -8,4 +8,4 @@
 
 package config
 
-var BuildNo = 4389
+var BuildNo = 4390
