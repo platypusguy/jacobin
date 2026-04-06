b08100dbda2f6c0b19ee2439363b736031247220
Richard Elkins <richard.elkins@gmail.com>
2026-04-06 09:41:55 -0500

    JACOBIN-887 boolean statics TRUE & FALSE were missing

--- src/types/constants.go
+++ src/types/constants.go
@@ -35,6 +35,7 @@ var EmptyString = ""
 var NullString = "null"
 
 // Other useful class names in alpha order
+var ClassNameBigBoolean = "java/math/Boolean"
 var ClassNameBigDecimal = "java/math/BigDecimal"
 var ClassNameBigInteger = "java/math/BigInteger"
 var ClassNameJavaLangClass = "java/lang/Class"
