b151223a2b3c225bba64aa8f921ee2cb1dfd31a8
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2026-09-05 22:56:54 -0700

    JACOBIN-880 Updated version number to reflect resolution of all go vet results (and then some!)

--- src/config/buildno.go
+++ src/config/buildno.go
@@ -8,4 +8,4 @@
 
 package config
 
-var BuildNo = 4557
+var BuildNo = 4559
--- src/config/version.go
+++ src/config/version.go
@@ -1,6 +1,6 @@
 /*
  * Jacobin VM - A Java virtual machine
- * Copyright (c) 2024-5 by the Jacobin Authors. All rights reserved.
+ * Copyright (c) 2024-6 by the Jacobin Authors. All rights reserved.
  * Licensed under Mozilla Public License 2.0 (MPL 2.0)  Consult jacobin.org.
  */
 
@@ -15,7 +15,7 @@ import "strconv"
 // v 0.8.000 made  31 Aug 25 at 4,245 GitHub commits (end of year 4 of the project)
 // v 0.9.000 made  28 Feb 26 at 5,015 Github commits (technically, this file updated on 6 Mar 26)
 
-var JacobinVersion = "0.9.013"
+var JacobinVersion = "0.9.014"
 
 // GetJacobinVersion returns a manually updated version number and an
 // automatically updated build #. The latter being updated by bumpbuildno.go
