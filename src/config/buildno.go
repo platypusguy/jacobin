9f2a23a0032a56da7b5df199b0fdcaee27b44da5
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2026-09-19 21:45:54 -0700

    JACOBIN-880 Reflect @texadactyl's addition of traps for JDK library functions using a new scheme; also his addition of missing unit tests.

--- src/config/buildno.go
+++ src/config/buildno.go
@@ -8,4 +8,4 @@
 
 package config
 
-var BuildNo = 4571
+var BuildNo = 4573
--- src/config/version.go
+++ src/config/version.go
@@ -15,7 +15,7 @@ import "strconv"
 // v 0.8.000 made  31 Aug 25 at 4,245 GitHub commits (end of year 4 of the project)
 // v 0.9.000 made  28 Feb 26 at 5,015 Github commits (technically, this file updated on 6 Mar 26)
 // v 0.9.100 made   6 Sep 26 at 5,330 GitHub commits (end of year 5 of the project)
-var JacobinVersion = "0.9.101"
+var JacobinVersion = "0.9.102"
 
 // GetJacobinVersion returns a manually updated version number and an
 // automatically updated build #. The latter being updated by bumpbuildno.go
